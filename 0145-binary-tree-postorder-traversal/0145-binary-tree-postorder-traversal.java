98f7ae682535c3c07af5fb2f1f33406d2ba81de0
Lina <117023721+wulin-no2@users.noreply.github.com>
2023-10-15 23:30:06 +0100

    Time: 0 ms (100.00%), Space: 40.5 MB (77.11%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -66,5 +66,22 @@ class Solution {
     }
     Collections.reverse(result);
     return result;
+//         another solution:
+//         LinkedList<Integer> ans = new LinkedList<>();
+// 	Stack<TreeNode> stack = new Stack<>();
+// 	if (root == null) return ans;
+	
+// 	stack.push(root);
+// 	while (!stack.isEmpty()) {
+// 		TreeNode cur = stack.pop();
+// 		ans.addFirst(cur.val);
+// 		if (cur.left != null) {
+// 			stack.push(cur.left);
+// 		}
+// 		if (cur.right != null) {
+// 			stack.push(cur.right);
+// 		} 
+// 	}
+// 	return ans;
     }
 }

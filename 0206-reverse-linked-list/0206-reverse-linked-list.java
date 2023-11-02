7193d5beb047a16b44bd2ab1250e72c7b4a30cdc
Lina <117023721+wulin-no2@users.noreply.github.com>
2023-11-02 15:05:02 +0000

    Time: 2 ms (100.00%), Space: 41.2 MB (45.67%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        if(head == null || head.next == null){return head;}
+        //new stack:
+        Stack<ListNode> s = new Stack<>();
+        //push Node into the stack;
+        ListNode p = head;
+        while(p != null){
+            s.push(p);
+            p = p.next;
+        }
+        // get a new head of the reversed List;
+        ListNode newHead = s.pop();
+        // the reversed List should be a ListNode;
+        ListNode lp = newHead;
+        
+        // pop the elements from the stack:
+        while(!s.isEmpty()){
+            lp.next = s.pop();
+            lp = lp.next;
+        }
+        // set the last node to null:
+        lp.next = null;
+        return newHead;
+        
+        
+        
+    /*
+        //new LinkedList;
+        LinkedList<ListNode> l = new LinkedList<>();
+        //new stack:
+        Stack<ListNode> s = new Stack<>();
+        // new pointer:
+        ListNode p = head;
+        s.push(p);
+        //push every p.next into the stack untill p.next == null:
+        while(p.next != null){
+            s.push(p.next);
+            p = p.next;
+        }
+        // pop the elements from the stack:
+        while(!s.isEmpty()){
+            l.add(s.pop());
+        }
+        return l.get(0);  
+        */
+        
+    }
+}

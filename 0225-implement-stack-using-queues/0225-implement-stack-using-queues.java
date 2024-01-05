67ffc2a201f12ad4221493f30a9d63de79c0eae8
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-05 19:35:48 +0000

    Time: 0 ms (100.00%), Space: 41.5 MB (5.31%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,58 @@
+class MyStack {
+    // analyze:
+    // use 2 queues;
+    // when push(), q1.push();
+    // when pop(), q2.push(q1.pop()) until q1.size = 1, then get q1.pop(); then push elements back to q1;
+    // when top(), similar to pop();
+    // when empty(), see if q1 is empty;
+    // Java only has Deque() class, instead of Queue() class;
+    Queue q1 = new LinkedList();
+    Queue q2 = new LinkedList();
+    
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+        q1.offer(x);
+        
+    }
+    
+    public int pop() {
+        while(q1.size() > 1){
+            q2.offer(q1.poll());
+        }
+        int res = (int)q1.poll();
+        while(q2.size() > 0){
+            q1.offer(q2.poll());
+        }
+        return res;    
+    }
+    
+    public int top() {
+        while(q1.size() > 1){
+            q2.offer(q1.poll());
+        }
+        int res = (int)q1.peek();
+        q2.offer(q1.poll());
+        
+        while(q2.size() > 0){
+            q1.offer(q2.poll());
+        }
+        return res;   
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+        
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */

b011117c3d5a63832f928305cf1c2183f2eaa0bb
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-07 22:24:50 +0000

    Time: 0 ms (100.00%), Space: 41.7 MB (10.99%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -15,73 +15,17 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        // List<Integer> arrL = new ArrayList<>();
-//         following is the recursive answer:
-        // if(root != null){
-        //     arrL.addAll(postorderTraversal(root.left));
-        //     arrL.addAll(postorderTraversal(root.right));
-        //     arrL.add(root.val);
-        // }
-        // return arrL;
-        
-//         following is the iterative answer:
-        // Stack<TreeNode> stack = new Stack<>();
-        // TreeNode node = root;
-        // if(node != null){
-        //     stack.push(root);
-        // }
-        // while(node != null){
-        //     if(node.left != null && node.right != null){
-        //         stack.push(node.right);
-        //         stack.push(node.left);
-        //         node = node.left;
-        //     }else if(node.left != null && node.right == null){
-        //         stack.push(node.left);
-        //         node = node.left;
-        //     }else if(node.left == null && node.right != null){
-        //         stack.push(node.right);
-        //         node = node.right;
-        //     }else{
-        //         arrL.add(stack.pop().val);
-        //         node = stack.pop();
-        //         stack.push(node);
-        //     }
-        // }
-        // while(!stack.isEmpty()){
-        //     arrL.add(stack.pop().val);
-        // }
-        // return arrL;
-    List<Integer> result = new ArrayList<>();
-    Deque<TreeNode> stack = new ArrayDeque<>();
-    TreeNode p = root;
-    while (p != null || !stack.isEmpty()) {
-        if (p != null) {
-            result.add(p.val);  // Add before going to children
-            stack.push(p);
-            p = p.right;
-        } else {
-            p = stack.pop();
-            p = p.left;
+        // analyze:
+        // 2 approach:
+        // first, recursive:
+        ArrayList<Integer> arr = new ArrayList<>();
+        // base case:
+        if(root == null){
+            return arr;
         }
-    }
-    Collections.reverse(result);
-    return result;
-//         another solution:
-//         LinkedList<Integer> ans = new LinkedList<>();
-// 	Stack<TreeNode> stack = new Stack<>();
-// 	if (root == null) return ans;
-	
-// 	stack.push(root);
-// 	while (!stack.isEmpty()) {
-// 		TreeNode cur = stack.pop();
-// 		ans.addFirst(cur.val);
-// 		if (cur.left != null) {
-// 			stack.push(cur.left);
-// 		}
-// 		if (cur.right != null) {
-// 			stack.push(cur.right);
-// 		} 
-// 	}
-// 	return ans;
+        arr.addAll(postorderTraversal(root.left));
+        arr.addAll(postorderTraversal(root.right));
+        arr.add(root.val);
+        return arr;
     }
 }

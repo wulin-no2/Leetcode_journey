35314fd80da7399dd1b7a7a44ce0df54ed7d302e
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-07 23:30:19 +0000

    Time: 0 ms (100.00%), Space: 40.9 MB (58.15%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -57,7 +57,7 @@ class Solution {
         
         // if not, it should be as following:
         // third approach:
-        List<Integer> res = new ArrayList<Integer>();
+        /* List<Integer> res = new ArrayList<Integer>();
   
           if (root == null)
             return res;
@@ -82,6 +82,39 @@ class Solution {
             res.add(s2.pop().val);
 
           return res;
+          */
+        
+        // my solution correctly:
+        // Create an array list to store the solution result...
+        List<Integer> sol = new ArrayList<Integer>();
+        // Return the solution answer if the tree is empty...
+        if(root==null) return sol;
+        // Create an empty stack and push the root node...
+        Stack<TreeNode> bag = new Stack<TreeNode>();
+        bag.push(root);
+        // Loop till stack is empty...
+        while(!bag.isEmpty()) {
+            // set peek a node from the stack...
+            TreeNode node = bag.peek();
+            // If the subtrees of that node are null, then pop & store the pop value into solution result...
+            if(node.left==null && node.right==null) {
+                TreeNode pop = bag.pop();
+                sol.add(pop.val);
+            }
+            else {
+                // Push the right child of the popped node into the stack...
+                if(node.right!=null) {
+                    bag.push(node.right);
+                    node.right = null;
+                }
+                // Push the left child of the popped node into the stack...
+                if(node.left!=null) {
+                    bag.push(node.left);
+                    node.left = null;
+                }
+            }
+        }
+        return sol;     // Return the solution list...
         
     }
 }

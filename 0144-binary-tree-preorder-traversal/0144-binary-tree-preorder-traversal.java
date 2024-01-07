a4634b3598b354d8f920e0ead673d902929dba43
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-07 22:01:57 +0000

    Time: 0 ms (100.00%), Space: 41.6 MB (11.15%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,6 +15,34 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        // analyze:
+        // we need an arraylist to store the values;
+        // we can use recursive approach or iterative approach:
+        
+        // first, recursive approach:
+        ArrayList<Integer> arr = new ArrayList<>();
+        // base case:
+        if(root == null){
+            return arr;
+        }
+        arr.add(root.val);
+        arr.addAll(preorderTraversal(root.left));
+        arr.addAll(preorderTraversal(root.right));
+        return arr;
+        
+        // every time, we add the value into the arrayList, then
+        // we push them into stack.if the node has children, 
+        // right child first, left child last;
+        // then pop an elements;
+        // repeat the above process;
+        
+        
+        
+        
+        
+        
+        
+        /* following is iterative approach before.
         List<Integer> arrayList1 = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<TreeNode>();
         stack.push(root);
@@ -27,5 +55,6 @@ class Solution {
             }
         }
         return arrayList1;
+        */
     }
 }

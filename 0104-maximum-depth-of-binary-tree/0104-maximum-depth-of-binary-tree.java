d3a869471e191ffe6dfb6ad0112da2f77a427c11
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-08 15:54:42 +0000

    Time: 3 ms (12.82%), Space: 42.5 MB (22.94%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -18,11 +18,95 @@ class Solution {
         // analyze:
         // d(root) = d(child) + 1;
         // we use recursive approach:
+        // time and space complexity is O(n)
+        /*
         if(root == null){
             return 0;
         }
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        */
+        
+        // now we try to do it in iterative approach:
+        // DFS, use stack;
+        /* no!!! I can't do it T-T
+        if(root == null){
+            return 0;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        // for each node: if it has children, h++, push children into stack; until stack is null;
+        int h = 1;
+        stack.push(root);
+        TreeNode temp = null;
+        while(! stack.isEmpty()){
+            temp = stack.pop();
+            if(temp.left != null && temp.right != null){
+                h++;
+                stack.push(temp.left);
+                stack.push(temp.right);
+            }else if(temp.left != null && temp.right == null){
+                h++;
+                stack.push(temp.left);
+            }else if(temp.left == null && temp.right != null){
+                h++;
+                stack.push(temp.right);
+            }else{
+                continue;
+            }
+        }
+        return h;
+        */
+        
+        // see others:
+        // DFS:
+        
+        if(root == null) {
+        return 0;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> value = new Stack<>();
+        stack.push(root);
+        value.push(1);
+        int max = 0;
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int temp = value.pop();
+            max = Math.max(temp, max);
+            if(node.left != null) {
+                stack.push(node.left);
+                value.push(temp+1);
+            }
+            if(node.right != null) {
+                stack.push(node.right);
+                value.push(temp+1);
+            }
+        }
+        return max;
         
         
+        
+        // BFS:
+        /*
+        if(root == null) {
+        return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while(size-- > 0) {
+                TreeNode node = queue.poll();
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            count++;
+        }
+        return count;
+        */
     }
 }

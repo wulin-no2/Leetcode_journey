db87d3f8b0f54143173edda306a65ba097ede3bb
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-08 17:09:00 +0000

    Time: 24 ms (43.68%), Space: 40.1 MB (27.52%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -9,6 +9,7 @@ public class Solution extends VersionControl {
         // find the true 1, then the left one must be false;
         
         // we numst use minimize numbers of API
+        /*
         int low = 1;
         int high = n;
         while(low <= high){
@@ -24,5 +25,24 @@ public class Solution extends VersionControl {
             }
         }
         return n;
+        */
+        
+        // there is a faster approach in discuss:
+        int i = 1;
+        int j = n;
+
+        while (i < j) {
+
+            int mid = i + (j - i) / 2;
+
+            if (isBadVersion(mid)) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+
+        }
+
+        return i;
     }
 }

164cf74c017f8ec5960bfffce148caa81f48ae9d
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-08 00:12:45 +0000

    Time: 0 ms (100.00%), Space: 40.9 MB (52.79%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,7 +15,8 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        // recursive approach:
+        // recursive approach: 2024.1.7.23.52;
+        /*
         List<Integer> arr = new ArrayList<>();
         // base case:
         if(root == null){
@@ -25,6 +26,33 @@ class Solution {
         arr.add(root.val);
         arr.addAll(inorderTraversal(root.right));
         return arr;
+        */
         
+        // iterative approach:
+        // first, go to the leftmost;
+        //      if a node has left children, push in stack, go to left child;
+        // if a node has no left child, add its value into arr and see right child;
+        
+        // use stack;
+        List<Integer> arr = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if(root == null){
+            return arr;
+        }
+        // use a pointer:
+        TreeNode p = root;
+        while(! stack.isEmpty() || p!= null){
+            // traverse until the leftmost;
+            while(p != null){
+                stack.add(p);
+                p = p.left;
+            }
+            // pop a node and check;
+            p = stack.pop();
+            arr.add(p.val);
+            p = p.right; 
+        }
+        
+        return arr;
     }
 }

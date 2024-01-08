7cf8d069aaa0999ee43792d7fd880a636af61d64
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-08 14:32:20 +0000

    Time: 0 ms (100.00%), Space: 44.2 MB (20.72%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,23 +14,44 @@
  * }
  */
 class Solution {
+    boolean res = true;
     public boolean isBalanced(TreeNode root) {
         // analyze:
-        // if both the height of the left child and the height of right child <=1, return true;
+        // if both the left child and the right child is balanced and their height difference <= 1, return true;
         // how to get the height of a node?
         // DFS, use a pointer, while the pointer has a child, h++; pointer move to its left child or its right child;
-        // use recursive approach:
+        // use recursive approach, but O(n^2):
+        /*
         // base case:
         if(root == null){
             return true;
         }
         return (Math.abs(h(root.left) - h(root.right)) <= 1) && isBalanced(root.left) && isBalanced(root.right);
-            
-        
     }
     // write a method to get height:
     public int h(TreeNode root){
         if(root == null){return 0;}
         return  Math.max(h(root.left), h(root.right)) + 1;
+    }
+    */
+        
+        // the second approach, optimized by checking if subtree is balanced in the h() method:
+        // base case:
+        if(root == null){
+            return true;
+        }
+        int h1 = h(root.left);
+        int h2 = h(root.right);
+        return Math.abs(h1 - h2) <= 1 && res;
+    }
+    public int h(TreeNode root){
+        if(root == null){return 0;}
+        int height1 = h(root.left);
+        int height2 = h(root.right);
+        int height = Math.max(height1, height2) + 1;
+        if (Math.abs(height1 - height2) > 1){
+            res = false;
+        }
+        return height;
     }
 }

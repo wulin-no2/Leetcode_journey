b6f985c91c25ef02bedaa6cc106f652d9b355c19
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-08 13:34:14 +0000

    Time: 0 ms (100.00%), Space: 44.9 MB (20.55%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -16,6 +16,8 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         // can't do it, juct copy one and see why
+        // BFS iterative approach:
+        /*
         // base case:
         List<List<Integer>> res = new ArrayList<>(); 
         if (root == null) return res;  
@@ -41,6 +43,31 @@ class Solution {
           res.add(list);   
         }  
         return res;
+        */
+        
+        // DFS recursive approach:
+        List<List<Integer>> res = new ArrayList<>();
+		if (root == null)
+			return res;
+		levelOrderHelper(res, root, 0);
+		return res;
+	}
+	
+	public void levelOrderHelper(List<List<Integer>> res, TreeNode root, int level) {
+		if (root == null)
+			return;
+		List<Integer> curr;
+		if (level >= res.size()) {
+			curr = new ArrayList<>();
+			curr.add(root.val);
+			res.add(curr);
+		} else {
+			curr = res.get(level); 
+			curr.add(root.val); 
+			//res.add(curr); // No need to add the curr into the res, because the res.get(index) method does not remove the index element
+		}
+		levelOrderHelper(res, root.left, level + 1);
+		levelOrderHelper(res, root.right, level + 1);
         
     }
 }

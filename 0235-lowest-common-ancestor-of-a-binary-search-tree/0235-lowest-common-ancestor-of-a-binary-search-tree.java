710193618812a87ef3a2fd12e5cb56d132a7f963
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-09 19:14:01 +0000

    Time: 5 ms (100.00%), Space: 45.2 MB (8.03%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -17,16 +17,13 @@ class Solution {
         // if one >= p.val && one =< p.val, then return p;
         // we can use recursive:
         // 1) order them;
-        if(p.val > q.val){
-            TreeNode temp = q;
-            q = p;
-            p = temp;
-        } 
-        // 2) traverse from root;
-        if(p.val <= root.val && q.val >= root.val) return root;
+        int max = Math.max(q.val, p.val);
+        int min = Math.min(q.val, p.val);
+
+        if(min <= root.val && max >= root.val) return root;
         // branch 1: all the left;
-        if(q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+        if(max < root.val) return lowestCommonAncestor(root.left, p, q);
         // branch 2: all the right;
-        return lowestCommonAncestor(root.right, p, q);
+        return lowestCommonAncestor(root.right, p, q);  
     }
 }

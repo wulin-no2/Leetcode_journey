c3713925792ea810f179b565fa22394eb5a8ddb5
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-09 11:47:40 +0000

    Time: 7 ms (13.52%), Space: 44.5 MB (6.80%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -13,8 +13,25 @@ class Solution {
         // the problem is. a and b are too long.
         // we can't simply tranfer them to int, or they will surpass the constraint of the boundary of int
         // we can use BigInteger
-        
+        /*
         return new java.math.BigInteger(a, 2).add(new java.math.BigInteger(b, 2)).toString(2);
-        
+        */
+        String result = "";
+        int carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            carry = sum / 2;
+            result = (sum % 2) + result;
+        }
+        return result;
     }
 }

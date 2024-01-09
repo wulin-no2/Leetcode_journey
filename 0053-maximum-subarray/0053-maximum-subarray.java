a3bb0ca101d90e4c95f66f5fa13a229dbf2b7fe6
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-09 16:19:36 +0000

    Time: 1 ms (99.98%), Space: 57.3 MB (71.39%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // analyze:
+        // we can use kadane's algorithm;
+        // use 2 int and 2 LinkedList to store arrays;
+        // 1) int max_ending_here
+        // 2) int max
+        // update step by step
+        
+        int maxEndingHere = nums[0];
+        //LinkedList<Integer> cddArray = new LinkedList<>();
+        //cddArray.add(nums[0]);
+        
+        int max = nums[0];
+        //LinkedList<Integer> maxArray = new LinkedList<>();
+        //maxArray.add(nums[0]);
+        
+        for(int i = 1; i < nums.length; i++){
+            if(maxEndingHere + nums[i] > nums[i]){
+                maxEndingHere = maxEndingHere + nums[i];
+                //cddArray.add(nums[i]); 
+            }else{
+                maxEndingHere = nums[i];
+                //cddArray = null;
+                //cddArray.add(nums[i]); 
+            }
+            
+            if(max < maxEndingHere){
+                max = maxEndingHere;
+                //maxArray = cddArray;
+            } 
+        }
+        return max;
+        
+        
+            
+        
+    }
+}

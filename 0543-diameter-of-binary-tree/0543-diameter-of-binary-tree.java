894f2295b51bab2573cc8cd2369e56512c961de1
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-09 14:56:31 +0000

    Time: 0 ms (100.00%), Space: 43.2 MB (10.02%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,11 +14,13 @@
  * }
  */
 class Solution {
+    /*
     public int diameterOfBinaryTree(TreeNode root) {
         // analyze:
         // if level is 1, res = 0; level is 2, res = 1; 
         // res = max(each node's height of left child + height of right child) 
         // how to avoid redundant calculation? now it's O(n^2)
+        /*
         if(root == null) return 0;
         if(root.left == null && root.right == null) return 0;
         int max = 0;
@@ -46,4 +48,28 @@ class Solution {
         // if(root.left == null && root.right == null) return 0;-----------shouldn't add this line. why???
         return Math.max(h(root.left), h(root.right)) + 1; 
     }
+    */
+    
+    // how to make it better? 
+    //we can calculate the diameter each time we get height.
+    // then diameter will be a viarable of the class;
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        height(root);
+        return max;
+        
+    }
+    private int height(TreeNode root){
+        //if root==null height==0
+        if(root == null) return 0;
+        
+        int L = height(root.left);
+        int R = height(root.right);
+        //ans signfies(no. of nodes farthest apart) or the DIAMETER
+        max = Math.max(max, L + R);
+        //height of the tree is max of LST & RST +1
+        return Math.max(L, R) + 1;
+    }
+    
 }

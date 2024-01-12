8f1e0e0b201d8f347661a1a428551a0fb82e3245
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-12 19:31:07 +0000

    Time: 102 ms (46.07%), Space: 46.3 MB (11.53%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -3,6 +3,7 @@ class Solution {
         // from 16:30
         // from this solution:https://leetcode.com/problems/longest-palindromic-substring/discuss/4212564/Beats-96.49-oror-5-Different-Approaches-oror-Brute-Force-oror-EAC-oror-DP-oror-MA-oror-Recursion-oror
         // Manacher's Algorithm & DP
+        /* first: O(N)
         if(s.length() == 1) return s;
         int maxLen = 1;
         String maxStr = s.substring(0, 1);
@@ -30,5 +31,32 @@ class Solution {
             }
         }
         return maxStr;
+        */
+        
+        // Second: O(n^2)
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int start = 0;
+        int end = 0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, end + 1);
     }
 }

4c7efa0748a7324024a782488c184a3b2c77926b
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-12 23:21:48 +0000

    Time: 0 ms (100.00%), Space: 43.7 MB (68.09%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        // recursive:
+        // base case:
+        if(root == null) return true;
+        if(root.left == null && root.right == null) return true;
+        // we must get the rightmost value of left and leftmost value of right
+        boolean l = true;
+        boolean r = true;
+        if(root.left != null) l = isValidBST(root.left) && (mValue(root.left, 1) < root.val);
+        if(root.right != null) r = isValidBST(root.right) && (mValue(root.right, 0) > root.val);
+        return l && r;
+    }
+    // return a BST's min or max
+    private int mValue(TreeNode root, int dir){
+        // dir == 0, means left, min; dir == 1, means right, max;
+        // if(root == null) return null;
+        if(root.left== null && root.right== null) return root.val;
+        TreeNode p = root;
+        if(dir == 0){
+            while(p.left != null){
+                p = p.left;
+            }
+            
+        }else{
+            while(p.right != null){
+                p = p.right;
+            }
+        }
+        return p.val;
+    }
+}

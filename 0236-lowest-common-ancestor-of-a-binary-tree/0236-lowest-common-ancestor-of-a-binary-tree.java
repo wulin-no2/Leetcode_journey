1acaab379d4a27d623e39c00333d3488c31fab49
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-13 01:58:28 +0000

    Time: 18 ms (6.06%), Space: 45.3 MB (9.89%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -26,6 +26,7 @@ class Solution {
     */
     
     // another approach. we don't need to know if node is inTree:
+    /*
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if( root == p || root == q || root == null) return root;
         TreeNode left = lowestCommonAncestor( root.left,  p,  q);
@@ -34,6 +35,33 @@ class Solution {
         else if (right == null) return left;
         else return root;
     }
+    */
+    
+    // another iterative approach:
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    Map<TreeNode, TreeNode> parent = new HashMap<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    parent.put(root, null);
+    queue.add(root);
+    while (!parent.containsKey(p) || !parent.containsKey(q)) {
+        TreeNode node = queue.poll();
+        if (node != null) {
+            parent.put(node.left, node);
+            parent.put(node.right, node);
+            queue.add(node.left);
+            queue.add(node.right);
+        }
+    }
+    Set<TreeNode> set = new HashSet<>();
+    while (p != null) {
+        set.add(p);
+        p = parent.get(p);
+    }
+    while (!set.contains(q)) {
+        q = parent.get(q);
+    }
+    return q;
+}
         
     
     

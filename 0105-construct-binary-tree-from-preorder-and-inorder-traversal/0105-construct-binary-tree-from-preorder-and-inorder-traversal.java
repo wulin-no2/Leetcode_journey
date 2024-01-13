f3430433db64e81b77950fe97d0b836fd8882d7d
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-13 01:17:53 +0000

    Time: 2 ms (68.46%), Space: 44.9 MB (13.30%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -40,6 +40,9 @@ class Solution {
         }
         */
     //another approach:
+    /*
+    // find root, then we know left and right;
+    
     int[] preorder;
     int[] inorder;
     int index;
@@ -65,6 +68,24 @@ class Solution {
             }
         }
         return -1;
-
+    }
+    */
+    
+    // 3rd approch: use hashmap so it's faster;
+    public TreeNode buildTree(int[] P, int[] I) {
+        Map<Integer, Integer> M = new HashMap<>();
+        for (int i = 0; i < I.length; i++)
+            M.put(I[i], i);
+        return splitTree(P, M, 0, 0, I.length-1);
+    }
+    
+    private TreeNode splitTree(int[] P, Map<Integer, Integer> M, int pix, int ileft, int iright) {
+        int rval = P[pix], imid = M.get(rval);
+        TreeNode root = new TreeNode(rval);            
+        if (imid > ileft)
+            root.left = splitTree(P, M, pix+1, ileft, imid-1);
+        if (imid < iright)
+            root.right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright);
+        return root;
     }
 }

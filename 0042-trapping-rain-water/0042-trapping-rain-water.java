736fd3b684aeff2cdf7c52b034bc87fb4e3cd22b
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-14 19:39:47 +0000

    Time: 0 ms (100.00%), Space: 46.7 MB (5.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,6 +1,6 @@
 class Solution {
     public int trap(int[] height) {
-        /* 1st solution:
+        /* 1st solution:don't understand:
         // if (height == null) return 0;  //unnecessary in this case bc n == height.length, 1 <= n <= 2 * 104
         int i     = 0;
         int water = 0;
@@ -46,7 +46,7 @@ class Solution {
         */
         
         // solution3: following is brute force approach: o(n^2) sloooooow; last 1;
-        /*
+        /* easy understand;
           int n = height.length;
           int totalWater = 0;
           for (int k = 0; k < n; ++k) {
@@ -64,24 +64,44 @@ class Solution {
           return totalWater;
           */
         // solution4 : DP,based on solution3, but get leftMax and rightMax first to improve efficiency;
-      int n = height.length;
-      if (n <= 2) return 0;
-      // pre-compute
-      int[] leftMax = new int[n];
-      int[] rightMax = new int[n];
-      leftMax[0] = height[0]; // init
-      rightMax[n - 1] = height[n - 1];
-      for (int i = 1, j = n - 2; i < n; ++i, --j) {
-        leftMax[i] = Math.max(leftMax[i - 1], height[i]);
-        rightMax[j] = Math.max(rightMax[j + 1], height[j]);
-      }
-      // water
-      int totalWater = 0;
-      for (int k = 1; k < n - 1; ++k) { // do not consider the first and the last places
-        int water = Math.min(leftMax[k - 1], rightMax[k + 1]) - height[k];
-        totalWater += (water > 0) ? water : 0;
-      }
-      return totalWater;
+        /* easy understand;
+          int n = height.length;
+          if (n <= 2) return 0;
+          // pre-compute
+          int[] leftMax = new int[n];
+          int[] rightMax = new int[n];
+          leftMax[0] = height[0]; // init
+          rightMax[n - 1] = height[n - 1];
+          for (int i = 1, j = n - 2; i < n; ++i, --j) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            rightMax[j] = Math.max(rightMax[j + 1], height[j]);
+          }
+          // water
+          int totalWater = 0;
+          for (int k = 1; k < n - 1; ++k) { // do not consider the first and the last places
+            int water = Math.min(leftMax[k - 1], rightMax[k + 1]) - height[k];
+            totalWater += (water > 0) ? water : 0;
+          }
+          return totalWater;
+        */ 
+          int n = height.length;
+          int lo = 0, hi = n - 1;
+          int leftMax = 0, rightMax = 0;
+          int water = 0;
+          while (lo < hi) {
+            // update
+            if (height[lo] > leftMax)  leftMax = height[lo];
+            if (height[hi] > rightMax) rightMax = height[hi];
+            // compute
+            if (leftMax < rightMax) { // consider the min
+                water += (leftMax - height[lo]); // leftMax >= height[lo]
+                ++lo;
+            } else {
+                water += (rightMax - height[hi]);
+                --hi;
+            }
+          }
+          return water;
         
     }
 }

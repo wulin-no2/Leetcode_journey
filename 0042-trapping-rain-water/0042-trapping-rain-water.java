e7239ad2ea49c325e8b2aada4561c54758948211
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-14 19:07:06 +0000

    Time: 1 ms (77.54%), Space: 45.8 MB (34.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -44,6 +44,9 @@ class Solution {
         }
         return water;
         */
+        
+        // solution3: following is brute force approach: o(n^2) sloooooow; last 1;
+        /*
           int n = height.length;
           int totalWater = 0;
           for (int k = 0; k < n; ++k) {
@@ -59,5 +62,26 @@ class Solution {
             totalWater += (water > 0) ? water : 0;
           }
           return totalWater;
+          */
+        // solution4 : DP,based on solution3, but get leftMax and rightMax first to improve efficiency;
+      int n = height.length;
+      if (n <= 2) return 0;
+      // pre-compute
+      int[] leftMax = new int[n];
+      int[] rightMax = new int[n];
+      leftMax[0] = height[0]; // init
+      rightMax[n - 1] = height[n - 1];
+      for (int i = 1, j = n - 2; i < n; ++i, --j) {
+        leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        rightMax[j] = Math.max(rightMax[j + 1], height[j]);
+      }
+      // water
+      int totalWater = 0;
+      for (int k = 1; k < n - 1; ++k) { // do not consider the first and the last places
+        int water = Math.min(leftMax[k - 1], rightMax[k + 1]) - height[k];
+        totalWater += (water > 0) ? water : 0;
+      }
+      return totalWater;
+        
     }
 }

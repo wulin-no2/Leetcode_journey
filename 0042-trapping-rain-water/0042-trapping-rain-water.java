79b512afa2f5f93e6dfb668f6e4652f523bbd4a1
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-14 19:02:17 +0000

    Time: 688 ms (5.00%), Space: 46.4 MB (17.66%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -20,6 +20,8 @@ class Solution {
         }
         return water;
         */
+        // another approach: 2 pointers. don't understand:
+        /*
         int left = 0, right = height.length - 1;
         int leftMax = height[0], rightMax = height[height.length - 1];
         int water = 0;
@@ -41,5 +43,21 @@ class Solution {
             }
         }
         return water;
+        */
+          int n = height.length;
+          int totalWater = 0;
+          for (int k = 0; k < n; ++k) {
+            int leftMax = 0;
+            for (int i = 0; i <= k - 1; ++i) {
+              leftMax = Math.max(leftMax, height[i]);
+            }
+            int rightMax = 0;
+            for (int i = k + 1; i < n; ++i) {
+              rightMax = Math.max(rightMax, height[i]);
+            }
+            int water = Math.min(leftMax, rightMax) - height[k];
+            totalWater += (water > 0) ? water : 0;
+          }
+          return totalWater;
     }
 }

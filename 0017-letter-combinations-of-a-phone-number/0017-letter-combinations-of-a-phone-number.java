dc445546219706de7521c415af58186b586fe062
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-14 01:08:31 +0000

    Time: 6 ms (16.91%), Space: 42.1 MB (21.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public List<String> letterCombinations(String digits) {
         if (digits.isEmpty()) return Collections.emptyList();
 
@@ -18,5 +19,35 @@ class Solution {
             }
         }
     }
+    */
+    // another approach:
+    /*
+    method combine is to add new letters to old list, using 2 for-loop.
+    for example:
+    gave digits = "23"
+    i=0 -> result=combine("abc", [""]) ---> [a,b,c];
+    i=1 -> result=combine("def", [a,b,c]) ---> [ad,bd,cd, ae,be,ce, af,bf,cf];
+    */
+    public static List<String> letterCombinations(String digits) {
+        String digitletter[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String> result = new ArrayList<String>();
 
+        if (digits.length()==0) return result;
+
+        result.add("");
+        for (int i=0; i<digits.length(); i++) 
+            result = combine(digitletter[digits.charAt(i)-'0'],result);
+
+        return result;
+    }
+
+    public static List<String> combine(String digit, List<String> l) {
+        List<String> result = new ArrayList<String>();
+
+        for (int i=0; i<digit.length(); i++) 
+            for (String x : l) 
+                result.add(x+digit.charAt(i));
+
+        return result;
+    }
 }

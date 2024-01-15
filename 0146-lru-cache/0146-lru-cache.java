e5f3f9909277709fab84799fa49089118d1c0c8d
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-15 23:49:20 +0000

    Time: 47 ms (49.92%), Space: 124.4 MB (19.56%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,6 +1,7 @@
-class LRUCache {
+//class LRUCache {
 
     // one solution from discuss:
+    /*
     Node head = new Node(0, 0), tail = new Node(0, 0);
     Map<Integer, Node> map = new HashMap();
     int capacity;
@@ -55,10 +56,75 @@ class LRUCache {
           value = _value;
         }
     }
+    */
+    class Node{//节点[pre|key|value|next]
+    int key,val;
+    Node next,pre;
+    public Node(int key,int val){
+        this.key = key;
+        this.val = val;
+    }
+}
+class DoubleList{//双链表head<->[key,val]<->tail
+    Node head;//头结点
+    Node tail;//尾结点
+    public DoubleList(){
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        head.next = tail;
+        tail.pre = head;
+    }
+    void addFirst(Node n){//头插
+        head.next.pre = n;
+        n.next = head.next;
+        n.pre = head;
+        head.next = n;
+    }
+    void remove(Node n){//删除指定节点n
+        n.pre.next = n.next;
+        n.next.pre = n.pre;
+    }
+    Node removeLast(){//删除尾结点，并返回该节点
+        Node res = tail.pre;
+        remove(res);
+        return res;
+    }
+}
+class LRUCache {
+    HashMap<Integer,Node> map;
+    DoubleList cache;
+    int cap;//容量
+    public LRUCache(int capacity) {
+        map = new HashMap<>();
+        cache = new DoubleList();
+        this.cap = capacity;
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key))//若该节点不存在
+            return -1;
+        Node res = map.get(key);
+        cache.remove(res);
+        cache.addFirst(res);
+        return res.val;
+    }
+    
+    public void put(int key, int value) {
+        Node n = new Node(key,value);
+        if(map.containsKey(key)){//若该节点已经存在
+            cache.remove(map.get(key));
+        }else if(map.size()==cap){//若该节点不存在，但是cache已满
+            Node last = cache.removeLast();
+            map.remove(last.key);
+        }
+        cache.addFirst(n);
+        map.put(key,n);
+    }
+}
   
     
     
-}
+//}
 
 /**
  * Your LRUCache object will be instantiated and called as such:

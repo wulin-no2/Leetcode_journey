77b1ca2c90cb01aa90b4305f3e7f02d05c5a6137
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-16 14:48:34 +0000

    Time: 28 ms (77.39%), Space: 42.2 MB (84.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,9 +1,9 @@
 class Solution {
-    boolean[] dp;
+    
     public boolean canPartition(int[] nums) {
         // 1) sum must be even, or return false;
         // 2) find sub array so that their sum is sum/2;
-        // dp[subsum] in array = dp[subsum-array[n-1]] in subarray;
+        // dp[subsum] in array = dp[subsum-array[n-1]] or dp[subsum] in subarray;
         // calculate sum:
         int sum = 0;
         for(int i =0 ; i < nums.length ; i++) sum = sum + nums[i];
@@ -11,17 +11,16 @@ class Solution {
         // 2) state transition equation:
         sum = sum / 2;
         // find if we can find sum of some elements from the array;
-        dp = new boolean[sum + 1];
-        
+        boolean[] dp = new boolean[sum + 1];
+        // the key part of dp solution:
+        // why dp[0] is true?
+        // why traverse from sum to num;
         dp[0] = true;
         for (int num : nums) {
             for (int j = sum; j >= num; j--) {
                 dp[j] = dp[j] || dp[j - num];
             }
         }
-
         return dp[sum];
-        
     }
-
 }

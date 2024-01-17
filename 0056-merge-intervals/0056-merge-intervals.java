ef15b525fb7eae934bebea2b43851ee259bca4d1
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-17 17:35:43 +0000

    Time: 12 ms (19.81%), Space: 46.3 MB (47.52%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        // sort them according to first numbers;
+        // then each time compare new start with first end, if start > end, add it;
+        // else, compare their end, set the lager end as now end; pop one and push one;
+        // till the end;
+        // maybe we don't need to sort them, just compare? No we have to sort them. or it will be complicated;
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        int len = intervals.length;
+        // use arrayList to add new intervals; more flexible;
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
+        for(int i = 1; i < len; i++){
+            int[] last = list.get(list.size() - 1);
+            if(intervals[i][0] > last[1]) list.add(intervals[i]);
+            else{
+                list.remove(list.size() - 1);
+                list.add(new int[]{Math.min(intervals[i][0],last[0]), Math.max(intervals[i][1], last[1])});
+            }
+        }
+        // now we get list. how to transfer list to Array;
+        return list.toArray(new int[list.size()][2]);
+    }
+}

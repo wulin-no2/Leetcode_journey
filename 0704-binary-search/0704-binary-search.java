ded0e9d3f8bd8b320392d32a789aab3a46677051
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-17 15:58:53 +0000

    Time: 0 ms (100.00%), Space: 45.6 MB (30.12%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -6,7 +6,7 @@ class Solution {
         // change low and high, check mid;
         int low = 0;
         int high = nums.length - 1;
-        while(low <= high){
+        while(low < high){
             int mid = (low + high) / 2;
             if(nums[mid] == target){
                 return mid;
@@ -16,7 +16,7 @@ class Solution {
                 high = mid - 1;
             }
         }
-        return -1;
-        
+        if(nums[low] == target) return low;
+        return -1;   
     }
 }

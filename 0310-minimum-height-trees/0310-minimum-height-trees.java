9bbd64365574f14a3586fed2bd308ea90be3c699
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-19 18:36:50 +0000

    Time: 28 ms (65.14%), Space: 58 MB (24.70%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -0,0 +1,51 @@
+class Solution {
+    /*
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        // dfs;
+        int min = n;
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0 ; i < n ; i ++){
+            int temp = dfs(i, n, n, edges);
+            if(min == temp) res.add(i);
+            else if(min > temp) {
+                min = temp;
+                res.clear();
+                res.add(i);
+            } 
+        }
+    }
+    private int dfs(int i, int remain, int len, int n, int[][] edges){
+        if(remain == 0) return len;
+        if(i >= 0 && i < n){
+            len = 1 + dfs()
+            
+            
+        } 
+    }*/
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n == 1) return Collections.singletonList(0);
+
+        List<Set<Integer>> adj = new ArrayList<>(n);
+        for (int i = 0; i < n; ++i) adj.add(new HashSet<>());
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; ++i)
+            if (adj.get(i).size() == 1) leaves.add(i);
+
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int i : leaves) {
+                int j = adj.get(i).iterator().next();
+                adj.get(j).remove(i);
+                if (adj.get(j).size() == 1) newLeaves.add(j);
+            }
+            leaves = newLeaves;
+        }
+        return leaves;
+    }
+}

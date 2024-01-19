91b8c27813f7da0e4d68fab45f9f791eadea5a10
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-19 17:07:57 +0000

    Time: 30 ms (77.84%), Space: 48.4 MB (43.53%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -27,17 +27,20 @@ class Solution {
        }
        // Now deal with the remain; 2 pointers?        
     }
-    */
-        public List<List<String>> accountsMerge(List<List<String>> accounts) {
+    */ 
+    // solution from neetcode: 
+    // hard to know it's dsu. even know, still can't do it
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
         int n = accounts.size();
         DSU dsu = new DSU(n);
 
         Map<String, Integer> map = new HashMap<>();  // email -> index of acc
-
+        // put unique emails into map;
+        // union duplicate emails in dsu;
         for(int i = 0; i < n; i++){
             for(int j = 1; j < accounts.get(i).size(); j++){
                 String email = accounts.get(i).get(j);
-                String name = accounts.get(i).get(0);
+                //String name = accounts.get(i).get(0); // didn't use it?
 
                 if(!map.containsKey(email))
                     map.put(email, i);
@@ -47,22 +50,26 @@ class Solution {
         }
 
         Map<Integer, List<String>> merged = new HashMap<>();  // index of acc -> list of emails
+        // traverse emails in map.
+        // get each email's rootIndex in dsu;
+        // add them into merged map<Integer, List<String>>
         for(String email : map.keySet()){
-            int group = map.get(email);
-            int lead = dsu.find(group);
-            
-            if(!merged.containsKey(lead))
-                merged.put(lead, new ArrayList<String>());
+            int index = map.get(email);
+            int rootIndex = dsu.find(index);
 
-            merged.get(lead).add(email);    
+            if(!merged.containsKey(rootIndex)) 
+                merged.put(rootIndex, new LinkedList<String>());
+            merged.get(rootIndex).add(email);    
         }
-
+        // convert map to List;
+        // use list to store result;
+        // use tempList to store each element list;
         List<List<String>> res = new ArrayList<>();  
-        for(int ac : merged.keySet()){
-            List<String> grp = merged.get(ac);
-            Collections.sort(grp);
-            grp.add(0, accounts.get(ac).get(0));
-            res.add(grp);
+        for(int i : merged.keySet()){
+            List<String> temp = merged.get(i);
+            Collections.sort(temp);
+            temp.add(0, accounts.get(i).get(0));
+            res.add(temp);
         }
         return res;
     }
@@ -86,20 +93,15 @@ class DSU {
     }
 
     // Union By Rank
-
     public boolean union(int x, int y) {
         int xr = find(x), yr = find(y);
-        if (xr == yr) {
-            return false;
-        } else if (rank[xr] < rank[yr]) {
-            parent[xr] = yr;
-        } else if (rank[xr] > rank[yr]) {
-            parent[yr] = xr;
-        } else {
+        if (xr == yr) return false;
+        else if (rank[xr] < rank[yr]) parent[xr] = yr;
+        else if (rank[xr] > rank[yr]) parent[yr] = xr;
+        else {
             parent[yr] = xr;
             rank[xr]++;
         }
         return true;
     }
-    
 }

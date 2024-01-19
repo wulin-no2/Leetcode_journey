e7dd96c63c03dc3767329419be6118d65f504857
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-19 18:50:36 +0000

    Time: 28 ms (65.14%), Space: 58.3 MB (18.44%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -24,24 +24,27 @@ class Solution {
     }*/
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         if (n == 1) return Collections.singletonList(0);
-
+        // get a preMap for each node;
         List<Set<Integer>> adj = new ArrayList<>(n);
         for (int i = 0; i < n; ++i) adj.add(new HashSet<>());
         for (int[] edge : edges) {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-
+        // find leaves;
         List<Integer> leaves = new ArrayList<>();
         for (int i = 0; i < n; ++i)
             if (adj.get(i).size() == 1) leaves.add(i);
-
+        // 
         while (n > 2) {
+            // n is number of non-leave nodes;
             n -= leaves.size();
             List<Integer> newLeaves = new ArrayList<>();
             for (int i : leaves) {
+                // trim leaves;
                 int j = adj.get(i).iterator().next();
                 adj.get(j).remove(i);
+                // get newLeaves;
                 if (adj.get(j).size() == 1) newLeaves.add(j);
             }
             leaves = newLeaves;

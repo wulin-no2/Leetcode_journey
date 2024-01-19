c1d3a0da11a671fba5484222fb8d0c1b815f69e7
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-19 21:28:37 +0000

    Time: 29 ms (34.63%), Space: 45.4 MB (52.83%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -3,13 +3,16 @@ class Solution {
         // analyze: find the most task, put it every n; 
         // then put the 2nd most task after each first task;
         if (n == 0) return tasks.length;
+        // here is a comparator implemented in lambda expression:
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
         int[] arr = new int[26];
+        // find the frequency of each letter;
         for (char c : tasks) arr[c - 'A']++;
+        // if frequency > 0,add to pq;
         for (int val : arr) if (val > 0) pq.add(val);
         int time = 0;
-
+        // start running:
         while ((!pq.isEmpty() || !q.isEmpty())) {
             time++;
             if (!pq.isEmpty()) {
@@ -23,6 +26,5 @@ class Solution {
             );
         }
         return time;
-        
     }
 }

e7d1bb420eaa1cdd38a1cb52b0a8e3cfbcb728ce
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-23 22:50:20 +0000

    Time: 0 ms (100.00%), Space: 41.8 MB (34.13%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        // 2pointers
+        int slow = 0;
+        for(int fast = 0; fast < nums.length; fast++){
+            if(nums[fast] != val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+        
+    }
+}

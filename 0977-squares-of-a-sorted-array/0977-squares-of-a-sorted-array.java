7014b6abfdb51bbf438173e6aa7238f0f3568b46
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-24 17:56:22 +0000

    Time: 3 ms (46.49%), Space: 46.1 MB (55.89%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -12,26 +12,26 @@ class Solution {
         Deque<Integer> list = new LinkedList<>();
         int left = 0;
         int right = nums.length - 1;
-        System.out.println(" left: "+left + " right: " + right);
+        //System.out.println(" left: "+left + " right: " + right);
         while(left <= right){
             if(nums[left]*nums[left] >= nums[right]*nums[right]){
                 list.addFirst(nums[left]*nums[left]);
                 left++;
-                System.out.println(" left: "+left + " right: " + right);
+                //System.out.println(" left: "+left + " right: " + right);
             }
 
             else{
                 list.addFirst(nums[right]*nums[right]);
                 right--;
-                System.out.println(" left: "+left + " right: " + right);
+                //System.out.println(" left: "+left + " right: " + right);
             }
         }
-        System.out.println(list);
-        System.out.println(list.size());
+        //System.out.println(list);
+        //System.out.println(list.size());
         int[] result = new int[nums.length];
         for(int i = 0; i < nums.length; i++){
             result[i] = list.removeFirst();
-            System.out.println(" result[]"+ i+ "] " + result[i]);
+            //System.out.println(" result[]"+ i+ "] " + result[i]);
         }
         return result; 
         

bb8faf68edb1133e859ac093771c9ec992a51180
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-26 02:28:59 +0000

    Time: 1 ms (91.60%), Space: 45.6 MB (33.09%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,10 +10,11 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        /* from solution;
         if (head == null) {
             return head;
         }
-        // 因为删除可能涉及到头节点，所以设置dummy节点，统一操作
+        // use a dummy head as the pre of the head;
         ListNode dummy = new ListNode(-1, head);
         ListNode pre = dummy;
         ListNode cur = head;
@@ -26,9 +27,23 @@ class Solution {
             cur = cur.next;
         }
         return dummy.next;
+        */
+        // write on my own;
+        if(head == null) return head;
+        // use a dummy head as the pre of the head;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        // use a pointer to traverse;
+        ListNode cur = dummy; // since we need to delete the next node. so the first one should be dummy then we won't ignore head;
+        while(cur.next != null){
+            if(cur.next.val == val){
+                cur.next = cur.next.next;
+            }
+            else{
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
 
-        
-        
-        
     }
 }

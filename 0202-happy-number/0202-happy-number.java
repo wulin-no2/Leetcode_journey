9a9d79a6a3ea35b57a71d6259bf038f91c03ba4c
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-28 18:28:07 +0000

    Time: 0 ms (100.00%), Space: 40.5 MB (57.48%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -21,7 +21,8 @@ class Solution {
         }
     }
     */
-    // solution from camp:
+    // solution from camp using set:
+    /*
     public boolean isHappy(int n) {
         Set<Integer> record = new HashSet<>();
         while (n != 1 && !record.contains(n)) {
@@ -39,5 +40,40 @@ class Solution {
             n = n / 10;
         }
         return res;
+    }
+    */
+    // solution from discuss using 2 pointers. just see it as cycled linkedlist;
+    public boolean isHappy(int n) {
+        
+        int slow = n;
+        int fast = n;
+//while loop is not used here because initially slow and 
+//fast pointer will be equal only, so the loop won't run.
+        do {
+//slow moving one step ahead and fast moving two steps ahead
+
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast);
+
+//if a cycle exists, then the number is not a happy number
+//and slow will have a value other than 1
+
+        return slow == 1;
+    }
+    
+//Finding the square of the digits of a number
+
+    public int square(int num) {
+        
+        int ans = 0;
+        
+        while(num > 0) {
+            int remainder = num % 10;
+            ans += remainder * remainder;
+            num /= 10;
+        }
+        
+        return ans;
     }
 }

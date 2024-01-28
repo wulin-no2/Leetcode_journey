29f4203056b84aba9512579d608f4274777ca4e1
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-28 18:21:48 +0000

    Time: 2 ms (22.76%), Space: 40.7 MB (33.00%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,4 +1,5 @@
 class Solution {
+    /* my solution using string to get digit:
     public boolean isHappy(int n) {
         // use a set to store each result. if it's contained in set, return false; if it's 1, return true;
         HashSet<Integer> set = new HashSet<>();
@@ -18,5 +19,25 @@ class Solution {
             num = temp + "";
             // System.out.println("set: " + set);
         }
+    }
+    */
+    // solution from camp:
+    public boolean isHappy(int n) {
+        Set<Integer> record = new HashSet<>();
+        while (n != 1 && !record.contains(n)) {
+            record.add(n);
+            n = getNextNumber(n);
+        }
+        return n == 1;
+    }
+
+    private int getNextNumber(int n) {
+        int res = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            res += temp * temp;
+            n = n / 10;
+        }
+        return res;
     }
 }

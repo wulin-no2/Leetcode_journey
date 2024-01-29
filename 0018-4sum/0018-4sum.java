cb3230be92d279ec1cede756f0a6326489dd6b94
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-29 12:59:02 +0000

    Time: 154 ms (12.44%), Space: 45.8 MB (7.60%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -7,24 +7,19 @@ class Solution {
         // just like 3 sum, we have more loop;
         for(int i = 0 ; i < nums.length - 3; i++){
             if(target > 0 && nums[i] > target || target < 0 && nums[i] >= 0) continue;
-            // System.out.println("i:" + i);
             for(int j = i + 1; j < nums.length - 2; j++){
                 if(target > 0 && nums[i] + nums[j] > target || target < 0 && nums[i] + nums[j] >= 0) continue;
-                // System.out.println("j:" + j);
                 // 2 pointers;
                 int left = j + 1;
                 int right = nums.length - 1;
                 while(left < right){
-                    // System.out.println("left and right:" + left + " " + right);
                     if(nums[left] + nums[right] + nums[i] + nums[j] == target){
                         set.add(Arrays.asList(nums[left], nums[right], nums[i], nums[j]));
                         right--;
                         left++;
-                        // System.out.println("set:" + set);
                     } 
                     else if(nums[left] + nums[right] + nums[i] + nums[j] > target) right--;
                     else left++;
-                    // System.out.println("left and right:" + left + " " + right);
                 }
             }
         }

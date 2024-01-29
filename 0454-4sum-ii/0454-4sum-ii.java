0f512b952167badc23325d1de27e34ac15e50bd4
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-29 11:30:42 +0000

    Time: 147 ms (25.21%), Space: 45.2 MB (8.85%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        // we need to know all the combitions of the tuples
+        // first, we can get the first 2 arrays sum results and
+        // the last 2 arrays sum results;
+        // then, it's 2 sum question now.
+        // we can use map to store them. cause we need to know the counts;
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+        for(int i : nums1){
+            for(int j : nums2){
+                map1.put( i + j, map1.getOrDefault( i + j , 0) + 1);
+            }
+        }
+        for(int i : nums3){
+            for(int j : nums4){
+                map2.put( i + j, map2.getOrDefault( i + j , 0) + 1);
+            }
+        }
+        // HashSet<Integer> set = new HashSet<>();
+        int res = 0;
+        for(Integer i : map1.keySet()){
+            int temp = 0 - i;
+            if(map2.containsKey(temp)) res += map1.get(i) * map2.get(temp);
+        }
+        return res;  
+    }
+}

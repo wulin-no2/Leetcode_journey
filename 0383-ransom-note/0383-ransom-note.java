7def4bd8828c4743fea47ba9aff1e198a893a8b6
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-29 12:02:56 +0000

    Time: 1 ms (99.18%), Space: 44.3 MB (78.63%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        // it's letters so we can use array or map;
+        int[] arr = new int[26];
+        for(int i = 0 ; i < magazine.length() ; i ++){
+            arr[magazine.charAt(i) - 'a']++;
+        }
+        for(int i = 0 ; i < ransomNote.length() ; i ++){
+            arr[ransomNote.charAt(i)-'a']--;
+            if(arr[ransomNote.charAt(i)-'a']< 0) return false;
+        }
+        return true;
+        
+        
+        
+        
         // find elements from collection
         // order is not important
         // can be repeated
@@ -48,6 +62,7 @@ class Solution {
         */
         
         // again, it's a string with alphametic and we have to know the count of each alphametic, so we we can use int[] to store the count of magezine
+        /*
         int[] count = new int[26];
         for(char m : magazine.toCharArray()){
             count[m - 'a']++;
@@ -59,9 +74,6 @@ class Solution {
             }
         }
         return true;
-        
-  
-    
-    
+        */
     }
 }

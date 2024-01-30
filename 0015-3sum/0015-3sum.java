a6c864e6014977e7395fe4864703259276955047
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-30 23:45:26 +0000

    Time: 23 ms (98.90%), Space: 51.3 MB (67.16%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,5 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+                List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+	// 找出a + b + c = 0
+        // a = nums[i], b = nums[left], c = nums[right]
+        for (int i = 0; i < nums.length; i++) {
+	    // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
+            if (nums[i] > 0) { 
+                return result;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1]) {  // 去重a
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (right > left) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+		    // 去重逻辑应该放在找到一个三元组之后，对b 和 c去重
+                    while (right > left && nums[right] == nums[right - 1]) right--;
+                    while (right > left && nums[left] == nums[left + 1]) left++;
+                    
+                    right--; 
+                    left++;
+                }
+            }
+        }
+        return result;
+        /* my solution with hashset:
         // use List to store the result;
         // no!!! we should use set to store the result!!! Elements should be unique!!!!
         HashSet<List<Integer>> set = new HashSet<>();
@@ -29,31 +64,6 @@ class Solution {
             }
         }
         res.addAll(set);
-        return res;
-        //from discuss:
-        /*
-        int target = 0;
-        Arrays.sort(nums);
-        // need to use hashSet;
-        Set<List<Integer>> s = new HashSet<>();
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++){
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                } else if (sum < target) {
-                    j++;
-                } else {
-                    k--;
-                }
-            }
-        }
-        res.addAll(s);
         return res;
         */
         

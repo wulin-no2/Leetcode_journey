6a9ad3385f6560b177ae2943c3c9dab1ef5b18b1
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-30 12:10:16 +0000

    Time: 14 ms (79.01%), Space: 44.1 MB (50.64%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -113,22 +113,21 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         // sort it:
         Arrays.sort(nums);
-        // use hashSet to store result;
-        //HashSet<List<Integer>> set = new HashSet<>();
-        ArrayList<List<Integer>> set = new ArrayList<>();
+        // use list to store the result;
+        ArrayList<List<Integer>> list = new ArrayList<>();
         // just like 3 sum, we have more loop;
         for(int i = 0 ; i < nums.length - 3; i++){
             if(target > 0 && nums[i] > target || target < 0 && nums[i] >= 0) continue;
-            if (i > 0 && nums[i - 1] == nums[i]) continue;    // 对nums[i]去重
+            if (i > 0 && nums[i - 1] == nums[i]) continue;    // remove duplicate
             for(int j = i + 1; j < nums.length - 2; j++){
-                if (j > i + 1 && nums[j - 1] == nums[j]) continue; // 对nums[j]去重
+                if (j > i + 1 && nums[j - 1] == nums[j]) continue; // remove duplicate
                 if(target > 0 && nums[i] + nums[j] > target || target < 0 && nums[i] + nums[j] >= 0) continue;
                 // 2 pointers;
                 int left = j + 1;
                 int right = nums.length - 1;
                 while(left < right){
                     if(nums[left] + nums[right] + nums[i] + nums[j] == target){
-                        set.add(Arrays.asList(nums[left], nums[right], nums[i], nums[j]));
+                        list.add(Arrays.asList(nums[left], nums[right], nums[i], nums[j]));
                         while (right > left && nums[right] == nums[right - 1]) right--;
                         while (right > left && nums[left] == nums[left + 1]) left++;
                         right--;
@@ -139,9 +138,7 @@ class Solution {
                 }
             }
         }
-        // List<List<Integer>> res = new ArrayList<>();
-        // res.addAll(set);
-        return set;
+        return list;
     }
     
 }

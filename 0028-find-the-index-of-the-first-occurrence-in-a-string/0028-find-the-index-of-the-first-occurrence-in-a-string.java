47be5a4c64347ace1f42494ca475c697d9038255
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-31 21:33:19 +0000

    Time: 0 ms (100.00%), Space: 41.1 MB (66.73%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -10,20 +10,16 @@ class Solution {
         // ...loop entil the end;
         int i = 0; // hay pointer;
         int temp = 0; // index of the start;
-        while( i < hay.length){
+        while(i < hay.length){
             int j = 0; // need pointer;
             // find first;
-            //System.out.println("i: " + i + " j: " + j + " temp: " + temp);
             if(hay[i] != need[j]) i++;
             // if ==, record the index;
             else{
                 temp = i;
-                //System.out.println("i: " + i + " j: " + j + " temp: " + temp);
                 // see if following letters are the same;
                 while(j < need.length && i < hay.length){
-                    //System.out.println("i: " + i + " j: " + j);
                     if(hay[i] == need[j]) {
-                        //System.out.println("i: " + i + " j: " + j + " need.length - 1: " + (need.length-1));
                         if(j == need.length - 1) return temp;
                         i++;
                         j++;

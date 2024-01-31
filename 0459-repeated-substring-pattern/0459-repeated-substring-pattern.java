d0330b04497c1e15f6ab2ee68a552a63c9c7e6ee
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-01-31 22:37:46 +0000

    Time: 34 ms (72.09%), Space: 45 MB (56.33%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,33 +1,30 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        ///*
+        /* from GPT:
+        
         for (int i = 1; i <= s.length() / 2; i++) {
             if (s.length() % i == 0) {
                 String substring = s.substring(0, i);
                 StringBuilder temp = new StringBuilder();
-                for (int j = 0; j < s.length() / i; j++) {
-                    temp.append(substring);
-                }
-                if (temp.toString().equals(s)) {
-                    return true;
-                }
+                for (int j = 0; j < s.length() / i; j++) temp.append(substring);
+                if (temp.toString().equals(s)) return true;
             }
         }
         return false;
-        //*/
-        /* my own solution: slooooooow;
+        */
+        // /* my own solution: slooooooow;
         // convert to StringBuilder;
         // StringBuilder sb = new StringBuilder(s);
         // find the nth same letter with first letter; use subArray to see if it's work;
         for(int i = 1 ; i <= s.length() / 2; i++){
-            String sub = s.substring(0, i);
-            StringBuilder temp = new StringBuilder();
-            while (temp.length() < s.length()){
-                temp.append(sub);
+            if (s.length() % i == 0) {
+                String sub = s.substring(0, i);
+                StringBuilder temp = new StringBuilder();
+                while (temp.length() < s.length()) temp.append(sub);
+                if(temp.toString().equals(s)) return true;
             }
-            if(temp.toString().equals(s)) return true;
         }
         return false;  
-        */
+        // */
     }
 }

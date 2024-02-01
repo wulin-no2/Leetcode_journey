096ac90e137c31c42d9ab7e4a49c769e1a80848a
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-01 20:02:38 +0000

    Time: 0 ms (100.00%), Space: 41.3 MB (37.82%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,9 +1,41 @@
 class MyQueue {
+    Deque<Integer> s1 = new LinkedList<>();
+    Deque<Integer> s2 = new LinkedList<>();
+    public MyQueue(){
+        
+    }
+    public void push(int x){
+        // always push to s1;
+        s1.push(x);
+    }
+    public int pop(){
+        // always pop() from s2 until it's empty; then pop all elements from s1 to s2 and pop();
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+    public int peek(){
+        // always peek()  from s2 until it's empty; then pop all elements from s1 to s2 and peek();
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+    public boolean empty(){
+        return s1.isEmpty() && s2.isEmpty();
+        
+    }
+    /*mu solution before:
     // analyze:
     // how to make if FIFO?
-    // use 2 stacks;
-    // when push(), find the non empty one to push;
-    // when pop(), pop() all the elements to the other one, and pop(), and pop() to the original one again;
+    // use 2 stacks in which the second one is as an assistant;
+    // when push(), find first one to push;
+    // when pop(), pop() all the elements to the assistant and pop(), and pop() to the first one again;
     // when peek, similar to pop;
     // when empty(), the first stack should be empty;
     Stack stack;
@@ -44,6 +76,7 @@ class MyQueue {
     public boolean empty() {
         return stack.isEmpty();
     }
+    */
 }
 
 /**

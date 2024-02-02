9398bfc3fe928e7b069dd6382a9aea50d00489e2
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-02 22:20:10 +0000

    Time: 5 ms (96.18%), Space: 45.5 MB (41.68%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,5 +1,8 @@
 class Solution {
+
     public String removeDuplicates(String s) {
+        // my own solution:
+        /*
         // use stack to handle adjacent letters;
         Deque<Character> stack = new LinkedList<>();
         for(int i = 0 ; i < s.length() ; i ++){
@@ -13,5 +16,24 @@ class Solution {
             sb.insert(0, stack.pop());
         }
         return sb.toString();
+        */
+        
+        // from camp: fast slow approach:
+        char[] ch = s.toCharArray();
+        int fast = 0;
+        int slow = 0;
+        while(fast < s.length()){
+            // 直接用fast指针覆盖slow指针的值
+            ch[slow] = ch[fast];
+            // 遇到前后相同值的，就跳过，即slow指针后退一步，下次循环就可以直接被覆盖掉了
+            if(slow > 0 && ch[slow] == ch[slow - 1]){
+                slow--;
+            }else{
+                slow++;
+            }
+            fast++;
+        }
+        return new String(ch,0,slow);
+        
     }
 }

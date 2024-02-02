cf35a4fa79b840a34e3e7af40c06e2c3ee45b87f
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-02 22:29:06 +0000

    Time: 13 ms (80.47%), Space: 45.3 MB (59.04%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,8 +1,25 @@
 class Solution {
 
     public String removeDuplicates(String s) {
+        // from camp:
+        StringBuffer res = new StringBuffer();
+        // top为 res 的长度
+        int top = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // 当 top > 0,即栈中有字符时，当前字符如果和栈中字符相等，弹出栈顶字符，同时 top--
+            if (top >= 0 && res.charAt(top) == c) {
+                res.deleteCharAt(top);
+                top--;
+            // 否则，将该字符 入栈，同时top++
+            } else {
+                res.append(c);
+                top++;
+            }
+        }
+        return res.toString();
         // my own solution:
-        // /*
+        /*
         // use stack to handle adjacent letters;
         Deque<Character> stack = new LinkedList<>();
         char[] arr = s.toCharArray();
@@ -17,7 +34,7 @@ class Solution {
             sb.insert(0, stack.pop());
         }
         return sb.toString();
-        // */
+        */
         
         // from camp: fast slow approach:
         /*

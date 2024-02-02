16b70911175a286d15ba7989003097ae0a248a97
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-02 23:01:05 +0000

    Time: 6 ms (74.52%), Space: 44.2 MB (84.00%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,5 +1,30 @@
 class Solution {
     public int evalRPN(String[] tokens) {
+        // use stack, each time we came across non number,  pop() twice;
+        // or we push it;
+        if(tokens == null || tokens.length == 0) return 0;
+        Deque<Integer> stack = new LinkedList<>();
+        String operators = "+-*/";
+        for(int i = 0 ; i < tokens.length ; i ++){
+            if(operators.contains(tokens[i])){
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                if(tokens[i].equals("+")) stack.push(num1 + num2);
+                if(tokens[i].equals("-")) stack.push(num1 - num2);
+                if(tokens[i].equals("*")) stack.push(num1 * num2);
+                if(tokens[i].equals("/")) stack.push(num1 / num2);
+            }
+            else stack.push(Integer.valueOf(tokens[i]));              
+        }
+        return stack.pop();
+                                                 
+        
+        
+        
+        
+        
+        
+        
         // analyze:
         // traverse:
         // 1) if number, push to stack;
@@ -42,9 +67,10 @@ class Solution {
         }
         return res;   
         */
+        /* before:
         if(tokens == null || tokens.length == 0) return 0;
         Deque<Integer> stack = new LinkedList<>();
-        String operators = "+-*/";
+        String operators = "+-* /";
         for(int i = 0;i< tokens.length;i++){
             if(operators.contains(tokens[i])){
                 int num1 = stack.pop();
@@ -56,5 +82,6 @@ class Solution {
             }else stack.push(Integer.valueOf(tokens[i]));
         }
         return stack.peek();
+        */
     }
 }

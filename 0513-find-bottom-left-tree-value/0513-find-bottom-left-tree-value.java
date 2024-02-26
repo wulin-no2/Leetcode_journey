58f1b3f1a8e970cd30816acea4b48cad50291834
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-26 16:10:24 +0000

    Time: 0 ms (100.00%), Space: 43.1 MB (83.29%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -15,7 +15,10 @@
  */
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
+        dfs(root,1);
+        return res;
         // remember the leftmost of each layer, update the value when level-order traverse;
+        /* first approach: DFS
         Deque<TreeNode> q = new LinkedList<>();
         q.offer(root);
         int res = root.val;
@@ -31,5 +34,30 @@ class Solution {
         }
         
         return res;
+        */
+    }
+    // 2 approach: DFS-recursively. find the maxDepth, then left child first
+    // record the first child of the maxDepth level.
+    int maxDepth = Integer.MIN_VALUE;
+    int res;
+    private void dfs(TreeNode root, int depth){
+        // when should we end? find the leaf:
+        if(root.left==null && root.right==null){
+            if(depth > maxDepth){
+                maxDepth = depth;
+                res = root.val; 
+            }
+            return;
+        }
+        if(root.left!=null){
+            depth++;
+            dfs(root.left, depth);
+            depth--;
+        }
+        if(root.right!=null){
+            depth++;
+            dfs(root.right, depth);
+            depth--;
+        }
     }
 }

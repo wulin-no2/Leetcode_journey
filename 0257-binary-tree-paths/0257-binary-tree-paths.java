91e830352e21e2ca4a9ca87cc186dc6168dda2d5
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-26 17:38:57 +0000

    Time: 1 ms (99.40%), Space: 42.2 MB (90.67%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -21,9 +21,20 @@ class Solution {
         return res;
         
     }
+    /*
     private void dfs(TreeNode node, String s, List res){
         if(s.equals("")) s = s + node.val;
         else s = s + "->" + node.val;
+        if(node.left==null && node.right==null) {res.add(s); return;}
+        if(node.left!=null){dfs(node.left, s, res);}
+        if(node.right!=null){dfs(node.right, s, res);}
+    }
+    */
+    
+    // we can use StringBuilder() to make it faster;
+    private void dfs(TreeNode node, String s, List res){
+        if(s.equals("")) s = new StringBuilder(s).append(node.val).toString();
+        else s = new StringBuilder(s).append("->").append(node.val).toString(); 
         if(node.left==null && node.right==null) {res.add(s); return;}
         if(node.left!=null){dfs(node.left, s, res);}
         if(node.right!=null){dfs(node.right, s, res);}

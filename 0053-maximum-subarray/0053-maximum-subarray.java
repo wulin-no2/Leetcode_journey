09a2c825cad9b6429998d39d4fb6bac238c6d018
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-28 20:55:40 +0000

    Time: 1 ms (99.63%), Space: 57.1 MB (35.94%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -37,15 +37,22 @@ class Solution {
         
         // there are other 2 approaches;
         // Dynamic Programming
-        int storage []=  new int[nums.length];
+        /*
+        int maxArray []=  new int[nums.length];
         int max  =  nums[0];
-        storage[0] =  nums[0];
+        maxArray[0] =  nums[0];
         for(int i = 1; i < nums.length; i++){
-           storage[i] = Math.max(storage[i-1] + nums[i], nums[i]);
-           if(storage[i] > max){
-               max = storage[i];
-           }
+           maxArray[i] = Math.max(storage[i-1] + nums[i], nums[i]);
+           max = Math.max(maxArray[i], max);
         }
-        return max;     
+        return max;   
+        */
+        int max = nums[0];
+        int maxCur = max;
+        for(int i = 1; i < nums.length; i++){
+            maxCur = Math.max(maxCur + nums[i], nums[i]);
+            max = Math.max(maxCur, max);
+        }
+        return max;
     }
 }

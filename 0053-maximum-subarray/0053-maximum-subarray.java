ed286e47be10a9fe9dc09e7ea3273b32723145a1
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-28 20:58:12 +0000

    Time: 1 ms (99.63%), Space: 57 MB (48.90%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -42,7 +42,7 @@ class Solution {
         int max  =  nums[0];
         maxArray[0] =  nums[0];
         for(int i = 1; i < nums.length; i++){
-           maxArray[i] = Math.max(storage[i-1] + nums[i], nums[i]);
+           maxArray[i] = Math.max(maxArray[i-1] + nums[i], nums[i]);
            max = Math.max(maxArray[i], max);
         }
         return max;   

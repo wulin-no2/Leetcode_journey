f0de1ff6b4171c075f2bc60562901402d143a55a
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-29 19:47:15 +0000

    Time: 3 ms (88.24%), Space: 45.2 MB (82.81%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -25,37 +25,25 @@ class Solution {
         return res;
         */
         // another 1pass approach from discuss, don't understand:
-        if(ratings == null || ratings.length == 0) return 0;
-        int n = ratings.length;
-        int i = 0;
-        int s = 0; //record the start index of the left and right sides of the peaks
-        int sum = 1; //initialize as 1 because in each round we minus 1 for the left bottom 
-                   //since it was calculated once in the previous round
-        while(i < n-1){
-            //calculate the length from bottom -> peak on the left side of peak point, peak not included
-            while(i < n-1 && ratings[i+1] > ratings[i]){
-                i++;
-            }
-            int left = i-s;
-            s = i;
-            //calculate the length from peak-> bottom on the right side of peak point, peak not included
-            while(i < n-1 && ratings[i+1] < ratings[i]){
-                i++;
-            }
-            int right = i-s;
-            s = i;
-            //calculate the candies that needs to be assign to the peak
-            int max = Math.max(left, right)+1;
-            //total candies needed for current bottom->peak->bottom sub array
-            sum += (1+left)*left/2+(1+right)*right/2+max-1;
-            //skip points that have same value, increase i and final result by 1
-            while(i < n-1 && ratings[i+1] == ratings[i]){
-                i++;
-                sum++;
-            }
-            s = i;
+        if (ratings == null || ratings.length == 0) return 0;
+        int total = 1, prev = 1, countDown = 0;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] >= ratings[i-1]) {
+                if (countDown > 0) {
+                    total += countDown*(countDown+1)/2; // arithmetic progression
+                    if (countDown >= prev) total += countDown - prev + 1;
+                    countDown = 0;
+                    prev = 1;
+                }
+                prev = ratings[i] == ratings[i-1] ? 1 : prev+1;
+                total += prev;
+            } else countDown++;
         }
-        
-        return sum;
+        if (countDown > 0) { // if we were descending at the end
+            total += countDown*(countDown+1)/2;
+            if (countDown >= prev) total += countDown - prev + 1;
+        }
+        return total;
+
     }
 }

5ce6c9bd619cdee1627f44724df8348f1763cfec
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-29 20:16:14 +0000

    Time: 3 ms (23.13%), Space: 45.1 MB (18.24%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        /* DIM: 
+        // DIM: 
         if(nums.length == 0) return null;
         // find maxIndex:
         int maxIndex = 0;
@@ -31,26 +31,6 @@ class Solution {
         root.left = constructMaximumBinaryTree(leftNums); 
         root.right = constructMaximumBinaryTree(rightNums); 
         return root;
-        */
-        // from GPT:
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        for (int num : nums) {
-            TreeNode curr = new TreeNode(num);
-            while (!stack.isEmpty() && stack.peek().val < num) {
-                // Current node is the parent of the last node in the stack
-                curr.left = stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                // The last node in the stack is the parent of the current node
-                stack.peek().right = curr;
-            }
-            stack.push(curr);
-        }
-        // The bottom of the stack holds the root of the Maximum Binary Tree
-        TreeNode root = null;
-        while (!stack.isEmpty()) {
-            root = stack.pop(); // The last element is the root
-        }
-        return root;
+        
     }
 }

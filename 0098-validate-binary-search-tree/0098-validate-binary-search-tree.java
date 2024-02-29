7c86d361b2b1d07acd491013b857afac1a72ad2f
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-29 20:53:43 +0000

    Time: 1 ms (25.50%), Space: 43.3 MB (66.35%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -46,7 +46,7 @@ class Solution {
         return p.val;
     }
     */
-    /* an inOrder soluton:
+    // an inOrder soluton:
 public boolean isValidBST(TreeNode root) {
    if (root == null) return true;
    Stack<TreeNode> stack = new Stack<>();
@@ -63,9 +63,9 @@ public boolean isValidBST(TreeNode root) {
    }
    return true;
 }
-*/
+
     // an concise and clean version same with me:
-    
+    /*
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
@@ -75,7 +75,7 @@ public boolean isValidBST(TreeNode root) {
         if (root.val >= maxVal || root.val <= minVal) return false;
         return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
-   
+   */
     
     
     

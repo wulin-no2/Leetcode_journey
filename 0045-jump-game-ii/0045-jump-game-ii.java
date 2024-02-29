6bfd38889c78fb69d64d3c5e4fe0998161c4fa8c
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-29 00:00:21 +0000

    Time: 2 ms (78.36%), Space: 45.2 MB (40.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,28 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
-        // for each position, record the smallest count we reach here, and the furtherest index we can reach, until furtherest >= length - 1;
-        /*
-        if(nums.length==1) return 0;
-        int count = 1;
-        int furtherest = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            if(i == furtherest) count++;
-            furtherest = Math.max(furtherest, nums[i] + i); 
-        }
-        return count;
-        */
-    
-        
+        // for each position, record the smallest count we reach here, and the far index we can reach;
+
         int count = 0;
-        int curFar = 0;
-        int nexFar = 0;
-        for (int i = 0; i <= curFar && curFar < nums.length - 1; i++) {
-            nexFar  = Math.max(nexFar , i + nums[i]);
-            // 可达位置的改变次数就是跳跃次数
-            if (i == curFar) {
-                curFar = nexFar ;
-                count++;
-            }
+        int preFar = 0;
+        int far = 0;
+        for (int i = 0; i <= preFar && preFar < nums.length - 1; i++) {
+            far = Math.max(far , i + nums[i]);
+            // if index already reach the boundary of preFar, update preFar and count;
+            if (i == preFar) {preFar = far; count++;}
         }
         return count;
     }

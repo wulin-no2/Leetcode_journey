4892562a1eeeab8764422bf5e22dd823ac8fd154
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-02-29 18:04:01 +0000

    Time: 3 ms (82.31%), Space: 41.9 MB (95.96%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
@@ -2,17 +2,15 @@ class Solution {
     public int largestSumAfterKNegations(int[] nums, int k) {
         // 1) for all the negative values, choose it from small to big
         // 2) if all the negative values are done. see the remaining k is even or odd; if it's even, return sum; if it's odd, subtract the smallest value and return;
-        
         Arrays.sort(nums);
-        int i = 0;
-        while(k > 0 && i < nums.length && nums[i] < 0) {nums[i] = -nums[i]; i++; k--;}
-        if(k == 0) return sumArray(nums);
-        if(k % 2 == 0) return sumArray(nums);
-        else {Arrays.sort(nums); return sumArray(nums) - nums[0]*2;}
-    }
-    private int sumArray(int[] nums){
+        int smallest = 101;
+        for (int i = 0; i < nums.length && k > 0; i++) {
+            if (nums[i] < 0) { nums[i] = -nums[i];k--;}
+            smallest = Math.min(smallest, nums[i]);
+        }
         int sum = 0;
-        for(int i = 0; i<nums.length; i++) sum += nums[i];
-        return sum;
+        for (int num : nums) sum += num;
+        if (k % 2 == 1) sum -= 2 * smallest;
+        return sum;   
     }
 }

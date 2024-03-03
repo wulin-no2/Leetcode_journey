887b9b66f586ee467acb38fd9562598c0e1a769e
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-03 00:09:33 +0000

    Time: 2 ms (97.52%), Space: 55.9 MB (47.40%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
         //use map to trace money we have;
+        /*
         Map<Integer, Integer> map = new HashMap<>();
         map.put(5, 0);
         map.put(10, 0);
@@ -24,6 +25,23 @@ class Solution {
                 else if(map.get(10) <= 0 && map.get(5) >= 3){
                     map.put(5, map.get(5)-3);
                 }
+                else return false;
+            }
+        }  
+        return true;
+        */
+        // now use 2 int to trace;
+        int five = 0;
+        int ten = 0;
+        for(int i : bills){
+            if(i == 5) five++;
+            else if(i == 10){
+                if(five <= 0) return false;
+                else {five--;ten++;}
+            }
+            else{
+                if(five >= 1 && ten >= 1) {five--;ten--;}
+                else if(ten <= 0 && five >= 3) five -= 3;
                 else return false;
             }
         }  

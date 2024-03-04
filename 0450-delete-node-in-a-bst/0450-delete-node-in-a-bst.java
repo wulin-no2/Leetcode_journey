f540499f051665f4996e9e2b2f97510e21b580f9
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-04 00:41:44 +0000

    Time: 0 ms (100.00%), Space: 45.7 MB (78.44%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return root;
+        if(key < root.val) root.left = deleteNode(root.left, key);
+        else if(key > root.val) root.right = deleteNode(root.right, key);
+        else if(key==root.val) {
+            // choose the biggest node from left or the smallest node from rght;
+            // reconstruct;
+            if(root.left==null && root.right==null) return null;
+            if(root.left!=null){
+                // find biggest element of left as root;
+                TreeNode p = findBiggest(root.left);
+                p.left = deleteNode(root.left, p.val);
+                p.right = root.right;
+                root = p; 
+            }
+            else {
+                TreeNode p = findSmallest(root.right);
+                p.right = deleteNode(root.right, p.val);
+                p.left = root.left;
+                root = p; 
+            }
+        }
+        return root;
+    }
+    private TreeNode findBiggest(TreeNode node){
+        TreeNode p = node;
+        while(p.right!=null){
+            p = p.right;
+        }
+        return p;
+    } 
+    private TreeNode findSmallest(TreeNode node){
+        TreeNode p = node;
+        while(p.left!=null){
+            p = p.left;
+        }
+        return p;  
+    } 
+}

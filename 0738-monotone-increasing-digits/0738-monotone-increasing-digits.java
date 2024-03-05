5971ae910ffa223360580c5fc92282274717c8f6
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-05 18:58:37 +0000

    Time: 1 ms (82.48%), Space: 40.4 MB (78.47%) - LeetHub

---
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int monotoneIncreasingDigits(int n) {
+        // the rule is, if we need to borrow 1 from this digit, then the following digits after that should all be 9; it should decrease 1;
+        if(n < 10) return n;
+        String s = n + "";
+        char[] chars = s.toCharArray();
+        for(int i = chars.length - 1; i >= 1 ; i--){
+            if(chars[i - 1] > chars[i]) {
+                chars[i - 1] = (char)(chars[i - 1] - 1);
+                // set all the digit be 9;
+                int temp = i;
+                while(temp < chars.length){
+                    chars[temp]='9';
+                    temp++;
+                }
+            }
+            if(chars[i] == '0') {
+                // set all the digit after be 9;
+                int temp = i;
+                while(temp < chars.length){
+                    chars[temp] = '9';
+                    temp++;
+                }
+            }
+        }
+        return Integer.parseInt(new String(chars));
+    }
+}

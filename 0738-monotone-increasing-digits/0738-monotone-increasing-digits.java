175eae048421b7caa6d2a49d9c06d0f09a85e27a
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-05 19:11:49 +0000

    Time: 1 ms (82.48%), Space: 40.4 MB (63.50%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
@@ -6,9 +6,8 @@ class Solution {
         char[] chars = s.toCharArray();
         for(int i = chars.length - 1; i >= 1 ; i--){
             if(chars[i] == '0' || chars[i - 1] > chars[i]) {
-                if(chars[i - 1] > chars[i]) {
-                    chars[i - 1] = (char)(chars[i - 1] - 1);
-                }
+                if(chars[i - 1] > chars[i]) chars[i - 1] = (char)(chars[i - 1] - 1);
+                
                 // set all the digit after be 9;
                 int temp = i;
                 while(temp < chars.length){

8d6e4dbcafd8173fbfda2f1f294598b293ef1fda
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-05 00:01:38 +0000

    Time: 4 ms (92.66%), Space: 42 MB (72.33%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        // partition them in one-element parts;
-        // then merge;
         List<Integer> res = new ArrayList<>();
         int[] nums = new int[26]; // used to record which position each letter is in
         char[] chars = s.toCharArray();
+        //first pass: find the latest position of each letter
         for(int i = 0 ; i < s.length() ; i++){
-            nums[chars[i] - 'a'] = i;// update the latest position of each letter
+            nums[chars[i] - 'a'] = i;
         }
-        // now we have latest position of each letter;
-        // partition intervals;
+        // second pass:
+        // partition intervals;According to the largest position of current interval
         int index = 0;
         int last = -1; // end of last interval;
         for(int i = 0 ; i < s.length() ; i++){
-            index = Math.max(index, nums[chars[i] - 'a']); // find largest position of current interval
+            index = Math.max(index, nums[chars[i] - 'a']); 
+                // if i equals largest position of current interval, split it;
             if(i == index){
                 res.add(i - last);
                 last = i;
             }
         }
         return res;
+
     }
 }

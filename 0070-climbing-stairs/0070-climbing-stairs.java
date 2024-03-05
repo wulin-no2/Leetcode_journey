910b16cbefe79d61b3ea503bb6fe55d2de1b3086
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-05 21:00:22 +0000

    Time: 0 ms (100.00%), Space: 40.2 MB (56.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,5 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
+        // dp[n] = dp[n - 1] + d[n - 2];
+        if(n<=2) return n;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+        
         // we can recursive it.
         // it's great but it's exponential time complexity. performance is bad;
         /*
@@ -26,10 +36,12 @@ class Solution {
         
         // approach2:memorization, time and space complexityO(n)
         // use a map to memorize computed results for each step n
+        /*
         HashMap<Integer, Integer> map = new HashMap<>();
         if(n <= 3) return n;
         for(int i = 0; i <= 3; i++) map.put(i,i);
         for(int i = 4; i <= n; i++) map.put(i, map.get(i-1) + map.get(i - 2));
         return map.get(n);
+        */
     }
 }

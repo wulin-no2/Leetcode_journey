623ab013ffa03013d2152befddb951cadb36aaae
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-05 00:10:18 +0000

    Time: 9 ms (56.84%), Space: 46.2 MB (82.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,23 +5,16 @@ class Solution {
         // else, compare their end, set the lager end as now end; pop one and push one;
         // till the end;
         // maybe we don't need to sort them, just compare? No we have to sort them. or it will be complicated;
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            public int compare(int[] a, int[] b){
-                return Integer.compare(a[0], b[0]);
-            }
-        });
-        int len = intervals.length;
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0], b[0]));
         // use arrayList to add new intervals; more flexible;
         List<int[]> list = new ArrayList<>();
         list.add(intervals[0]);
-        for(int i = 1; i < len; i++){
+        for(int i = 1; i < intervals.length; i++){
             int[] last = list.get(list.size() - 1);
+            // not overlap, add to list:
             if(intervals[i][0] > last[1]) list.add(intervals[i]);
-            else{
-                last[1] = Math.max(intervals[i][1], last[1]);
-                // list.remove(list.size() - 1);
-                // list.add(new int[]{Math.min(intervals[i][0],last[0]), Math.max(intervals[i][1], last[1])});
-            }
+            // overlap, merge:
+            else last[1] = Math.max(intervals[i][1], last[1]);
         }
         // now we get list. how to transfer list to Array;
         return list.toArray(new int[list.size()][2]);

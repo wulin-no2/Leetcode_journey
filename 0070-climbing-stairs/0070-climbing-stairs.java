0d1741b35a22c66d408bb83d346c9ef411a12708
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-05 21:02:43 +0000

    Time: 0 ms (100.00%), Space: 40.1 MB (70.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         // dp[n] = dp[n - 1] + d[n - 2];
-        if(n<=2) return n;
+        /*
         int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
@@ -9,31 +9,19 @@ class Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
-        
-        // we can recursive it.
-        // it's great but it's exponential time complexity. performance is bad;
-        /*
-        if(n <= 3){
-            return n;
-        }
-        return climbStairs(n-1) + climbStairs(n-2);
         */
-        
-        // find a way to avoid recursive;
-        /* approach1: dynamic programming, time and space complexityO(n)
-        if(n <= 3){
-            return n;
+        if(n == 1) return n;
+        int a = 1;
+        int b = 2;
+        for(int i = 3; i <= n; i++){
+            int res = a + b;
+            a = b;
+            b = res;
         }
-        // dynamic programming;
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
-        */
+        return b;
         
+        
+
         // approach2:memorization, time and space complexityO(n)
         // use a map to memorize computed results for each step n
         /*

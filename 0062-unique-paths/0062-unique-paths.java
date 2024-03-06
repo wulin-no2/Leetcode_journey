83cb4b369fe2fb71946194b2cb3d955780ebf9a3
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-06 20:25:05 +0000

    Time: 0 ms (100.00%), Space: 41 MB (6.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -20,33 +20,8 @@ class Solution {
         }
         
         return dp[m-1][n-1];
-        
-        // from 19:50
-        // find dp logic; dp[m][n] means how many unique ways can we find for grid[m][n]
-        // dp[m][n] vs dp[m][n - 1]: 
-        // dp[m][n] = dp[m][n - 1] + dp[m - 1][n];
-        // we assume move right = 1,move down = 0;
-        // then each step should 0 or 1;
-        // the whole step = m - 1 + n - 1; We can use combinational approach; go m - 1's 1;
-        
-        /* O(N2) O(N2)
-        int[][] dp = new int[m][n];
-        
-        // watch the base case!!!
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
-        }
-        for(int i = 1; i < m ; i++){
-            for(int j = 1; j < n; j++){
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-            }
-        }   
-        return dp[m - 1][n - 1];
     
-        */
+        
         // another solution. why?
         /*
         long ans = 1;

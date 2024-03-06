1a1b450a5c667fb73441ed35861eed8f232157cb
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-06 20:23:57 +0000

    Time: 0 ms (100.00%), Space: 40.4 MB (39.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,5 +1,26 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        // dp again;
+        // dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        // dp[i][j] means how many unique paths from [0][0] to [i][j];
+        int[][] dp = new int[m][n];
+        // base case:
+        for(int j = 0; j < n ; j++){
+            dp[0][j] = 1;
+        }
+        for(int i = 0; i < m ; i++){
+            dp[i][0] = 1;
+        }
+        
+        // for loop order:
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
+        
         // from 19:50
         // find dp logic; dp[m][n] means how many unique ways can we find for grid[m][n]
         // dp[m][n] vs dp[m][n - 1]: 
@@ -27,10 +48,12 @@ class Solution {
     
         */
         // another solution. why?
+        /*
         long ans = 1;
         for (int i = 1; i <= m - 1; i++) {
             ans = ans * (n - 1 + i) / i;
         }
         return (int)ans;
+        */
     }
 }

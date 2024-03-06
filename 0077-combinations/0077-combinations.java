e2e190f3d9152aaa722406e5adb783ec666153d4
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-06 19:19:58 +0000

    Time: 15 ms (88.61%), Space: 94.4 MB (61.11%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -6,13 +6,15 @@ class Solution {
         return res;
     }
     private void backtracking(int n, int k, int start){
-        
+        // end conditon:
         if(path.size() == k){
+            // get result:
             res.add(new ArrayList<>(path));
             return;
         }
+        // each level logic:
         for(int i = start; i <= n ; i++){
-            if( n - i + 1 < (k - path.size())) return;
+            if( n - i + 1 < (k - path.size())) return; // trim;
             path.add(i);
             backtracking(n, k, i + 1);
             path.remove(path.size() - 1);

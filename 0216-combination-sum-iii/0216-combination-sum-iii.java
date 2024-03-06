b19c20fd9c3026698f13e80e1be33ead3e64cb20
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-06 19:39:16 +0000

    Time: 0 ms (100.00%), Space: 40.8 MB (79.03%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -8,12 +8,14 @@ class Solution {
     }
     void backtracking(int k, int target, int start, int end){
         // end condition:
-        if(path.size() == k && target == 0){
-            res.add(new ArrayList<>(path));
+        if(path.size() == k){
+            if(target == 0) res.add(new ArrayList<>(path));
             return;
         }
         // each level:
         for(int i = start ; i <= end ; i++){
+            // trim:
+            if(end - start + 1 < k - path.size()) return;
             path.add(i);
             backtracking(k, target - i, i + 1, end);
             path.remove(path.size() - 1);

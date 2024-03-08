5135edd5407af09fbb472b18db0a99cdf947a2d1
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-08 01:37:42 +0000

    Time: 4 ms (45.17%), Space: 42.3 MB (20.78%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,33 +1,32 @@
 class Solution {
-    /*
+    
     public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
         String path = "";
         String[] s = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        backtracking(digits, s, 0, digits.length() - 1, res, path);
+        backtracking(digits, s, 0, res, path);
         return res;
-        
     }
-    void backtracking(String digits, String[] s, int start, int end, List<String> res, String path){
+    void backtracking(String digits, String[] s, int index, List<String> res, String path){
         // end condition:
+        if(digits.length() == 0) return;
         if(path.length() == digits.length()){
             // get result;
             res.add(path);
             return;
         }
         // each level:
-        for(int i = start ; i < digits.length() ; i++){
-            int num = (int)digits.charAt(i);
-            for(int j = 0 ; j < s[num - 2].length(); j++){
-                path = path + s[num - 2].charAt(j);
-                backtracking(digits, s, i + 1, end, res, path);
-                path = path.substring(0, path.length() - 1);
-            }  
-        }
+        int num = digits.charAt(index) - '0';
+        for(int j = 0 ; j < s[num - 2].length(); j++){
+            path = path + s[num - 2].charAt(j);
+            backtracking(digits, s, index + 1, res, path);
+            path = path.substring(0, path.length() - 1);
+        }  
     }
-*/
-    // do backtracking again;
     
+
+    // do backtracking again;
+    /*
     public List<String> letterCombinations(String digits) {
         // first base case:
         if(digits.length() == 0) return new ArrayList();
@@ -50,7 +49,7 @@ class Solution {
             }
         }
     }
-    
+    */
 
     // another approach:
     /*

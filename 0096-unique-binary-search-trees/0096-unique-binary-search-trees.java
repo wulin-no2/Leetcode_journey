4fd2bb2c4b74d4da4eb62463b3b9099c5b4aef4b
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-08 03:28:11 +0000

    Time: 0 ms (100.00%), Space: 40.3 MB (51.61%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,11 +1,5 @@
 class Solution {
     public int numTrees(int n) {
-        //dp[i] = dp[i - 1] + dp[i - 1] + dp[i - 2];
-        // which means;
-        // n node as the root: dp[n - 1];
-        // n node as the lower right corner / leaf: dp[n - 1];
-        // n node at layer higher than leaf: dp[n - 2];
-        //       which equals n - 1 node at lower right corner;
         if(n < 3) return n;
         int[] dp = new int[n + 1];
         dp[0] = 1;

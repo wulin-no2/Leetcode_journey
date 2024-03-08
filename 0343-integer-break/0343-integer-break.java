7654ae95660e8d54a5c21cc0eb86474a1ec0dda7
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-08 02:38:38 +0000

    Time: 1 ms (45.15%), Space: 40.2 MB (70.80%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -11,12 +11,25 @@ class Solution {
         // 10 = 3 * 3 * 4
         // 11 = 3 * 3 * 3 * 2
         // basically, we only need multile 3s, then one 2 or one 4
+        /*
         if(n < 4) return n - 1;
         int exp = n / 3;
         int remain = n % 3;
         if(remain == 2) return (int)Math.pow(3, exp) * 2;
         else if(remain == 1) return (int)Math.pow(3, exp - 1) * 4;
         else return (int)Math.pow(3, exp);
+        */
+        //DP solution:
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for(int i = 3; i <= n; i++) {
+            for(int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+        
+        
         
     }
 }

b3bbdf2a5cae6eb82673340575e2f6c22950599c
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-03-09 01:52:42 +0000

    Time: 2 ms (74.30%), Space: 42.9 MB (83.79%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
+        /*
         // the last digit must be 1 since it's odd;
         // then make remaining 1s as left as possible;
         // 2 pointers;
@@ -17,5 +18,23 @@ class Solution {
         chars[p - 1] = '0';
         chars[chars.length - 1] = '1';
         return new String(chars);
+        */
+        char[] arr = s.toCharArray();
+        int N = arr.length;
+        
+        Arrays.sort(arr);
+
+        // Reverse order for the first N - 1 elements of the array
+        // Because we want to keep a 1 at the last index
+        // The last element of the array is index N - 1, and the second to last element is at N - 2
+        int secondLast = N - 2;
+        for (int i = 0; i < N / 2; i++) {
+            char temp = arr[i];
+            arr[i] = arr[secondLast - i];
+            arr[secondLast - i] = temp;
+        }
+
+        // Return result
+        return new String(arr);
     }
 }

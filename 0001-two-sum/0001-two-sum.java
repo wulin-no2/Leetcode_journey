d3c8cc9a60bd7a17727906d53cdf892ef8e46d4f
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-05-15 15:23:31 +0100

    Time: 2 ms (97.61%), Space: 44.7 MB (69.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -11,13 +11,26 @@ class Solution {
         
         // find 1 elements by index(array, map)
         // solution 1: brute force
+        /*
         for(int i = 0; i < nums.length - 1; i++){
             int temp = target - nums[i];
             for(int j = i + 1; j < nums.length; j++){
                 if(nums[j]==temp) return new int[]{i,j};
             }
         }
-        return null;
+        return null;*/
+        // solution 2: use map to remember the elements as key, put the index as value;
+        // then we can get it in O(1) time;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < nums.length; i++){
+            int complement = target - nums[i];
+            // check if there is a key equals nums[i]
+            // if exists, return result
+            if(map.containsKey(nums[i])) return new int[]{i,map.get(nums[i])};
+            // if not, put it in map;
+            map.put(complement, i);
+        }
+        return new int[0];
     }
 }      
         

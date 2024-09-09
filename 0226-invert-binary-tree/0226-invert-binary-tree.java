d445314598a1c7344857d55141b67916df1a3242
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-09 19:29:57 +0100

    Time: 0 ms (100.00%), Space: 41.1 MB (35.24%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,77 +15,21 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        // 0208
-        // recursive first: pre-order or post-order
-        /*
-        if(root == null) return root;
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-        invertTree(root.left);
-        invertTree(root.right);
-        return root;
-        */
-        // irerative: level order
-        if(root == null) return root;
-        Deque<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            while(size -- > 0){
-                TreeNode temp = q.poll();
-                if(temp.left != null) q.offer(temp.left);
-                if(temp.right != null) q.offer(temp.right);
-                TreeNode temp1 = temp.right;
-                temp.right = temp.left;
-                temp.left = temp1;
-            }
-            
-        }
-        return root;
-        
-        
-        
-        
-        
-        // use recursive approach:
-        /* Time Complexity : O(n), Space Complexity : O(n)
-        if(root == null){
-            return root;
-        }
-        TreeNode temp = root.left;
-        root.left = invertTree(root.right);
-        root.right = invertTree(temp);
-        return root;
-        */
-        
-        // use iterative approach:
-        // it's similar to levelOrder, use linkedlist to create a queue;
-        // analyze:
-        /*
-        LinkedList<TreeNode> q = new LinkedList<>();
-        // base case:
-        if(root == null){
-            return root;
-        }
-        q.add(root);
-        // loop until empty;
-        while(!q.isEmpty()){
-            // get a node from front;
-            TreeNode temp = q.poll();
-            // see if it has children, enqueue it;
-            if(temp.left != null){
-                q.add(temp.left);
-            }
-            if(temp.right != null){
-                q.add(temp.right);
-            }       
-            // swap them;
-            TreeNode p = temp.left;
-            temp.left = temp.right;
-            temp.right = p;
-        }
-        return root;  
-        */
+        // post order. invert children, then pass it to the parent node;
+        TreeNode result = traversal(root);
+        return result;
+    }
+    public TreeNode traversal(TreeNode node){
+        //base case:
+        if(node==null) return node;
+        if(node.left==null && node.right==null) return node;
+        // left
+        TreeNode leftTree = traversal(node.right);
+        // right
+        TreeNode rightTree = traversal(node.left);
+        node.left = leftTree;
+        node.right = rightTree;
+        // node
+        return node;
     }
 }

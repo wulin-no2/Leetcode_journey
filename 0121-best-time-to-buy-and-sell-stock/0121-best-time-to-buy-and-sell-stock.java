ce607f92a460133448a4c3a7c07608fd2ebad0cb
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-09 18:49:01 +0100

    Time: 27 ms (7.20%), Space: 57.5 MB (97.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,24 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // analyze:
-        // Everyday, find the minPrice and the maxResult.
-        if(prices.length <= 1){
-            return 0;
+        // dynamic programming
+        // dp[j][0] jth day we don't hold a stock, how much we have;
+        // dp[j][1] jth day we hold a stock, how much we have;
+        // dp[j][0] = max{dp[j - 1][0], dp[j - 1][1] + prices[j]};
+        // dp[j][1] = max{dp[j - 1][1], dp[j - 1][0] - prices[j]};
+        // int result = max{dp[j][0], dp[j][1]};
+        // we need dp[prices.size()-1][0];
+        int length = prices.length;
+        int[][] dp = new int[length][2];
+        
+        // initialize it:
+        dp[0][0]=0;
+        dp[0][1]= -prices[0];
+        
+        //calculate:
+        for(int j = 1; j < length; j++){
+            dp[j][0] = Math.max(dp[j - 1][0], dp[j - 1][1] + prices[j]);
+            dp[j][1] = Math.max(dp[j - 1][1], - prices[j]);
         }
-        int maxResult = 0;
-        int minPrice = prices[0];
-        for(int i = 0; i < prices.length; i++){
-            // if(prices[i] < minPrice){
-            //     minPrice = prices[i];
-            // }
-            // replace the if with Math.min
-            minPrice = Math.min(prices[i],minPrice);
-            // if(prices[i] - minPrice > maxResult){
-            //     maxResult = prices[i] - minPrice;   
-            // }
-            // replace the if with Math.max
-            maxResult = Math.max(prices[i]-minPrice, maxResult);
-        }
-        return maxResult;
+        return dp[length-1][0];
     }
 }

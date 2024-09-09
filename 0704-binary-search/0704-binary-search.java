53ff6d92ed3df545a9355c71107d4b0988a659c7
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-09 19:55:16 +0100

    Time: 0 ms (100.00%), Space: 46.3 MB (8.75%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,22 +1,15 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // analyze:
-        // binary search:
-        // use 3 pointers: low, high, mid;
-        // change low and high, check mid;
-        int low = 0;
-        int high = nums.length - 1;
-        while(low < high){
-            int mid = (low + high) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[mid] < target){
-                low = mid + 1;
-            }else{
-                high = mid - 1;
-            }
+        int right = nums.length - 1; 
+        int left = 0;
+        
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(target < nums[mid]) right = mid - 1;
+            else if(target > nums[mid]) left = mid + 1;
+            else return mid;
         }
-        if(nums[low] == target) return low;
-        return -1;   
+        return -1;
+        
     }
 }

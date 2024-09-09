a82bf6f3ab9b51130b674f5677789ec8367eb9b3
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-09 18:03:32 +0100

    Time: 1 ms (98.67%), Space: 41.6 MB (25.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,78 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        // use stack to push() left brackets's pair right brackets, once we have right brackets, pop() and compare;
-        Deque<Character> stack = new LinkedList<>();
-        for(int i = 0; i < s.length() ; i ++){
-            if(s.charAt(i) == '(') stack.push(')');
-            else if(s.charAt(i) == '[') stack.push(']');
-            else if(s.charAt(i) == '{') stack.push('}');
-            else if(!stack.isEmpty()){
-                Character c = stack.pop();
-                if(c.equals(s.charAt(i))) continue;
-                else return false;
-            }
-            else return false;
-        }
-        //if(!stack.isEmpty()) return false;
-        return stack.isEmpty();
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /* this was the first time I did this problem and I couldn't solve it.
-        if(s.charAt(0) == ')'|| s.charAt(0)  == ']'||s.charAt(0) == '}'){
-            return false;
-        }
-        for(int i = 0 ; i < s.length(); i ++){
-            if ((s.charAt(i) == '(' && s.charAt(i + 1)!= ')') ||
-                (s.charAt(i) == '[' && s.charAt(i + 1)!= ']') ||
-                (s.charAt(i) == '{' && s.charAt(i + 1)!= '}')){
-                return false;
-            }
-        }
-        return true;
-        */
-        
-        // this is the second time I do it at 18:01, Jan 5th, 2024;
-        
-        // analyze:
-        // push the elements of the string into a stack if it's an open bracket;
-        // pop an elements and check if they are a pair if the next is a close bracket;
-        // if it's not a pair, return false;
-        // if the stack is empty and the string is traversed, return true;
-        /* my solution before:
-        Stack stack = new Stack();
-        int n = s.length();
-        int i = 0;
-        while(i < n){
-            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[' ){
-                stack.push(s.charAt(i));
-                i++;
-            }
-            else{
-                if(!stack.isEmpty()){
-                    // check if they are a pair:
-                    char c =(char)stack.pop();
-                    if((c == '(' && s.charAt(i) == ')') ||
-                       (c == '[' && s.charAt(i) == ']') ||
-                       (c == '{' && s.charAt(i) == '}')
-                       ){
-                        i++;
-                    } else{
-                        return false;
-                    }
-                }else{
-                    return false;
-                }
-            }
+        // this means we need to pair them up;
+        // each time we have a right signal, the left one must be the matched left signal;
+        // put left into stack; each time we have a right signal, try to match them;
+        // turn s into array so it can be processed quickly.
+        // char[] arr = new char[s.length()];
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='(') stack.push(')');
+            else if(c=='{') stack.push('}');
+            else if(c=='[') stack.push(']');
+            // match them if it's a right signal;
+            else if(stack.isEmpty() || stack.pop()!= c) return false;
         }
         return stack.isEmpty();
-        */
+        
+        
     }
 }

acfa887b20212af6c2e26c6575b75954aad88404
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-09 17:31:16 +0100

    Time: 2 ms (98.44%), Space: 45 MB (32.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,37 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // // put num into map. then find target - num; and return their index;
-        // Map<Integer, Integer> map = new HashMap<>();
-        // for(int i = 0 ; i < nums.length ; i++){
-        //     int temp = target - nums[i];
-        //     if(map.containsKey(temp)) return new int[]{map.get(temp), i};
-        //     map.put(nums[i], i);
-        // }
-        // return new int[2];
+        // 1) we have to record what numbers there are in the array. 2) we have to record the indices
+        // use HashMap so there are 2 dimension for us to use.
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<= nums.length -1;i++){
+            // if we find target-nums[i] in the map, the index is our result;
+            if(map.containsKey(target-nums[i])) return new int[]{i, map.get(target-nums[i])};
+            // else, add elements in the array into map;
+            map.put(nums[i], i);
+        }
+        return new int[] {};
         
-        // find 1 elements by index(array, map)
-        // solution 1: brute force
-        /*
-        for(int i = 0; i < nums.length - 1; i++){
-            int temp = target - nums[i];
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[j]==temp) return new int[]{i,j};
-            }
-        }
-        return null;*/
-        // solution 2: use map to remember the elements as key, put the index as value;
-        // then we can get it in O(1) time;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0 ; i < nums.length; i++){
-            int complement = target - nums[i];
-            // check if there is a key equals nums[i]
-            // if exists, return result
-            if(map.containsKey(nums[i])) return new int[]{i,map.get(nums[i])};
-            // if not, put it in map;
-            map.put(complement, i);
-        }
-        return new int[0];
+        
     }
-}      
-        
-        
+}

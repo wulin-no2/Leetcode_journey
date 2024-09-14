4c81c31c924e235bdd708373d6739a2479ecb043
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-14 16:54:39 +0100

    Time: 0 ms (100.00%), Space: 44 MB (89.57%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,56 +9,19 @@
  *     }
  * }
  */
-// second round: 1.22.2024
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // 2pointers
-        // 1 fast runs 2 steps a time; 1 slow runs 1 step a time; if they can equal. then return true;
+        // fast slow approach;
+        // if there is a cycle, they must meet each other;
         ListNode fast = head;
-        ListNode slow = head; 
-        if(head == null) return false;
-        while(fast.next != null && fast.next.next != null){            
+        ListNode slow = head;
+        if(fast==null) return false;
+        while(fast != null){
+            if(fast.next==null || fast.next.next==null) return false;
             fast = fast.next.next;
             slow = slow.next;
             if(fast == slow) return true;
-
         }
         return false;
     }
 }
-
-
-
-
-// public class Solution {
-//     public boolean hasCycle(ListNode head) {
-//         // 0 node:
-//         if(head == null){return false;}
-//         // n nodes:
-//         ListNode slow = head;
-//         ListNode fast = head;
-
-//         //int pos = -1;
-// /*        int index1 = 0;//the position of temp1;     
-//         while(temp1.next != null){
-//             for(int n = 0 ; n <= index1 ; n ++){
-//                 if(temp1.next == temp2){
-//                     //pos = n;
-//                     return true;
-//                 }else{
-//                     temp2 = temp2.next;
-//                 }
-//             }
-//             index1++;
-//             temp1 = temp1.next;
-//         }*/
-//         while (fast.next != null && fast.next.next != null) {
-//             fast = fast.next.next;
-//             slow = slow.next;
-//             if (fast == slow) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// }

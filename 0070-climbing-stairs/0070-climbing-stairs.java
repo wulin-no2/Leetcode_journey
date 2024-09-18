df07e097bc1b94cc80c33da183a31c488461b751
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-18 14:15:39 +0100

    Time: 0 ms (100.00%), Space: 40.1 MB (68.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,35 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        // dp[n] = dp[n - 1] + d[n - 2];
-        /*
+        // 1. dp definition: for n steps stairs, we have dp[n] ways to the top;
         int[] dp = new int[n + 1];
-        dp[1] = 1;
+        
+        // 2. dp equations
+        //dp[n] = dp[n-1] + dp[n-2];
+        
+        // 3. initialize
+        dp[1] = 1; if(n==1) return dp[1]; 
         dp[2] = 2;
-        for(int i = 3; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+        // 4. how to traverse
+        for(int i = 3; i <= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+            //System.out.println("dp["+n+"] is "+dp[n]);
         }
+        // 5. result
         return dp[n];
-        */
-        if(n < 3) return n;
-        int a = 1;
-        int b = 2;
-        for(int i = 3; i <= n; i++){
-            int res = a + b;
-            a = b;
-            b = res;
-        }
-        return b;
-        
-        
-
-        // approach2:memorization, time and space complexityO(n)
-        // use a map to memorize computed results for each step n
-        /*
-        HashMap<Integer, Integer> map = new HashMap<>();
-        if(n <= 3) return n;
-        for(int i = 0; i <= 3; i++) map.put(i,i);
-        for(int i = 4; i <= n; i++) map.put(i, map.get(i-1) + map.get(i - 2));
-        return map.get(n);
-        */
     }
 }

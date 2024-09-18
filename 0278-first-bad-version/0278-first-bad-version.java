52209dcb8c47c57e2340c07708496dc8fcabcf56
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-18 13:45:31 +0100

    Time: 74 ms (6.29%), Space: 40.4 MB (38.50%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,57 +3,16 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        // use binary search.
-        // each time check the middle one;
-        // if it's true,maybe there will older versions true.
-        // find the true 1, then the left one must be false;
-        
-        // we numst use minimize numbers of API
-        /*
-        int low = 1;
-        int high = n;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(isBadVersion(mid)){
-                if(mid == 1 || ! isBadVersion(mid - 1)){
-                    return mid;
-                }else{
-                    high = mid - 1;
-                }
-            }else{
-                low = mid + 1;
-            }
+        if(n==1) return 1;
+        if(isBadVersion(1)) return 1;
+        int left = 2;
+        int right = n;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(isBadVersion(mid) && ! isBadVersion(mid-1)) return mid;
+            if(isBadVersion(mid)) right = mid-1;
+            if(!isBadVersion(mid)) left = mid+1;
         }
-        return n;
-        */
-        
-        // there is a faster approach in discuss:
-        /*int i = 1;
-        int j = n;
-
-        while (i < j) {
-
-            int mid = i + (j - i) / 2;
-
-            if (isBadVersion(mid)) {
-                j = mid;
-            } else {
-                i = mid + 1;
-            }
-
-        }
-
-        return i;
-        */
-        int f = 1, l = n;
-        while(f<=l){
-            int m = f+(l-f)/2;
-            if(!isBadVersion(m)){
-                f = m+1;
-            }else{
-                l = m-1;
-            }
-        }
-        return f;
+        return -1;
     }
 }

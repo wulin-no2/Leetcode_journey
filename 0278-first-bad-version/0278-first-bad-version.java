2ce77a9b2af92421ed12cffeef3e9ed3e5c3de24
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-18 13:46:26 +0100

    Time: 53 ms (6.29%), Space: 40.5 MB (27.41%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -9,9 +9,9 @@ public class Solution extends VersionControl {
         int right = n;
         while(left <= right){
             int mid = left + (right - left)/2;
-            if(isBadVersion(mid) && ! isBadVersion(mid-1)) return mid;
+            if(isBadVersion(mid) && !isBadVersion(mid-1)) return mid;
             if(isBadVersion(mid)) right = mid-1;
-            if(!isBadVersion(mid)) left = mid+1;
+            else left = mid+1;
         }
         return -1;
     }

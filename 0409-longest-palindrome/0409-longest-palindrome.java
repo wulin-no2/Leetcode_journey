83cd37abc068e50512dd9df4572503fc2da2ce39
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-20 15:51:44 +0100

    Time: 1 ms (100.00%), Space: 41.2 MB (98.96%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,25 +1,21 @@
 class Solution {
     public int longestPalindrome(String s) {
-        // hashing
-        // count each digits, we can use all the even count nunmbers（+ 1 odd count number）
-        // hashing
-        HashMap<Character, Integer> map = new HashMap<>();
-        // traverse and put elements into map
-        // Traverse the string and populate the HashMap
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        // use array hashing
+        int[] arr = new int[128];
+        // traverse
+        for(char c: s.toCharArray()){
+            arr[c]++;
         }
-        // count the even count numbers
-        int hasOdd = 0;
+        // count
         int count = 0;
-        for(int i: map.values()){
+        int hasOdd = 0;
+        for(int i: arr){
             if(i % 2 == 0) count += i;
-            else {
-                count += i - 1;  // Add the largest even part of odd counts
-                hasOdd = 1;  // Mark that there's at least one odd count}
+            else{
+                count += i - 1;
+                hasOdd = 1;
             }
         }
-        return count + hasOdd;
-        
+        return count+hasOdd;
     }
 }

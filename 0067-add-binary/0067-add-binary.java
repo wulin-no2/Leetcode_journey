5d260a83ab2b88978a693d23fff396082c1bd49c
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-23 14:32:47 +0100

    Time: 2 ms (47.49%), Space: 41.8 MB (96.50%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,47 +1,26 @@
 class Solution {
     public String addBinary(String a, String b) {
-        // there are some methods we can use:
-        // first, transfer a String with number into decimal:
-        /*
-        int aInt = Integer.parseInt(a, 2);
-        int bInt = Integer.parseInt(b, 2);
-        int num = aInt + bInt; 
-        // second, transfer a number of decimal in to binary;
-        return Integer.toBinaryString(num);
-        */
-        
-        // the problem is. a and b are too long.
-        // we can't simply tranfer them to int, or they will surpass the constraint of the boundary of int
-        // we can use BigInteger
-        /*
-        return new java.math.BigInteger(a, 2).add(new java.math.BigInteger(b, 2)).toString(2);
-        */
-        
-
-        
-        
-        // write it on my own;
-        
-        String res = "";
-        int carry = 0;
+        // manipulate string
+        // traverse from end to start
+        StringBuilder result = new StringBuilder();
         int i = a.length() - 1;
         int j = b.length() - 1;
-        // int sum = 0;
-        while(i >= 0 || j >= 0 || carry != 0){
+        int carry = 0;
+        while(i >= 0 || j >= 0) {
             int sum = carry;
-            if(i >= 0){
-                sum += a.charAt(i) -'0';
+            if(i>=0){
+                sum += a.charAt(i) - '0';
                 i--;
             }
-            if(j >= 0){
-                sum += b.charAt(j) -'0';
+            if(j>=0){
+                sum += b.charAt(j) - '0';
                 j--;
             }
+            result.append(sum % 2);
             carry = sum / 2;
-            res = (sum % 2) + res;
         }
-        return res;
-        
+        if(carry!=0) result.append(carry);
+        return result.reverse().toString();
         
     }
 }

b802308c4d6eb3b721c648248e29269930b3e62b
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-23 16:29:53 +0100

    Time: 0 ms (100.00%), Space: 40.9 MB (60.49%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,38 +10,15 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        // analyze:
-        // traverse twice:
-        // 1. traverse and use an int count to count the nodes;
-        // 2. traverse and decrement the count until find the middle node;
-        // use a pointer to traverse;
-        
-        // my solution: Time complexity O(n) Space complexity O(1)
-        /*
-        int count = 1;
-        ListNode ptr = head;
-        
-        // first traverse, get the count;
-        while(ptr.next != null){
-            ptr = ptr.next;
-            count++;
-        }
-        // second traverse, find the middle;
-        int mid = count / 2; 
-        ptr = head;
-        while(mid > 0){
-            ptr = ptr.next;
-            mid--;
-        }
-        return ptr;
-        */
-        
-        // another solution: use two pointers. 1 runner is twice as fast as the other runner:
+        // fast slow approach
+        // fast pointer fun faster 2 steps; slow pointer run slower 1 step;
+        // when fast == null or fast.next == null return slow
         ListNode fast = head;
         ListNode slow = head;
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
+        while(fast != null){
+            if(fast.next==null) break;
+            fast = fast.next.next; // 3,5, null
+            slow = slow.next; // 2,3,4
         }
         return slow;
         

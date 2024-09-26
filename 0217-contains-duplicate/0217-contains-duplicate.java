e40da281735d2529013122d7c8f952758f29e0e1
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-26 22:05:03 +0100

    Time: 11 ms (72.15%), Space: 58.1 MB (39.40%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // add elements of the array into arrayList.
-        // use contains to check if an element is in the arrayList.
-        
-        // No!! We should use hashset to add and check.
-        // Cause it's more efficient to add elements with hashset. and the elements in hashset can't duplicate.
-        HashSet<Integer> set = new HashSet<>();
-        for(Integer num: nums){
-            if(set.contains(num)){
-                return true;
-            }
+        HashSet set = new HashSet<>();
+        // use set to check if there is alreay this elements
+        for(int num: nums){
+            if(set.contains(num)) return true;
             set.add(num);
         }
         return false;
+        
     }
 }

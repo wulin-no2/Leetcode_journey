4edb1d1a642313c7405f3bcbabb9a522a2904542
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-26 22:20:53 +0100

    Time: 1 ms (99.39%), Space: 57.1 MB (43.20%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,49 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // analyze:
-        // we can use kadane's algorithm;
-        // use 2 int and 2 LinkedList to store arrays;
-        // 1) int max_ending_here
-        // 2) int max
-        // update step by step
+        // calculate sum until now, if it's negative, discard it; if it's positive, add it;
+        int sum_until_now = 0;
+        int max_sum = Integer.MIN_VALUE;
         
-        /*
-        int maxEndingHere = nums[0];
-        int max = nums[0];
-        
-        for(int i = 1; i < nums.length; i++){
-            if(maxEndingHere + nums[i] > nums[i]){
-                maxEndingHere = maxEndingHere + nums[i];
-            }else{
-                maxEndingHere = nums[i];
-            }
-            
-            if(max < maxEndingHere){
-                max = maxEndingHere;
-            } 
+        for(int i = 0 ; i < nums.length ; i++){
+            sum_until_now += nums[i];
+            if(sum_until_now > max_sum) max_sum = sum_until_now;
+            if(sum_until_now < 0) sum_until_now = 0;
         }
-        return max;
-        */
-        
-        
-        // there are other 2 approaches;
-        // Dynamic Programming
-        /*
-        int maxArray []=  new int[nums.length];
-        int max  =  nums[0];
-        maxArray[0] =  nums[0];
-        for(int i = 1; i < nums.length; i++){
-           maxArray[i] = Math.max(maxArray[i-1] + nums[i], nums[i]);
-           max = Math.max(maxArray[i], max);
-        }
-        return max;   
-        */
-        int max = nums[0];
-        int maxCur = max;
-        for(int i = 1; i < nums.length; i++){
-            maxCur = Math.max(maxCur + nums[i], nums[i]);
-            max = Math.max(maxCur, max);
-        }
-        return max;
+        return max_sum;
     }
 }

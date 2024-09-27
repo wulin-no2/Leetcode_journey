b07311082129186067852645539416b19f15ee5f
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-27 18:32:11 +0100

    Time: 15 ms (42.77%), Space: 47.2 MB (58.39%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,72 +1,51 @@
 class Solution {
-    
     public int[][] updateMatrix(int[][] mat) {
-        /* first time. 2024.1.10. 17:48. can't do it
-        // dynamic programming
-        // 1) if mat[i][j] == 0, return 0
-        // 2) if mat[i][j] == 1:
-        //  2.1) if 4 pixel's around it,min has a 0, return 1;
-        //  2.2) if not, return 1 + min;
-        // 3) for all the 2:
-        // ... keep update, until all the pixel has a result; it's BFS
+        // use a matrix to store the result
+        // if mat[i][j]==0 res[i][j]=0;
+        // else use BFS to find the shortest distance of it to 0;
+        // 1st pass
         int m = mat.length;
         int n = mat[0].length;
-        int[][] res = new int[m][n];
-        // we should use hashMap to store upToNow.
-        HashMap<Integer, Integer> upToNow = new HashMap<>;
-        // traverse 1 time;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(mat[i][j] == 0) res[i][j] = 0; // upToNow[i][j] = 0;
-                else upToNow[i][j] = 1;
-            }
-        }
-        // traverse 2 time;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(upToNow[i][j] == 1) upToNow[i][j] = Math.min(upToNow[i][j], 1 + [][] ); // upToNow[i][j] = 0;
-                else upToNow[i][j] = 1;
-            }
-        }
-        while{! upToNow.isEmpty()}
-        */
         
-        // BFS from solution:
-        // since it's BFS, se can use queue for traversal;
-        if(mat == null || mat.length == 0 || mat[0].length == 0) return new int[0][0];
-        int m = mat.length, n = mat[0].length;
+        // use BFS to solve it
         Queue<int[]> queue = new LinkedList<>();
-        int MAX_VALUE = m * n;
-        // traverse 1 time;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(mat[i][j] == 0) queue.offer(new int[]{i, j}); // upToNow[i][j] = 0;
-                else mat[i][j] = MAX_VALUE;
-            }
-        }
         
-        // Use queue to traverse twice;
-        // really useful when need to go 4 directions:
-        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!queue.isEmpty()){
-            int[] cell = queue.poll();
-            for(int[] dir : directions){
-                int r = cell[0] + dir[0], c = cell[1] + dir[1];
-                if( r >= 0 && r < m && c >=0 && c < n && mat[r][c] > mat[cell[0]][cell[1]] + 1){
-                    queue.offer(new int[]{r, c});
-                    mat[r][c] = mat[cell[0]][cell[1]] + 1;
+        // Initialize the result matrix with max values for 1s, and add them into queue
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    result[i][j] = 0;
+                    queue.offer(new int[]{i,j}); // store coordinate into it
+                } else {
+                    result[i][j] = Integer.MAX_VALUE; // Large value for 1s
                 }
             }
         }
-        return mat;
+        // helper directions
+        int[][] directions = {{1, 0},{-1, 0},{0, 1},{0, -1}};
         
-        
-        
-        
-        
-        
-        
+        // BFS
+        while(!queue.isEmpty()){
+            // get an int[]
+            int[] cell = queue.poll();
+            int x = cell[0];
+            int y = cell[1];
             
-        
+            // and compare it with 4 directions
+            for(int[] direction: directions){
+                int newX = x + direction[0];
+                int newY = y + direction[1];
+             // if 4 directions > current value + 1, update 4 directions value, add it to queue 
+                if(newX>=0 && newX<mat.length && newY >=0 && newY<mat[0].length){
+                    if(result[newX][newY] > result[x][y] + 1){
+                        result[newX][newY] = result[x][y] + 1;
+                        queue.offer(new int[]{newX, newY});
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }

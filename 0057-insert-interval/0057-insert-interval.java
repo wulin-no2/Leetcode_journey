879c644c853d073cb1a6d11dd4703731cb960e11
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-09-27 15:09:31 +0100

    Time: 1 ms (98.64%), Space: 44.6 MB (89.84%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,68 +1,28 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        // analyze:
-        // we can see them as a serial of numbers in accending order;
-        // insert 2 integers into the serial in accending order;;
-        // then divide them into groups with 2 numbers;
-        // the difficult thing is:
-        // how to check if they are overlapping;
-        List<int[]> result = new LinkedList<>();
-	    int i = 0;
-	    // add all the intervals ending before newInterval starts
-	    while (i < intervals.length && intervals[i][1] < newInterval[0]){
-	        result.add(intervals[i]);
-	        i++;
-	    }
-	    
-	    // merge all overlapping intervals to one considering newInterval
-	    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-	    	// we could mutate newInterval here also
-	        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-	        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-	        i++;
-	    }
-	    
-	    // add the union of intervals we got
-	    result.add(newInterval); 
-	    
-	    // add all the rest
-	    while (i < intervals.length){
-	    	result.add(intervals[i]); 
-	    	i++;
-	    }
-	    
-	    return result.toArray(new int[result.size()][]);
-        /* another approach:
-        List<int[]> ans = new ArrayList<>();
-        int n = intervals.length;
-        for (int i = 0; i < n; i++) {
-            if (intervals[i][1] < newInterval[0]) {
-                // current interval starts first & not covered by newInterval, add intervals[i] to ans
-                // [curInterval]
-                //                      [newInterval]
-                ans.add(intervals[i]);
-            } else if (intervals[i][0] > newInterval[1]) {
-                // newInterval starts first and not covered by current interval
-                // add newInterval to ans and set newInterval = curInterval
-                //                       [curInterval]
-                // [newInterval]
-                ans.add(newInterval);
-                newInterval = intervals[i];
-            } else if (intervals[i][1] >= newInterval[0] || intervals[i][0] <= newInterval[1]) {
-                // they are overlapped, merge them
-                // [curInterval]
-                //        [newInterval]
-                // or
-                // [newInterval]
-                //        [curInterval]
-                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        // traverse intervals. for each interval,compare it with newInterval
+        // 3 conditions:
+        // 1. interval[1] > newInterval[0], put interval into result list
+        // 2. interval[0] < newInterval[1], put newInterval into res, then put interval in
+        // 3. overlapping
+        // 3.1. put [Math.min(interval[0], newInterval[0]), Math.max(interval[0], newInterval[0])] into res
+        // we can first put newInterval into res, each time get last element of list and compare with interval.
+        
+        List<int[]> res = new ArrayList<>();
+        
+        for(int[] interval: intervals){
+            if(interval[1] < newInterval[0]) res.add(interval);
+            else if(interval[0] > newInterval[1]) {
+                res.add(newInterval);
+                newInterval = interval; // why???
+            }
+            else{ // merge them
+                newInterval[0] = Math.min(newInterval[0], interval[0]);
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
             }
         }
-        // add the last interval
-        ans.add(newInterval);
-        return ans.toArray(new int[ans.size()][]);
-        */
+        res.add(newInterval);
+        return res.toArray(new int[res.size()][]);
+        
     }
-    
 }

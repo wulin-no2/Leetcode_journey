c7e9af4ab0cf0af8d9a6c88119fbb40f05acb727
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-03 13:29:57 +0100

    Time: 1 ms (90.22%), Space: 44.6 MB (88.65%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,85 +15,27 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        // 0205
-        // use a list to store result;
+        // BFS
+        // use Queue to traverse
         List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        // put first level into q
         if(root == null) return res;
-        // use a queue to BFS:
-        Deque<TreeNode> q = new LinkedList<>();
         q.offer(root);
         while(!q.isEmpty()){
+            // record level size
             int size = q.size();
             List<Integer> list = new ArrayList<>();
-            while(size-- > 0){
-                TreeNode temp = q.poll();
-                list.add(temp.val);
-                if(temp.left != null) q.offer(temp.left);
-                if(temp.right != null) q.offer(temp.right);
+            while(size > 0){
+                // traverse one level
+                TreeNode node = q.poll();
+                list.add(node.val);
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
+                size--;
             }
             res.add(list);
         }
         return res;
-        
-        
-        
-        
-        
-        
-        // can't do it, juct copy one and see why
-        // BFS iterative approach:
-        /*
-        // base case:
-        List<List<Integer>> res = new ArrayList<>(); 
-        if (root == null) return res;  
-        // use a queue:
-        LinkedList<TreeNode> q = new LinkedList<>();  
-        q.add(root);  
-        // each time, we retrive a node, add the value, then see if it has children. if it has, enqueue the children from left to right;
-        // repeat until the queue is empty;
-        // notice: for each node, we need a list to store the pair of values; each end of the node, we add the list into the result list;
-        while (!q.isEmpty()) {  
-          List<Integer> list = new ArrayList<>();  
-          int size = q.size();
-          for (int i = 0; i < size; i++) {  
-            TreeNode temp = q.poll();  
-            list.add(temp.val);  
-            if (temp.left != null) {  
-              q.add(temp.left);  
-            }
-            if (temp.right != null) {  
-              q.add(temp.right);  
-            }  
-          }  
-          res.add(list);   
-        }  
-        return res;
-        */
-        
-        // DFS recursive approach:
-        /* solution before:
-        List<List<Integer>> res = new ArrayList<>();
-		if (root == null)
-			return res;
-		levelOrderHelper(res, root, 0);
-		return res;
-	}
-	
-	public void levelOrderHelper(List<List<Integer>> res, TreeNode root, int level) {
-		if (root == null)
-			return;
-		List<Integer> curr;
-		if (level >= res.size()) {
-			curr = new ArrayList<>();
-			curr.add(root.val);
-			res.add(curr);
-		} else {
-			curr = res.get(level); 
-			curr.add(root.val); 
-			//res.add(curr); // No need to add the curr into the res, because the res.get(index) method does not remove the index element
-		}
-		levelOrderHelper(res, root.left, level + 1);
-		levelOrderHelper(res, root.right, level + 1);
-        */
     }
 }

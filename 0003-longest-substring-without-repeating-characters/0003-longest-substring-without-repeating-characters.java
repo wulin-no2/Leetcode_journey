fbae9d60c79945c58f05e8d9addca25f5b44beca
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-08 18:59:57 +0100

    Time: 6 ms (69.71%), Space: 45.1 MB (18.24%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,47 +1,23 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         // array hashing, record last seen index for each character
-//         int[] arr = new int[128];
-//         int res = 0;
-//         // use sliding window to track unique substring
-//         int left = 0;
-//         // traverse s
-//         for(int i = 0; i < s.length(); i++){
-//             char c = s.charAt(i);
-//             // if s.charAt(i) in arr, update its index, move left, update res
-//             if(arr[c] > 0 && arr[c] >= left) left = arr[c];
-//             res = Math.max(res,i - left + 1);
-//             arr[c] = i + 1; 
-//         }
-//         // return max(res, count)
-//         return res;
-//     }
-// }
-import java.util.HashMap;
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // HashMap to store the last seen index of each character
-        HashMap<Character, Integer> map = new HashMap<>();
+        // sliding window
+        // left and right pointers
         int res = 0;
-        int left = 0;  // The left boundary of the sliding window
-        
-        // Traverse the string
-        for (int i = 0; i < s.length(); i++) {
+        // without repeating characters: use set to store elements
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            
-            // If the character has been seen and is within the current window
-            if (map.containsKey(c) && map.get(c) >= left) {
-                left = map.get(c) + 1;  // Move the left boundary to one position after the last occurrence of c
+            // if(! set.contains(c)) {
+            //     set.add(c);
+            // }
+            while(set.contains(c)){
+                set.remove(s.charAt(left));
+                left++;
             }
-            
-            // Calculate the length of the current substring and update the result
+            set.add(c);
             res = Math.max(res, i - left + 1);
-            
-            // Update the last seen index of the character
-            map.put(c, i);
         }
-        
         return res;
     }
 }

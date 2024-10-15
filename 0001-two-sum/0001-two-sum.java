dc49aabc7a31c262c52df570e871a615d9403c30
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-15 21:53:32 +0100

    Time: 2 ms (98.79%), Space: 45.1 MB (19.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // 1) we have to record what numbers there are in the array. 2) we have to record the indices
-        // use HashMap so there are 2 dimension for us to use.
+        // record the number and the index, using map
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i<= nums.length -1;i++){
-            // if we find target-nums[i] in the map, the index is our result;
-            if(map.containsKey(target-nums[i])) return new int[]{i, map.get(target-nums[i])};
-            // else, add elements in the array into map;
-            map.put(nums[i], i);
+        // traverse
+        for(int i = 0; i < nums.length; i++){
+            int aim = target - nums[i];
+            if(map.containsKey(aim)) return new int[]{map.get(aim), i};
+            map.put(nums[i],i);
         }
-        return new int[] {};
-        
+        return null;
         
     }
 }

1472b0898a3941452f22c36ac661c7eb4d45352f
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-15 21:26:26 +0100

    Time: 14 ms (29.94%), Space: 44.7 MB (28.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length()!=t.length()) return false;
-        // we have to record the characters and the counts, using map;
+        // use map to track elements and counts;
         HashMap<Character, Integer> map = new HashMap<>();
-        char[] sc = s.toCharArray();
-        char[] tc = t.toCharArray();
-        for(int i = 0; i < sc.length; i++){
-            map.put(sc[i], map.getOrDefault(sc[i], 0) + 1);
+        // traverse 1st string;
+        for(char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        for(int i = 0; i < tc.length; i++){
-            if(! map.containsKey(tc[i])) return false;
-            else map.put(tc[i],map.get(tc[i])-1);
+        
+        // traverse 2nd string;
+        for(char c: t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if(map.get(c) < 0) return false;
         }
-        // if(map.isEmpty()) return true;// no!! value maybe 0, but it doesn't mean map is empty;
-        for(int count: map.values()){
-            if(count!=0) return false;
+        // check if all counts are zero
+        for(int count : map.values()) {
+            if(count != 0) return false;
         }
+        
         return true;
-            
         
     }
 }

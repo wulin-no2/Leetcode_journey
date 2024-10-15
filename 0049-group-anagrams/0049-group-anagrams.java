9fe4a0614b3cc06973b75760c7c8a1d2b9e3ae12
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-15 22:38:05 +0100

    Time: 18 ms (21.77%), Space: 47.8 MB (55.46%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // use map for tracking
+        HashMap<String, List<String>> map = new HashMap<>();
+        // traverse strs
+        for(String str: strs){
+            // convert it into a code and group by code
+            int[] codeArr = new int[26];
+            for(char c : str.toCharArray()){
+                codeArr[c-'a']++;
+            }
+            
+            // convert arr to string
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0;i < 26;i++){
+                sb.append('#');
+                sb.append(codeArr[i]);
+            }
+            String code = sb.toString();
+            
+            
+            // if code in map, group them; if not, put into map;
+            if(! map.containsKey(code)) map.put(code, new ArrayList<String>());
+            map.get(code).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}

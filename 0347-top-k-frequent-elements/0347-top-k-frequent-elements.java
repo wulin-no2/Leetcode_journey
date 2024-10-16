71f3cbef86c7aa03eb080c81e6f2aec18d67ba42
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-16 15:49:49 +0100

    Time: 14 ms (57.97%), Space: 45.7 MB (84.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,45 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // from camp: solution 2
-        Map<Integer,Integer> map = new HashMap<>();//key为数组元素值,val为对应出现次数
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        // 1. use map to record elements and their frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i ++){
+            map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
         }
-        //在优先队列中存储二元组(num,cnt),cnt表示元素值num在数组中的出现次数
-        //出现次数按从队头到队尾的顺序是从大到小排,出现次数最多的在队头(相当于大顶堆)
-        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2)->pair2[1]-pair1[1]);
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){//大顶堆需要对所有元素进行排序
-            pq.add(new int[]{entry.getKey(),entry.getValue()});
+        // 2. use priority queue to sort map.entry (min heap) with a comparator based on value of entry
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b)->Integer.compare(a.getValue(),b.getValue()));
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            pq.offer(entry);
+            // 2.1. once queue.size() > k, poll() to ensure remaining top k frequent elements
+            if(pq.size() > k) pq.poll();
         }
-        int[] ans = new int[k];
-        for(int i=0;i<k;i++){//依次从队头弹出k个,就是出现频率前k高的元素
-            ans[i] = pq.poll()[0];
+        
+        // 3. convert priority queue into array result;
+        int[] res = new int[k];
+        int i = 0;
+        for(Map.Entry<Integer, Integer> entry: pq){
+            res[i] = entry.getKey();
+            i++;
         }
-        return ans;
-        // from camp:
-        /*
-        Map<Integer,Integer> map = new HashMap<>();//key为数组元素值,val为对应出现次数
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        //在优先队列中存储二元组(num,cnt),cnt表示元素值num在数组中的出现次数
-        //出现次数按从队头到队尾的顺序是从小到大排,出现次数最低的在队头(相当于小顶堆)
-        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1,pair2)->pair1[1]-pair2[1]);
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){//小顶堆只需要维持k个元素有序
-            if(pq.size()<k){//小顶堆元素个数小于k个时直接加
-                pq.add(new int[]{entry.getKey(),entry.getValue()});
-            }else{
-                if(entry.getValue()>pq.peek()[1]){//当前元素出现次数大于小顶堆的根结点(这k个元素中出现次数最少的那个)
-                    pq.poll();//弹出队头(小顶堆的根结点),即把堆里出现次数最少的那个删除,留下的就是出现次数多的了
-                    pq.add(new int[]{entry.getKey(),entry.getValue()});
-                }
-            }
-        }
-        int[] ans = new int[k];
-        for(int i=k-1;i>=0;i--){//依次弹出小顶堆,先弹出的是堆的根,出现次数少,后面弹出的出现次数多
-            ans[i] = pq.poll()[0];
-        }
-        return ans;
-        */
+        return res;
+        
     }
 }

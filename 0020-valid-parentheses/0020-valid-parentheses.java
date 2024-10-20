037822d817fd737ac2433ffedb1154072f3068d5
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-21 00:42:55 +0100

    Time: 2 ms (59.39%), Space: 41.7 MB (28.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        // this means we need to pair them up;
-        // each time we have a right signal, the left one must be the matched left signal;
-        // put left into stack; each time we have a right signal, try to match them;
-        // turn s into array so it can be processed quickly.
-        // char[] arr = new char[s.length()];
+        // use stack
+        // if it's left bracket, push it;
+        // if it's right bracket, pop it and compare it;
         Deque<Character> stack = new ArrayDeque<>();
-        for(int i = 0;i<s.length();i++){
-            char c = s.charAt(i);
+        for(char c : s.toCharArray()){
             if(c=='(') stack.push(')');
-            else if(c=='{') stack.push('}');
             else if(c=='[') stack.push(']');
-            // match them if it's a right signal;
-            else if(stack.isEmpty() || stack.pop()!= c) return false;
+            else if(c=='{') stack.push('}');
+            else{
+                if(stack.isEmpty()) return false;
+                char temp = stack.pop();
+                if(c != temp) return false;
+            }
         }
         return stack.isEmpty();
-        
         
     }
 }

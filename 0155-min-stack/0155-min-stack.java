dc2196a2e2dd1116438b2f7de64a4d0156160dc2
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-21 16:27:58 +0100

    Time: 4 ms (96.94%), Space: 44.6 MB (83.83%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,29 +1,21 @@
 class MinStack {
-    // analyze:
-    // use a variable of the class, each time we pop or push an elment, update the mininum;
-    // the problem is,min value might duplicate. So we need to get the count of each value;
-    // use array to store the counts of each count?No!!
-    // use hashMap;??
-    // no!! we can still use stack, but always push 2nd min value into stack;
-    /*
-    int min = Integer.MAX_VALUE;
-    Stack<Integer> stack;
+    // use 2 stacks, one for storing all elements, one for tracking min elements
+    Deque<Integer> stack = new ArrayDeque<>();
+    Deque<Integer> mins = new ArrayDeque<>();
+    
 
     public MinStack() {
-        stack = new Stack<>();
+        
     }
     
     public void push(int val) {
-        if(min >= val){
-            stack.push(min);
-            min = val; 
-        }
+        if(mins.isEmpty() || val <= getMin()) mins.push(val);
         stack.push(val);
     }
     
     public void pop() {
-        //int temp = stack.pop();
-        if(stack.pop() == min) min = stack.pop();
+        if(stack.peek().equals(mins.peek())) mins.pop();
+        stack.pop();
     }
     
     public int top() {
@@ -31,47 +23,10 @@ class MinStack {
     }
     
     public int getMin() {
-        return min;
+        return mins.peek();
+        
     }
-    */
-    
-    // we can use stack to store int pair{val, min};
-    // done at 18:42.1.11.2024
-    private Stack<Pair<Integer, Integer>> stack;
-    public MinStack(){
-        stack = new Stack<>();
-    }
-    public void push(int val){
-        if(stack.isEmpty()){
-            stack.push(new Pair<>(val,val));
-        }
-        else{
-            int min = Math.min(stack.peek().getValue(), val);
-            stack.push(new Pair<>(val, min));
-        }
-    }
-    public void pop(){
-        if(!stack.isEmpty()){
-            stack.pop();
-        }
-    }
-    public int top(){
-        if(stack.isEmpty()){
-            return 0;
-        }
-        return stack.peek().getKey();
-    }
-    public int getMin(){
-        if(stack.isEmpty()){
-            return 0;
-        }
-        return stack.peek().getValue();
-    }
-    
 }
-
-    
-    
 
 /**
  * Your MinStack object will be instantiated and called as such:

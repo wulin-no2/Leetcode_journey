c3b23a33828a07d6d157f65f762403244faa65b5
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-10-22 16:00:18 +0100

    Time: 84 ms (53.94%), Space: 56.3 MB (83.82%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,22 +1,31 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-       int res = 0;
+       int n = position.length;
+        double[][] cars = new double[n][2];
+        
         // calculate arrival time
-       double[] timeArr = new double[target];
-       for (int i = 0; i < position.length; i++)
-        {
-            timeArr[position[i]]= (double)(target - position[i]) / speed[i];
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i]; // position
+            cars[i][1] = (double)(target - position[i]) / speed[i]; // time
         }
+
+        // sort based on position
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+        
+        int fleets = 0;
         double prev = 0.0;
-        for (int i = target-1; i >=0 ; i--)
-        {
-            double cur = timeArr[i];
-            if (cur > prev)
-            {
+        
+        // traverse from end to start
+        for (int i = 0; i < n; i++) {
+            double cur = cars[i][1];  // current arrival time
+            
+            // if longer than last car, we get a new fleet
+            if (cur > prev) {
+                fleets++;
                 prev = cur;
-                res++;
             }
         }
-        return res;   
+        
+        return fleets;
     }
 }

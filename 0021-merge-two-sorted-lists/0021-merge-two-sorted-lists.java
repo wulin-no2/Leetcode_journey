492dd7fccefe6e8d7529dadef5cbb1fd011b9717
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-11-08 15:29:14 +0000

    Time: 0 ms (100.00%), Space: 42.2 MB (75.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,35 +10,34 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // 2 pointers. one list one pointers. compare and choose the smaller one and put it in the new list;
+        // 2 pointers, p1, p2, put smaller one to next 
+        // use dummyHead 
+        // put all ramaining elements at last
+        ListNode dummyHead = new ListNode();
         ListNode p1 = list1;
         ListNode p2 = list2;
-        ListNode result = new ListNode();
-        ListNode temp = result;
+        ListNode temp = dummyHead;
         while(p1 != null && p2 != null){
-            if(p1.val>p2.val){
-                temp.next = p2;
-                temp = temp.next;
-                p2 = p2.next;
-            }
-            else{
+            if(p1.val < p2.val) {
                 temp.next = p1;
-                temp = temp.next;
                 p1 = p1.next;
             }
+            else{
+                temp.next = p2;
+                p2 = p2.next;
+            }
+            temp = temp.next;
         }
-        // now only one non-empty list is remained:
         while(p1 != null){
             temp.next = p1;
-            temp = temp.next;
             p1 = p1.next;
-            
+            temp = temp.next;
         }
         while(p2 != null){
             temp.next = p2;
-            temp = temp.next;
             p2 = p2.next;
+            temp = temp.next;
         }
-        return result.next;
+        return dummyHead.next;
     }
 }

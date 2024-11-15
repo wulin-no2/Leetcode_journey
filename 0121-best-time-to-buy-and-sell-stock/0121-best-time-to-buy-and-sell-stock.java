f8a21bfe5b9105e30c615be39ec4a61723d36673
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-11-15 16:11:45 +0000

    Time: 1 ms (99.76%), Space: 61.8 MB (39.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,24 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // dynamic programming
-        // dp[j][0] jth day we don't hold a stock, how much we have;
-        // dp[j][1] jth day we hold a stock, how much we have;
-        // dp[j][0] = max{dp[j - 1][0], dp[j - 1][1] + prices[j]};
-        // dp[j][1] = max{dp[j - 1][1], dp[j - 1][0] - prices[j]};
-        // int result = max{dp[j][0], dp[j][1]};
-        // we need dp[prices.size()-1][0];
-        int length = prices.length;
-        int[][] dp = new int[length][2];
-        
-        // initialize it:
-        dp[0][0]=0;
-        dp[0][1]= -prices[0];
-        
-        //calculate:
-        for(int j = 1; j < length; j++){
-            dp[j][0] = Math.max(dp[j - 1][0], dp[j - 1][1] + prices[j]);
-            dp[j][1] = Math.max(dp[j - 1][1], - prices[j]); // the problem is: only buy and sell once, so only -prices[j] here.
+        // traverse the array; get max_until_now in each index
+        // return Math.max(0, max_until_now);
+        int max_until_now = 0;
+        int smallest_until_now = prices[0];
+        for(int price: prices){
+            smallest_until_now = Math.min(smallest_until_now, price);
+            max_until_now = Math.max(max_until_now, price-smallest_until_now);
         }
-        return dp[length-1][0];
+        return Math.max(0, max_until_now);
     }
 }

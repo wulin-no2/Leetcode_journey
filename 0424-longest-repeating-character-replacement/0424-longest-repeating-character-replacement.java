6fd7b329452d9ae0bdc6220720b5d680b21903b0
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-11-20 15:10:51 +0000

    Time: 20 ms (37.75%), Space: 44.6 MB (19.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,29 +1,26 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // slidingwindow
-        // use int res to update result
-        // use map to record chars and counts in the window
-        // every time, check length of window - maxCount of char and see if it's less than k
-        // if less than k, valid, update res;
-        // if not, shrink window by move left until valid;
+        // for each substring, if the lengthOfSubstring - maxFrequency <= k, then this is a valid result;
+        // use slidingwindow, if lengthOfSubstring - maxFrequency > k, shrink it until valid again;
+        // use map to keep tracking of the characters and frenquencies;
+        // use int res to update our result;
         int res = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
         int left = 0;
-        int maxf = 0; // track maxCount of the window
-        for(int right = 0; right < s.length() ; right++){
-            // get maxCount of char
+        int maxF = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int right = 0; right < s.length(); right++){
             char c = s.charAt(right);
             map.put(c, map.getOrDefault(c, 0) + 1);
-            // get maxf of the window
-            maxf = Math.max(map.get(c), maxf);
-            // compare with k to check if window is valid
-            // if invalid
-            while(right - left + 1 - maxf > k){
+            // update maxF
+            maxF = Math.max(maxF, map.get(c));
+            while(right - left + 1 - maxF > k){
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
-                left++;
+                left++; 
             }
-            res = Math.max(res, right - left + 1); 
+            res = Math.max(right - left + 1, res);
         }
-        return res;
+        return res;    
     }
+        
+    
 }

bc7d2682c11a0c826067dbee1bacf9072d7f5e41
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-11-25 20:04:53 +0000

    Time: 5 ms (100.00%), Space: 45.3 MB (11.33%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,19 +10,22 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int small = Math.min(p.val,q.val);
-        int large = Math.max(p.val,q.val);
-        return traverse(root, small, large);
-
+        // pre order cause it's a BST
+        // if node.val >= lower and <= higher, then it's what we need
+        // if node.val >= higher. then go find left child
+        // if node.val <= lower. then go find right child
+        return traversal(root, p, q);
+        
     }
-    public TreeNode traverse(TreeNode root, int small, int large){
-        // preOrder traverse, similar to binary search;
-        // if node.value > large, traverse node.left;
-        // if node.value < small, traverse node.right;
-        // else return node;
-        if(root.val==small || root.val==large) return root;
-        if(root.val>large) return traverse(root.left, small, large);
-        if(root.val<small) return traverse(root.right, small, large);
-        return root;
+    public TreeNode traversal(TreeNode node, TreeNode p, TreeNode q){
+        int high = Math.max(p.val, q.val);
+        int low = Math.min(p.val, q.val);
+        // base case
+        if(node.val >= low && node.val <= high) return node;
+        // node
+        // left 
+        if(node.val <= low) return traversal(node.right, p, q);
+        // right
+        else return traversal(node.left, p, q);
     }
 }

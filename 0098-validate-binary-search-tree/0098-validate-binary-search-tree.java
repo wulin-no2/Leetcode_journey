0437fd54f9aa7b084e793169d5dc568abf5fdbea
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-11-25 21:58:40 +0000

    Time: 0 ms (100.00%), Space: 42.7 MB (97.90%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,85 +14,24 @@
  * }
  */
 class Solution {
-    /*
     public boolean isValidBST(TreeNode root) {
-        // recursive:
-        // base case:
-        if(root == null) return true;
-        if(root.left == null && root.right == null) return true;
-        // we must get the rightmost value of left and leftmost value of right
-        boolean l = true;
-        boolean r = true;
-        if(root.left != null) l = isValidBST(root.left) && (mValue(root.left, 1) < root.val);
-        if(root.right != null) r = isValidBST(root.right) && (mValue(root.right, 0) > root.val);
-        return l && r;
-    }
-    // return a BST's min or max
-    private int mValue(TreeNode root, int dir){
-        // dir == 0, means left, min; dir == 1, means right, max;
-        // if(root == null) return null;
-        if(root.left== null && root.right== null) return root.val;
-        TreeNode p = root;
-        if(dir == 0){
-            while(p.left != null){
-                p = p.left;
-            }
-            
-        }else{
-            while(p.right != null){
-                p = p.right;
-            }
-        }
-        return p.val;
-    }
-    */
-    // an inOrder soluton:
-    /*
-public boolean isValidBST(TreeNode root) {
-   if (root == null) return true;
-   Stack<TreeNode> stack = new Stack<>();
-   TreeNode pre = null;
-   while (root != null || !stack.isEmpty()) {
-      while (root != null) {
-         stack.push(root);
-         root = root.left;
-      }
-      root = stack.pop();
-      if(pre != null && root.val <= pre.val) return false;
-      pre = root;
-      root = root.right;
-   }
-   return true;
-}
-*/
-
-    // an concise and clean version same with me:
-    /*
-    public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    
-    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
-        if (root == null) return true;
-        if (root.val >= maxVal || root.val <= minVal) return false;
-        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
-    }
-   */
-    TreeNode max;
-    public boolean isValidBST(TreeNode root) {
-        // base case:
-        if (root == null) return true;
+        // post order
+        // if node.val <= maxLeft or >= smallestRight, it's false
+        return traversal(root, Long.MIN_VALUE, Long.MAX_VALUE);
         
-        // left child:
-        boolean left = isValidBST(root.left);
-        if (!left) return false;
         
-        // root:
-        if (max != null && root.val <= max.val) return false;
+    }
+    public boolean traversal(TreeNode node, long min, long max){
+        // base case
+        if(node==null) return true;
+        // left
+        // Check if the current node violates the min/max constraint
+        if (node.val <= min || node.val >= max) return false;
         
-        // right child:
-        max = root;
-        boolean right = isValidBST(root.right);
-        return right;
-    } 
+        boolean left = traversal(node.left, min, node.val);
+        // right
+        boolean right = traversal(node.right, node.val, max);
+        // node
+        return left && right;
+    }
 }

c4652026951c2d071d75bd396847511ad35342ae
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-11-27 17:00:13 +0000

    Time: 2 ms (64.92%), Space: 44.3 MB (46.33%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,78 +14,33 @@
  * }
  */
 class Solution {
-    /* don't understand;
-    private int i = 0;
-    private int p = 0;
-        // from 00:34, can't do it
-    
-        public TreeNode buildTree(int[] preorder, int[] inorder) {
-            //return build(preorder, inorder, Integer.MIN_VALUE);
-            return build(preorder, inorder, 3001);
-        }
-
-        private TreeNode build(int[] preorder, int[] inorder, int stop) {
-            if (p >= preorder.length) {
-                return null;
-            }
-            if (inorder[i] == stop) {
-                ++i;
-                return null;
-            }
-
-            TreeNode node = new TreeNode(preorder[p++]);
-            node.left = build(preorder, inorder, node.val);
-            node.right = build(preorder, inorder, stop);
-            return node;
-        }
-        */
-    //another approach:
-    /*
-    // find root, then we know left and right;
-    
-    int[] preorder;
-    int[] inorder;
-    int index;
+        // use preorder to find the node.val; 
+        // then use this val to find index of inorder array. cut it into 2 subtrees;
+        // use index range of inorder tree to cut the preorder tree into 2 subtrees;
+        // for each left trees and right trees, keep constructing trees as above;
+        // so:
+        // we have to find index of inorder tree easily according to its value; use a map to store the elements;
+    HashMap<Integer, Integer> inorderMap = new HashMap<>();
+    int preorderIndex;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        this.preorder = preorder;
-        this.inorder = inorder;
-        this.index = 0;
-        return fun(0, inorder.length - 1);
-    }
-
-    public TreeNode fun(int l, int r) {
-        if(l > r) {
-            return null;
+        for(int i = 0; i < inorder.length; i++){
+            inorderMap.put(inorder[i], i);
         }
-        int mid = find(preorder[index++], l, r);
-        return new TreeNode(inorder[mid], fun(l, mid - 1), fun(mid + 1, r));
+        preorderIndex = 0;
+        return construct(preorder, 0, inorder.length - 1);
     }
-
-    public int find(int num, int l, int r) {
-        for(int i = l; i <= r; i++) {
-            if(num == inorder[i]) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    */
-    
-    // 3rd approch: use hashmap so it's faster;
-    public TreeNode buildTree(int[] P, int[] I) {
-        Map<Integer, Integer> M = new HashMap<>();
-        for (int i = 0; i < I.length; i++)
-            M.put(I[i], i);
-        return splitTree(P, M, 0, 0, I.length-1);
-    }
-    
-    private TreeNode splitTree(int[] P, Map<Integer, Integer> M, int pix, int ileft, int iright) {
-        int rval = P[pix], imid = M.get(rval);
-        TreeNode root = new TreeNode(rval);            
-        if (imid > ileft)
-            root.left = splitTree(P, M, pix+1, ileft, imid-1);
-        if (imid < iright)
-            root.right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright);
+    public TreeNode construct(int[] preorder, int inorderStart, int inorderEnd){
+        if(inorderStart > inorderEnd) return null;
+        // node
+        int rootValue = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootValue);
+        // divide left and right based on inorder index
+        int rootIndex = inorderMap.get(rootValue);
+        // left 
+        root.left = construct(preorder, inorderStart, rootIndex - 1);
+        
+        // right
+        root.right = construct(preorder, rootIndex + 1, inorderEnd);
         return root;
     }
 }

a86edc0fbaf35c0cb28af3da0e65e32baad03655
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-12-29 15:53:32 +0000

    Time: 0 ms (100%), Space: 42.9 MB (35.52%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,19 @@
 class Solution {
-
     public List<List<Integer>> subsets(int[] nums) {
         // backtracking
-        // use list path to get each subset
-        // use list res to collect all the results
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
         backtracking(res, path, nums, 0);
         return res;
+        
     }
     public void backtracking(List<List<Integer>> res, List<Integer> path, int[] nums, int startIndex){
-        // collect result:
         res.add(new ArrayList(path));
-        // for loop to get all the result:
-        for(int i = startIndex; i < nums.length; i++){
+        for(int i = startIndex; i < nums.length ; i++){
             path.add(nums[i]);
             backtracking(res, path, nums, i + 1);
             path.remove(path.size() - 1);
         }
+        
     }
 }

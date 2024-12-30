4dba7a1f9e5903141c82c5a7de6d97bb8f197104
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-12-30 20:45:08 +0000

    Time: 129 ms (35.55%), Space: 46.3 MB (15.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,87 +1,41 @@
 class Solution {
     public String longestPalindrome(String s) {
-        // from 16:30
-        // from this solution:https://leetcode.com/problems/longest-palindromic-substring/discuss/4212564/Beats-96.49-oror-5-Different-Approaches-oror-Brute-Force-oror-EAC-oror-DP-oror-MA-oror-Recursion-oror
-        // Manacher's Algorithm & DP
-        ///* first: O(N)
-        if(s.length() == 1) return s;
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
-        s = "#" + s.replaceAll("","#") + "#";
-        
-        int[] dp = new int[s.length()];
-        int center = 0;
-        int right = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(i < right) {
-                dp[i] = Math.min(right - i, dp[2 * center - i]);
-            }
-            while(i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s.charAt(i - dp[i] - 1) == s.charAt(i + dp[i] + 1)){
-                dp[i]++;
-            }
-            
-            if(i + dp[i] > right){
-                center = i;
-                right = i + dp[i];
-            }
-            
-            if(dp[i] > maxLen){
-                maxLen = dp[i];
-                maxStr = s.substring(i - maxLen, i + maxLen + 1).replaceAll("#","");
-            }
-        }
-        return maxStr;
-        //*/
-        
-        // Second: DP, from center to expand, O(n^2)
-        /*
-        if (s.length() <= 1) {
-            return s;
-        }
+        // dp
+        // dp[i][j] = dp[i+1][j-1] && s[i]==s[j]
+        int n = s.length();
+        if (n <= 1) return s; 
 
-        int maxLen = 1;
-        int start = 0;
-        int end = 0;
-        boolean[][] dp = new boolean[s.length()][s.length()];
+        int maxLength = 1;
+        String res = s.substring(0,1);
 
-        for (int i = 0; i < s.length(); ++i) {
+        boolean[][] dp = new boolean[n][n]; 
+        // Base case: Single-character substrings
+        for (int i = 0; i < n; i++) {
             dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (i - j + 1 > maxLen) {
-                        maxLen = i - j + 1;
-                        start = j;
-                        end = i;
-                    }
-                }
+        }
+
+        // Base case: Two-character substrings
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                res = s.substring(i, i + 2);
+                maxLength = 2;
             }
         }
 
-        return s.substring(start, end + 1);
-        */
-        /*
-        if (s.length() <= 1) return s;
-        int maxLen = 1;
-        int start = 0;
-        int end = 0;
-        boolean[][] dp = new boolean[s.length()][s.length()];
-        // from center to expand;
-        // we need to get dp[i + 1][j - 1] then can we get dp[i][j], which means the end index must be outer loop;
-        for(int j = 0 ; j < s.length(); j ++){
-            dp[j][j] = true;
-            for(int i = 0; i <= j; i++){
-                if(s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
+        // DP for substrings longer than 2 characters
+        for (int len = 3; len <= n; len++) { // Length of the substring
+            for (int i = 0; i <= n - len; i++) { // Start index
+                int j = i + len - 1; // End index
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
                     dp[i][j] = true;
-                    if((j - i + 1) > maxLen){
-                        maxLen = j - i + 1;
-                        start = i;
-                        end = j;
+                    if (len > maxLength) {
+                        maxLength = len;
+                        res = s.substring(i, j + 1);
                     }
                 }
             }
         }
-        return s.substring(start, end + 1);
-        */
+        return res; 
     }
 }

bbc394dcf954132512f6856ae25e366fae9a27ec
Lina <117023721+wulin-no2@users.noreply.github.com>
2024-12-31 17:11:59 +0000

    Time: 11 ms (95.84%), Space: 43.9 MB (96.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,17 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        // dp = Math.min(             )
-         // dp[amount - coins[i]] + 1
-        // dp[amount - coins[j]] + 1
+        // dp
+        // dp[i] amount == i result
+        // traverse coins to see if we should include it or not
+        // for each coin: dp[i] = Math.min(dp[i-coin] + 1, dp[i])
+        // update result each step
+        int n = coins.length;
+        if(amount==0) return 0;
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
-        for(int j = 1; j <= amount; j++){
-            for(int i = 0; i < coins.length ; i++){
-                if(j >= coins[i]) dp[j] = Math.min(dp[j - coins[i]] + 1, dp[j]);
+        Arrays.fill(dp, amount + 1); // fill dp with unreachable values
+        dp[0]=0;
+        for(int coin: coins){
+            for(int i = coin; i <= amount; i++){
+                dp[i] = Math.min(dp[i-coin] + 1, dp[i]);
             }
         }
-        if(dp[amount] > amount) return -1;
-        return dp[amount];
+        return dp[amount]==amount+1? -1: dp[amount];
+
+
+        
     }
 }

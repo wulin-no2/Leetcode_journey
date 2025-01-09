3f5e1bafac6273ca401c5cbfc23bbafcd1fb7f6d
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-09 18:41:07 +0000

    Time: 11 ms (30.83%), Space: 48.4 MB (20.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,46 +1,27 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // use map for tracking
-        HashMap<String, List<String>> map = new HashMap<>();
-        // traverse strs
-        // sort and use sortedString as code
+        // encode all the strs, then group them based on their code
+        // use map
+        Map<String, List<String>> map = new HashMap<>(); // code, list of strings with same code
         for(String str: strs){
-            // get str and sort it as code
-            char[] arr = str.toCharArray(); 
-            Arrays.sort(arr);
-            // String sortedStr = arr.toString();
-            String sortedStr = new String(arr);  // Correctly convert sorted char array to String
-            
-            // check if code in map or not;
-            map.computeIfAbsent(sortedStr, k->new ArrayList()).add(str);
-
-        }
-        
-        
-        
-        
-        
-        
-        /* use code
-        for(String str: strs){
-            // convert it into a code and group by code
-            int[] codeArr = new int[26];
-            for(char c : str.toCharArray()){
-                codeArr[c-'a']++;
+            // get code
+            // array hashing, then turn it into String code
+            int[] arr = new int[26];
+            for(char c: str.toCharArray()){
+                arr[c-'a']++;
             }
-            
-            // convert arr to string
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0;i < 26;i++){
-                sb.append(codeArr[i]).append('#');
+            StringBuilder sb =  new StringBuilder();
+            // turn arr into sorted str
+            for(int i = 0 ; i < arr.length; i++){
+                if(arr[i] != 0) sb.append(i + 'a').append(arr[i]);
             }
+            // turn sb into str
             String code = sb.toString();
-            
-            
-            // if code in map, group them; if not, put into map;
-            map.computeIfAbsent(code, k-> new ArrayList<>()).add(str);
+
+            // add str into map's list;
+            map.computeIfAbsent(code, k -> new ArrayList<>()).add(str);
         }
-        */
-        return new ArrayList<>(map.values());
+        // turn map's values into list
+        return new ArrayList<>(map.values()); 
     }
 }

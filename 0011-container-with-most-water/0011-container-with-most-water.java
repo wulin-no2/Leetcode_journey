bc23bba5f63784c95814023e3a413a04a4e53154
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-09 23:30:01 +0000

    Time: 6 ms (7.24%), Space: 58.2 MB (20.83%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,26 @@
 class Solution {
     public int maxArea(int[] height) {
-        // 2 pointers inward
-        // move the shorter one to longer and update
+        // start with the largest broadth, then 2 pointers inward
+        // only when we find higher height, the area is likely to get bigger
+        // how to find igher height? move the shorter one inward to find a bigger one
+
         int left = 0;
         int right = height.length - 1;
-        int res = Math.min(height[left],height[right]) * (right - left);
+        int max = Math.min(height[right], height[left]) * (right - left);
         while(left < right){
-            // compare
-            if(height[left] < height[right]) left++; // move left   
-            else right--;
-            //update
-            res = Math.max(res, Math.min(height[left],height[right]) * (right - left));
+            if(height[left] <= height[right]){
+                left++;
+                if(height[left] > height[left-1]){
+                    max = Math.max(max, Math.min(height[right], height[left]) * (right - left));
+                }
+            }else{
+                right--;
+                if(height[right] > height[right + 1]){
+                    max = Math.max(max, Math.min(height[right], height[left]) * (right - left));
+                }
+            }
         }
-        return res;
+
+        return max;
     }
 }

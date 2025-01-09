15494d16b7eaffb7ed725a7353764a730478712d
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-09 18:02:00 +0000

    Time: 4 ms (76.63%), Space: 42.8 MB (95.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,38 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        
-        if(s.length()!=t.length()) return false;
-        /*
-        // use map to track elements and counts;
-        HashMap<Character, Integer> map = new HashMap<>();
-        // traverse 1st string;
-        for(char c: s.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        
-        // traverse 2nd string;
-        for(char c: t.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0) - 1);
-            if(map.get(c) < 0) return false;
-        }
-        return true;
-        */
-        
-        // use array as hashing cause we only have 26 letters
+        if(s.length() != t.length()) return false;
+        // hashing, map, array
         int[] arr = new int[26];
-        for(char c: s.toCharArray()){
-            arr[c-'a'] += 1;
+        for(int i = 0; i < s.length();i++){
+            arr[s.charAt(i)-'a']++;
         }
-        for(char c: t.toCharArray()){
-            arr[c-'a'] -= 1;
-            if(arr[c-'a'] < 0) return false;
-            
+        for(int i=0; i < t.length();i++){
+            arr[t.charAt(i)-'a']--;
+            if(arr[t.charAt(i)-'a'] < 0) return false;
         }
         return true;
-        
-        
-        
         
     }
 }

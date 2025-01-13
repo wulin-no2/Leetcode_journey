56f83bdf74c9e8cf6ad56120f0e629107dab8bdf
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-13 22:57:06 +0000

    Time: 24 ms (86.93%), Space: 60.6 MB (54.58%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        // check if there is a larger element after each element's position and calculate their difference of indice
+        // record the index of larger values
         // traverse from end to start
-        Deque<Integer> stack = new LinkedList<>();
+        // we should only record the indice whose value is larger than current elements
+        // monotonic stack (from large to small, smaller on the top)
         int n = temperatures.length;
         int[] res = new int[n];
-        for(int i = n-1 ; i >= 0 ; i--){
-            // for each element, keep popping until find a bigger one
+        Deque<Integer> stack = new LinkedList<>();
+        for(int i = n - 1; i >= 0 ; i--){
             int temp = temperatures[i];
-            while(! stack.isEmpty() && temperatures[stack.peek()] <= temp) stack.pop();
-            if(! stack.isEmpty()) res[i] = stack.peek() - i;
-            // Push the current index into the stack
+            while(! stack.isEmpty() && temp >= temperatures[stack.peek()]) stack.pop(); 
+            if(! stack.isEmpty()) res[i] = stack.peek() - i; 
             stack.push(i);
+
         }
         return res;
         

d93308ab57e3d49a49687b5a909d27127609e184
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-13 22:07:54 +0000

    Time: 25 ms (84.62%), Space: 60.6 MB (52.24%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,27 +1,19 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        // check if there is a larger element after each element's position and calculate their difference of indice
         // traverse from end to start
-        // use stack to store the traversed elements
-        Deque<Integer> stack = new ArrayDeque<>();
-        int[] res = new int[temperatures.length];
-        for(int i = temperatures.length - 1; i >= 0; i--){
-            // for each elements
-            // keep popping elements from stack until find a bigger one
+        Deque<Integer> stack = new LinkedList<>();
+        int n = temperatures.length;
+        int[] res = new int[n];
+        for(int i = n-1 ; i >= 0 ; i--){
+            // for each element, keep popping until find a bigger one
             int temp = temperatures[i];
-            // Pop elements from the stack if the current temperature is greater or equal
-            // than the temperatures corresponding to the indices in the stack.
-            while (!stack.isEmpty() && temperatures[stack.peek()] <= temp) {
-                stack.pop();
-            }
-            // If the stack is not empty, it means we found a warmer temperature in the future.
-            // The number of days to wait is the difference in indices.
-            if (!stack.isEmpty()) {
-                res[i] = stack.peek() - i;
-            }
-            // Push the current index onto the stack
+            while(! stack.isEmpty() && temperatures[stack.peek()] <= temp) stack.pop();
+            if(! stack.isEmpty()) res[i] = stack.peek() - i;
+            // Push the current index into the stack
             stack.push(i);
-
         }
         return res;
+        
     }
 }

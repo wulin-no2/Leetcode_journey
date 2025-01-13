b73edb5a6e065ef1d46276b74c523d3b0aa67140
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-13 21:54:58 +0000

    Time: 2 ms (48.17%), Space: 43.2 MB (64.84%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        // backtracking 
-        // left >= right
-        // left <= n
+        // backtracking
+        // number of open parentheses >= number of close parentheses
+        // number of open parentheses <= n
         List<String> res = new ArrayList<>();
         backtracking(n, "", 0, 0, res);
         return res;
-        
-        
     }
     public void backtracking(int n, String curr, int left, int right, List<String> res){
         // base case
-        if(left==n && right==n) {res.add(curr); return;}
-        // recursive 
-        if(left < n) backtracking(n, curr+"(",left+1, right, res);
-        if(right < left) backtracking(n, curr+")",left, right+1,res);
+        if(left==right && left==n) {res.add(curr); return;}
+        // recursive
+        if(left < n) backtracking(n, curr + "(", left + 1, right, res);
+        if(right < left) backtracking(n, curr + ")",left, right + 1, res);
+
+
     }
 }

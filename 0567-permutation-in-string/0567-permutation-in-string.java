33ffad2fd4685ab1b2d93a3841128d6e534d7d71
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-14 14:18:20 +0000

    Time: 11 ms (40.28%), Space: 42.9 MB (46.21%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,37 +1,37 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-         if (s1.length() > s2.length()) return false; // Edge case: s1 longer than s2
-
-        // hashing
-        // use 2 arrays to record the letters and frequencies in s1 and window;
-        // comapre them, then move window forward
-        // the problem is: how to compare? 1. directly compare the arrays; 2. use a number to track the count of letters of them that have same frequencies
+        // all the frequencies of 26 letters should be the same
+        // 2 array hashings,compare substring's hashing and s1's hashing
+        // how to compare?
+        // count how many letters have same frequencies
+        if(s1.length() > s2.length()) return false;
         int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-        int matches = 0;
-        for(int i = 0; i < s1.length(); i++){
-            arr1[s1.charAt(i) -'a'] ++;
-            arr2[s2.charAt(i) -'a'] ++;
+        int[] arr2 = new int[26]; 
+        for(int i = 0; i < s1.length() ; i++){ // encode s1 and s2.first substring
+            arr1[s1.charAt(i) - 'a']++;
+            arr2[s2.charAt(i) - 'a']++;
         }
-        // calculate matches
-        for(int i = 0; i < arr1.length; i++){
-            if(arr1[i]==arr2[i]) matches++;
+        // count count how many letters have same frequencies
+        int count = 0;
+        for(int i = 0; i < 26; i++){
+            if(arr1[i]==arr2[i]) count++;
         }
-        
-        // move window forward
+        // move sliding window. if count == 26, return true;
         for(int i = s1.length() ; i < s2.length(); i++){
-            if(matches==26) return true;
-            int outIdx = s2.charAt(i - s1.length()) - 'a'; // Character leaving the window
-            int inIdx = s2.charAt(i) - 'a'; // Character entering the window
-            if(arr1[outIdx] == arr2[outIdx]) matches--;
-            else if(arr1[outIdx] == arr2[outIdx]-1) matches++;
-            arr2[outIdx]--;
-            if(arr1[inIdx] == arr2[inIdx] + 1) matches++;
-            else if(arr1[inIdx] == arr2[inIdx]) matches--;
-            arr2[inIdx]++;
+            if(count==26) return true;
+            int leave = s2.charAt(i - s1.length()) - 'a';
+            int in = s2.charAt(i) - 'a';
+            if(arr1[leave]==arr2[leave]) count--;
+            if(arr2[leave]-arr1[leave]==1) count++;
+            // update arr2
+            arr2[leave]--;
+            if(arr1[in]==arr2[in]) count--;
+            if(arr1[in]-arr2[in]==1) count++;
+            arr2[in]++;
+
+
         }
-        return matches == 26;
-        
+        return count==26;
         
     }
 }

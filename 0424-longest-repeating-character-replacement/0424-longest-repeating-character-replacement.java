fb01ab96d7ee0ba67219412370ff311fe80339e9
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-14 13:53:35 +0000

    Time: 26 ms (37.66%), Space: 45.4 MB (10.86%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,26 +1,27 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // for each substring, if the lengthOfSubstring - maxFrequency <= k, then this is a valid result;
-        // use slidingwindow, if lengthOfSubstring - maxFrequency > k, shrink it until valid again;
-        // use map to keep tracking of the characters and frenquencies;
-        // use int res to update our result;
-        int res = 0;
-        int left = 0;
-        int maxF = 0;
+        // use map
+        // only one value can have bigger value than k; all the other value's sum should be <= k in the window; len - maxF <= k  
+        // sliding window
+        // once len - maxF > k,shrink window
         Map<Character, Integer> map = new HashMap<>();
-        for(int right = 0; right < s.length(); right++){
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        int maxF = 0;
+        while(right < s.length()){
             char c = s.charAt(right);
             map.put(c, map.getOrDefault(c, 0) + 1);
-            // update maxF
-            maxF = Math.max(maxF, map.get(c));
+            maxF = Math.max(maxF, map.get(c)); 
             while(right - left + 1 - maxF > k){
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
-                left++; 
+                left++;
             }
-            res = Math.max(right - left + 1, res);
+            res = Math.max(res, right - left + 1);
+            right++;
         }
-        return res;    
+        return res;
     }
-        
+    
     
 }

bf0a64acaf41f22b3fcd403d11a34b46082d5bc3
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-14 00:36:27 +0000

    Time: 0 ms (100%), Space: 41.8 MB (93.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,21 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
         // binary search
+        int n = nums.length;
+        if(n==1) return target == nums[0]? 0: -1;
+        // now there is a pivot, binary search
         int left = 0;
-        int right = nums.length;
+        int right = n;
         while(left < right){
             int mid = (left + right) / 2;
             if(nums[mid]==target) return mid;
-            if(nums[mid] >= nums[left]){ // left part ascending
-                if(target >= nums[left] && target < nums[mid]) right = mid;
-                else left = mid + 1; 
-            }
-            else{ // left part not ordered, right part ordered
-                if(target > nums[mid] && target <= nums[right-1]) left = mid + 1;
+            if(target >= nums[0]){ //left part
+                if(nums[mid] > target) right = mid;
+                else if(nums[mid] < target && nums[mid] >= nums[0]) left = mid + 1;
                 else right = mid;
+            } 
+            else{ // right part
+                if(nums[mid] < target) left = mid + 1;
+                else if(nums[mid] > target && nums[mid] < nums[0]) right = mid;
+                else if(nums[mid] > target && nums[mid] >= nums[0])left = mid + 1;
             }
         }
-        return -1;
-        
+        return -1;  
     }
 }

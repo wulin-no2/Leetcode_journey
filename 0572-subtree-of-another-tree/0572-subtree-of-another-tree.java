7739deef990f35b763c3fbdbed353267a4ea38a4
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-17 21:19:10 +0000

    Time: 2 ms (97.43%), Space: 44.6 MB (39.65%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,27 +15,27 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        // pre order 
-        // base case
-        // node
         if(isSameTree(root, subRoot)) return true;
-        if(root==null) return false; 
-        
+        if(root==null && subRoot != null) return false;
         // left
         boolean left = isSubtree(root.left, subRoot);
         // right
         boolean right = isSubtree(root.right, subRoot);
+        // node
         return left || right;
         
     }
     public boolean isSameTree(TreeNode p, TreeNode q){
+
         // base case
         if(p==null && q==null) return true;
-        if(p == null || q==null || p.val != q.val) return false;
+        if(p==null || q==null || q.val != p.val) return false;
         // left
-        boolean left = isSameTree(p.left, q.left);
+        boolean left = isSameTree(q.left, p.left);
+
         // right
-        boolean right = isSameTree(p.right, q.right);
+        boolean right = isSameTree(q.right, p.right);
+
         // node
         return left && right;
     }

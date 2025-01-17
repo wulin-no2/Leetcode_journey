9e22c7a7c6dd6255f7b0c9e4ed7db9a764867a67
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-17 21:14:17 +0000

    Time: 0 ms (100%), Space: 41.6 MB (10.71%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,20 +15,17 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // post order traversal
-        // their left child and right child should be equal
-        return traversal(p, q);
+        return traverse(p, q);
         
     }
-    public boolean traversal(TreeNode p, TreeNode q){
+    public boolean traverse(TreeNode p, TreeNode q){
         // base case
-        if(p == null && q == null) return true;
-        if(p == null || q == null || p.val != q.val) return false;
-        
+        if(p==null && q==null) return true;
+        if(p == null || q == null || q.val != p.val) return false;
         // left
-        boolean left = traversal(p.left, q.left);
+        boolean left = traverse(p.left, q.left);
         // right
-        boolean right = traversal(p.right, q.right);
+        boolean right = traverse(p.right, q.right);
         // node
         return left && right;
     }

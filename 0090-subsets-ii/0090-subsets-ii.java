d7d2d0581574e534d88bc7cfa509a0b4267de092
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-18 16:46:15 +0000

    Time: 1 ms (99.76%), Space: 43.4 MB (74.37%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         // backtracking
-        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
-        backtracking(res, path, nums, 0);
+        // sort it to avoid dupication
+        Arrays.sort(nums);
+        backtracking(nums, res, path, 0);
         return res;
         
     }
-    public void backtracking(List<List<Integer>> res, List<Integer> path, int[] nums, int startIndex){
-        res.add(new ArrayList(path));
-        for(int i = startIndex; i < nums.length ; i++){
+    public void backtracking(int[] nums, List<List<Integer>> res, List<Integer> path, int startIndex){
+        //base case
+        res.add(new ArrayList<>(path));
+        for(int i = startIndex ; i < nums.length; i++){
+            // avoid duplication in same layer
             if(i > startIndex && nums[i]==nums[i-1]) continue;
             path.add(nums[i]);
-            backtracking(res, path, nums, i + 1);
+            backtracking(nums, res, path, i + 1);
             path.remove(path.size() - 1);
         }
+
 
     }
 }

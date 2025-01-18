31ecf5f527540e8c0259422f0b6736c8f7ba58f9
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-18 16:32:42 +0000

    Time: 4 ms (44.16%), Space: 43.3 MB (66.98%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,25 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        // each number can only be used once; startIndex
-        // no duplicate combinations;
-        Arrays.sort(candidates);
+        // backtracking
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
-        backtracking(res, path, candidates, target, 0);
+        // the elements are not unique, avoid duplication
+        Arrays.sort(candidates);
+        backtracking(candidates, target, res, path, 0);
         return res;
+
     }
-    public void backtracking(List<List<Integer>> res, List<Integer> path, int[] candidates, int target, int startIndex){
-        if(target==0){
-            res.add(new ArrayList(path));
-            return;
-        }
+    public void backtracking(int[] candidates, int target, List<List<Integer>> res, List<Integer> path, int startIndex){
+        // base case
+        if(target == 0) res.add(new ArrayList<>(path));
         if(target < 0) return;
-        for(int i = startIndex ; i < candidates.length; i++){
+        for(int i = startIndex ; i < candidates.length ; i++){
             if(i > startIndex && candidates[i]==candidates[i-1]) continue;
             path.add(candidates[i]);
-            backtracking(res, path, candidates, target-candidates[i], i + 1);
+            backtracking(candidates, target - candidates[i], res, path, i + 1);
             path.remove(path.size() - 1);
         }
-    }
 
+    }
 }

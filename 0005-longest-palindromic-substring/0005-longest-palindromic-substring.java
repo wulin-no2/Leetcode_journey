7cfe41e1b48968b21e094e32be7143af22abb138
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-20 18:59:38 +0000

    Time: 108 ms (41.35%), Space: 46.2 MB (17.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,41 +1,37 @@
 class Solution {
     public String longestPalindrome(String s) {
-        // dp
-        // dp[i][j] = dp[i+1][j-1] && s[i]==s[j]
+        // dp[i][j] = dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j). dp[i][j] means if s.substring(i, j + 1) is Palindrome;
         int n = s.length();
-        if (n <= 1) return s; 
-
-        int maxLength = 1;
+        if(n==1) return s;
+        if(n==2) return s.charAt(0)==s.charAt(1) ? s : s.substring(0,1);
         String res = s.substring(0,1);
-
-        boolean[][] dp = new boolean[n][n]; 
-        // Base case: Single-character substrings
-        for (int i = 0; i < n; i++) {
+        int maxLength = 1;
+        boolean[][] dp = new boolean[n][n];
+        // all the single-letter substrings are valid
+        for(int i = 0; i < n ; i++){
             dp[i][i] = true;
-        }
 
-        // Base case: Two-character substrings
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
+        }
+        // for the length==2 substrings:
+        for(int i = 0; i < n - 1 ; i++){
+            if(s.charAt(i)==s.charAt(i + 1)) {
                 dp[i][i + 1] = true;
-                res = s.substring(i, i + 2);
                 maxLength = 2;
+                res = s.substring(i, i + 2);
             }
         }
-
-        // DP for substrings longer than 2 characters
-        for (int len = 3; len <= n; len++) { // Length of the substring
-            for (int i = 0; i <= n - len; i++) { // Start index
-                int j = i + len - 1; // End index
-                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    if (len > maxLength) {
-                        maxLength = len;
-                        res = s.substring(i, j + 1);
-                    }
+        // for substrings of length >= 3:
+        for(int len = 3; len <= n ; len++){
+            for(int i = 0 ; i <= n - len; i++){
+                int j = i + len - 1;
+                dp[i][j] = dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j);
+                if(dp[i][j] && len > maxLength){
+                    maxLength = len;
+                    res = s.substring(i, j + 1);
                 }
             }
         }
-        return res; 
+        return res;
+
     }
 }

b441092f457e796549445e2c43b4eb317a38d543
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-20 15:35:30 +0000

    Time: 7 ms (97.79%), Space: 57.2 MB (29.94%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,35 +1,34 @@
 class Solution {
     public List<List<String>> partition(String s) {
+        // brute force
+        // 2 pointers 
+        // second pointer is for traversing the string and partitioning the string
+        // first pointer is to explore if there's a valid palindrome
         List<List<String>> res = new ArrayList<>();
-        List<String> path =  new ArrayList<>();
-        backtracking(res, path, s, 0);
+        List<String> path = new ArrayList<>();
+        // if(s.length()== 1) {path.add(s.charAt(0)); res.add(path); return res;}
+        backtracking(s, res, path, 0);
         return res;
-        
     }
-    public void backtracking(List<List<String>> res,List<String> path, String s, int startIndex){
-        if(startIndex == s.length()){
-            res.add(new ArrayList(path));
-            return;
-        }
+    public void backtracking(String s, List<List<String>> res, List<String> path, int startIndex){
+        // base case
+        if(startIndex == s.length()) {res.add(new ArrayList<>(path)); return;}
         for(int i = startIndex ; i < s.length() ; i++){
-            if(isPalindrome(s, startIndex, i)){
+            // check if substring is valid
+            if(isValid(s, startIndex, i)) {
                 path.add(s.substring(startIndex, i + 1));
-                backtracking(res, path, s, i + 1);
+                backtracking(s, res, path, i + 1);
                 path.remove(path.size()-1);
             }
-
-
         }
     }
-    // Helper function to check if a substring is a palindrome
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
+    public boolean isValid(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)) return false;
             left++;
             right--;
         }
         return true;
+
     }
 }

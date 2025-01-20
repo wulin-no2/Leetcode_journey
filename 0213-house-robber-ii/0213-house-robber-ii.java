c7875d7c974a806262ce3d64b016cb28c4f83ea0
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-20 17:03:25 +0000

    Time: 0 ms (100%), Space: 41.3 MB (36.94%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,21 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
+        // simplify this problem into 2 small problems
+        // 1. don't rob first house, get the maximum money
+        // 2. don't rob last house, get the maximum money
+        // 3. math.max
         int n = nums.length;
-        if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0], nums[1]);
-        return Math.max(robber(nums, 0, n - 2), robber(nums, 1, n - 1));
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
+        int exFirst = robHelper(nums, 1, n - 1);
+        int exLast = robHelper(nums, 0, n - 2);
+        return Math.max(exFirst, exLast);
     }
-    public int robber(int[] nums, int i, int j){
-        // dp
-        int n = nums.length;
-        if(n==1) return nums[0];
-        int[][] dp = new int[n][n];
-        dp[i][i] = nums[i];
-        dp[i][i + 1] = Math.max(nums[i], nums[i + 1]);
-
-        for(int m = 2; m < n; m++){
-            dp[i][m] = Math.max(dp[i][m - 1], dp[i][m - 2] + nums[m]);
+    public int robHelper(int[] nums, int start, int end){
+        int[] dp = new int[nums.length];
+        // dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+        for(int i = start + 2; i < end + 1; i++){
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
         }
-        return dp[i][j];
+        return dp[end];
     }
 }

98e42da88146d2666e7a0d76c85453e72b55bcd2
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-20 21:40:24 +0000

    Time: 26 ms (83.81%), Space: 42 MB (90.67%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int num: nums) sum += num;
+        for(int num : nums) sum += num;
         if(sum % 2 != 0) return false;
         int target = sum / 2;
-        // 0-1 knapsack 
-        // dynamic programming
-        // dp[j] = dp[j] || dp[j-num]   //j is target, dp[j] is whether we can get target 
         boolean[] dp = new boolean[target + 1];
+
+        // initialize
         dp[0] = true;
+
+        // 0-1 knapsack, must update snapsack from end to start
         for(int num: nums){
-            for(int j = target; j >= 0; j--){
-                if(j-num >= 0) dp[j] = dp[j] || dp[j-num];
+            for(int i = target; i >= num ; i--){
+                dp[i] = dp[i] || dp[i - num];
             }
         }
         return dp[target];
-        
     }
 }

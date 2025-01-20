e7191bc9f200f57e16a4b30dffd3bd93fda369cf
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-20 21:26:03 +0000

    Time: 12 ms (92.66%), Space: 44.4 MB (64.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        // typical unbounded knapsack question
-        // choose infinite amount of goods and see it we can reach a goal
-        // dp[j] = Math.min (dp[j] || dp[j - coin] + 1); // j means amount; dp[j] means we can reach amount j with current goods
+        // unbounded knapsack
+        if(amount==0) return 0;
+        int n = coins.length;
         int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
-        for(int j = 1; j <= amount; j++){
-            dp[j] = amount + 1;
-        }
         for(int coin: coins){
-            for(int j = 0 ; j <= amount; j++){
-                if(j-coin >= 0) dp[j] = Math.min(dp[j], dp[j - coin] + 1);
+            for(int i = 1 ; i <= amount; i++){
+                if(i >= coin) dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
-        return dp[amount]==amount + 1? -1:dp[amount];
+        return dp[amount] == amount + 1? -1 : dp[amount];
+
         
     }
 }

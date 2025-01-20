f6e83bc625957ff8b1404ada932e7f2c9842544f
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-20 16:09:47 +0000

    Time: 0 ms (100%), Space: 40.2 MB (88.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        // easy to solve it with dynamic programming
-        // we could easily get result from the previous step and the step before that
-        // arr[n] = arr[n-1] + arr[n-2]
-        int[] arr = new int[n+1];
-        arr[0] = 1;
-        arr[1] = 1;
-        for(int i = 2; i<=n; i++){
-            arr[i] = arr[i-1] + arr[i-2];
-            //System.out.println(i + " " + arr[i]);
+        // dp
+        // dp[i] = dp[i - 1] + dp[i - 2];
+        // i is stair i, dp[i] methods to reach i
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return arr[n];
+        return dp[n];
         
     }
 }

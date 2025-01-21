d4d5d37ad8ecf052e8425f0abadddc40a5cf4abb
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-21 22:13:28 +0000

    Time: 46 ms (38.73%), Space: 44.3 MB (63.85%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,22 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         // dp
-        /*
+        // dp[i] = Math.max(dp[i], dp[j] + 1); dp[i] means if the last element we choose if the ith element,
+        // what the longest increasing subsequence would be 
+        // only when j < i, and nums[j] < nums[i]
+        // use 2 for-loops to find all the possible subsequence
         int n = nums.length;
-        if(n==1) return 1;
-        int[] dp = new int[n + 1];
+        int max = 0;
+        if(n == 0 || n == 1) return n;
+        int[] dp = new int[n];
         Arrays.fill(dp, 1);
-        
-        int maxLength = 1;
-
-        for(int i = 1 ; i < n ; i++){
-            for(int j = 0; j < i ; j++) if(nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
-            maxLength = Math.max(maxLength, dp[i]);
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i ; j++){
+                if(nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+                max = Math.max(max, dp[i]);
+            }
         }
-        return maxLength;
-        */
 
-        // dp + binary search
-        List<Integer> list = new ArrayList<>();
-
-        for(int num: nums){
-            int pos = Collections.binarySearch(list, num);
-            if(pos < 0) pos = -(pos + 1);
-            if(pos < list.size()) list.set(pos, num);
-            else list.add(num);
-        }
-        return list.size();
-        
+        return max;
     }
 }

5f384e013d49737e3ed423c4f681398cb0b29ef9
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-01-23 19:12:02 +0000

    Time: 2 ms (95.16%), Space: 46.6 MB (81.78%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,22 +1,3 @@
-// class Solution {
-//     public int[] countBits(int n) {
-//         int[] ans = new int[n + 1];
-//         for (int i = 0; i <= n; i++) {
-//             ans[i] = countOnes(i);
-//         }
-//         return ans;
-//     }
-    
-//     private int countOnes(int num) {
-//         int count = 0;
-//         while (num > 0) {
-//             count += (num & 1); // Check the last bit
-//             num >>= 1;         // Shift right by 1
-//         }
-//         return count;
-//     }
-// }
-
 public class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];

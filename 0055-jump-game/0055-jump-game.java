5627678c38a67d63b131a108b7dfae928e6022b1
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-02-04 20:59:37 +0000

    Time: 3 ms (45.61%), Space: 46 MB (31.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        // update the farthest index we can go at each index
-        int farthest = 0;
-        for(int i = 0; i < nums.length - 1; i++){
-            farthest = Math.max(nums[i] + i, farthest);
-            if(farthest <= i) return false;
+        // travarse, update farthest index at each step
+        int max = 0;
+        if(nums.length==1) return true;
+        for(int i = 0; i < nums.length ; i++){
+            max = Math.max(nums[i] + i, max);
+            // System.out.println(max);
+            if(i < nums.length-1 && max==i) return false;
+
         }
         return true;
         

85d8a6fcde4155ca3e809f3e2d65107932a1a82f
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-02-12 22:24:03 +0000

    Time: 5 ms (79.66%), Space: 42.4 MB (44.52%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -1,29 +1,24 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        // 1. record the last index of each element's occurance
-        // 2. partition it 
-        List<Integer> res = new ArrayList<>();
-        int[] arr = new int[26]; // use to store occurance
-        for(int i = 0; i < s.length() ; i++){
+        // first pass: traverse, record the last index that element came up;
+        int[] position = new int[26];
+        for(int i = 0 ; i < s.length() ; i++){
             char c = s.charAt(i);
-            arr[c-'a'] = i;
+            position[c-'a'] = i;
         }
-        // partition with 2 pointers
+        // second pass: mark the farthest index current part can reach
+        // until i == farthest, divide it
+        int farthest = 0;
         int start = 0;
-        int end = 0;
-        for(int i = start; i < s.length() ; i++){
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0 ; i < s.length() ; i++){
             char c = s.charAt(i);
-            end = Math.max(arr[c-'a'], end);
-            if(end == i) {
-                res.add(end - start + 1);
-                start = end + 1;
-                end = start;
-            }
-            // System.out.println("char is: " + c + " end is: " + end + " start is: " + start);
+            farthest = Math.max(farthest, position[c-'a']);
+            if(i==farthest) {res.add(i - start + 1); start = i + 1;}
+            
         }
         return res;
 
-        
         
     }
 }

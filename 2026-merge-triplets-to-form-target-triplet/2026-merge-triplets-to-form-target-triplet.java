36eae3e06bed0a98a3c9bca762da70fe3c2fbb76
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-02-12 22:13:44 +0000

    Time: 5 ms (67.83%), Space: 121.8 MB (25.38%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
-        // 1. filter out triplet if any ith element bigger than target[i]
-        // 2. check if there are ith element = target[i] for all the elements in target
+        // 1. filter out all the triples that triple[i] > target [i]
+        // 2. for all the target[i], must be at least one triple[i]==target[i]
         int[] res = new int[3];
-        for(int[] triplet: triplets){
+
+        for(int[] triple: triplets){
             // 1. filter out
-            if(triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) continue;
-            // 2. check each element
-            if(triplet[0] == target[0]) res[0] = 1;
-            if(triplet[1] == target[1]) res[1] = 1;
-            if(triplet[2] == target[2]) res[2] = 1;
+            if(triple[0] > target[0] || triple[1] > target[1] || triple[2] > target[2]) continue;
+            // 2. match 3 elements
+            if(triple[0] == target[0]) res[0]=1;
+            if(triple[1] == target[1]) res[1]=1;
+            if(triple[2] == target[2]) res[2]=1;
         }
         return res[0]+res[1]+res[2]==3;
-
         
     }
 }

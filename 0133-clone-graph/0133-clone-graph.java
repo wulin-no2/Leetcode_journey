ce22e75e643e418f176bfe138c1345e9b736d96b
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-02-13 16:06:59 +0000

    Time: 28 ms (18.32%), Space: 42.9 MB (35.41%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,33 +19,24 @@ class Node {
 */
 
 class Solution {
+    private Map<Node, Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
-        // bfs
-        // use a queue 
-        if(node == null) return null;
-        // use a map to store origin and cloned node, in case we clone duplicated nodes
-        Map<Node, Node> map = new HashMap<>(); 
-        Deque<Node> q = new LinkedList<>();
-        // clone the first node and put it in the queue and map
-        Node newNode = new Node(node.val);
-        map.put(node, newNode); 
-        q.offer(node);
-        while(! q.isEmpty()){
-            Node temp = q.poll();
-            List<Node> neighbors = temp.neighbors;
-            for(Node neighbor : neighbors){
-                if(! map.containsKey(neighbor)){
-                    Node cloned = new Node(neighbor.val);
-                    map.put(neighbor, cloned);
-                    q.offer(neighbor); // why???
-                }
-                // add the cloned neighbor to the cloned node's neighbor list
-                map.get(temp).neighbors.add(map.get(neighbor));
-            }
+        if(node==null) return null;
+        return dfs(node);
+    }
+    private Node dfs(Node node){
+        if(map.containsKey(node)) return map.get(node);
 
+        // clone 
+        Node clone = new Node(node.val);
+        map.put(node, clone);
+
+        // neighbors
+        for(Node neighbor: node.neighbors){
+            clone.neighbors.add(dfs(neighbor));
         }
-        return newNode;
 
-        
+        return clone;
+
     }
 }

3e7bbb9ea53fc97885c7a59363fc90d6e451147d
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-02-13 15:55:55 +0000

    Time: 28 ms (18.32%), Space: 43.2 MB (13.03%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,54 +19,33 @@ class Node {
 */
 
 class Solution {
-    HashMap<Node, Node> map = new HashMap<>();
-    
     public Node cloneGraph(Node node) {
-        // DFS
+        // bfs
+        // use a queue 
         if(node == null) return null;
-        // check if it's cloned or not
-        if(map.containsKey(node)) return map.get(node);
-        // clone node
-        Node clone = new Node(node.val); 
-        map.put(node, clone);
-        // clone neighbors
-        for(Node neighbor: node.neighbors){
-            // clone it
-            Node clonedNeighbor = cloneGraph(neighbor);
-            clone.neighbors.add(clonedNeighbor);
-        }
-        return clone;
-        
-        
-        /* BFS
-        if(node == null) return null;
-        // use map to store original node for us to clone so that we can't clone duplicatedly
-        HashMap<Node, Node> map = new HashMap<>();
-        // use BFS to traverse and clone
-        Queue<Node> q = new LinkedList<>();
+        // use a map to store origin and cloned node, in case we clone duplicated nodes
+        Map<Node, Node> map = new HashMap<>(); 
+        Deque<Node> q = new LinkedList<>();
+        // clone the first node and put it in the queue and map
+        Node newNode = new Node(node.val);
+        map.put(node, newNode); 
         q.offer(node);
-        
-        //put node into map
-        map.put(node, new Node(node.val));
-        
-        // traverse
         while(! q.isEmpty()){
-            // get a node
             Node temp = q.poll();
-            // process all the neighbours
-            for(Node neighbor: temp.neighbors){
-                if(!map.containsKey(neighbor)){
-                        // if not cloned, clone it
-                        map.put(neighbor, new Node(neighbor.val));
-                        // add it to the queue
-                        q.offer(neighbor);
+            List<Node> neighbors = temp.neighbors;
+            for(Node neighbor : neighbors){
+                if(! map.containsKey(neighbor)){
+                    Node cloned = new Node(neighbor.val);
+                    map.put(neighbor, cloned);
+                    q.offer(neighbor); // why???
                 }
-                // add cloned neighbor to the current node's cloned version
+                // add the cloned neighbor to the cloned node's neighbor list
                 map.get(temp).neighbors.add(map.get(neighbor));
             }
+
         }
-        return map.get(node);
-        */
+        return newNode;
+
+        
     }
-    
 }

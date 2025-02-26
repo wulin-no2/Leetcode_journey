b9fce2ee06415fd1b99bcc74fcf115d1998cea20
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-02-26 12:52:59 +0000

    Time: 10 ms (24.43%), Space: 46.6 MB (54.29%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,24 +2,27 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         // overlapping means if we sort them based on their first elements,
         // the latter interval's first element <= former interval's last element
-        int n = intervals.length;
+        if (intervals.length == 0) return new int[0][];
+        
+        // Sort intervals based on the start time
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
         List<int[]> res = new ArrayList<>();
-        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
         res.add(intervals[0]);
-        for(int i = 1 ; i < n; i++){
-            int former = res.get(res.size()-1)[1];
-            int latter = intervals[i][0];
-            if(latter <= former) {
-                int x = res.get(res.size()-1)[0];
-                int y = Math.max(intervals[i][1], res.get(res.size()-1)[1]);
-                res.set(res.size()-1, new int[]{x,y});
-                //res.add();
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] lastInterval = res.get(res.size() - 1);
+            int[] current = intervals[i];
+
+            // Check if intervals overlap
+            if (current[0] <= lastInterval[1]) {
+                lastInterval[1] = Math.max(lastInterval[1], current[1]); // Merge intervals
+            } else {
+                res.add(current);
             }
-            else {
-                res.add(intervals[i]);
-                }
         }
-        return res.toArray(new int[res.size()][2]);
+        
+        return res.toArray(new int[res.size()][]);
         
     }
 }

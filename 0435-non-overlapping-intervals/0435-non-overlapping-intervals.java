cb415d03c49c7a000b8a87f5563748fe18d17ad0
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-02-27 17:34:46 +0000

    Time: 51 ms (30.21%), Space: 73.7 MB (47.83%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -6,20 +6,13 @@ class Solution {
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
         int n = intervals.length;
         int count = 0;
-        List<int[]> list = new ArrayList<>();
-        list.add(intervals[0]);
+        int prevEnd = intervals[0][1]; // First interval's end 
         for(int i = 1; i < n; i++){
-            int[] first = list.get(list.size() - 1); 
-            int[] second = intervals[i];
             // if overlapping, keep the one with the smaller end
-            if(first[1] > second[0]) {
-                int x = first[0];
-                int y = Math.min(first[1], second[1]);
-                list.set(list.size() - 1, new int[]{x, y});
-                count++;
-            }
+            if(prevEnd > intervals[i][0]) count++;
+            
             // else, put it in the list
-            else list.add(intervals[i]);
+            else prevEnd = intervals[i][1];
         }
         return count;
 

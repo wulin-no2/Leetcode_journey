d6c769846c46e9b2a6d775b4bc676976358fd61d
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-03-10 15:55:51 +0000

    Time: 17 ms (27.43%), Space: 48.7 MB (16.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,27 +1,28 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // encode all the strs, then group them based on their code
-        // use map
-        Map<String, List<String>> map = new HashMap<>(); // code, list of strings with same code
+        // encode each string in a rule
+        // how to encode them? char+number+char+number in letter's order
+        // use map to store the result: map<String, List<String>>, then turn it into res;
+        // how to encode a string? using array hashing
+        Map<String, List<String>> map = new HashMap<>();
         for(String str: strs){
-            // get code
-            // array hashing, then turn it into String code
             int[] arr = new int[26];
             for(char c: str.toCharArray()){
                 arr[c-'a']++;
             }
-            StringBuilder sb =  new StringBuilder();
-            // turn arr into sorted str
-            for(int i = 0 ; i < arr.length; i++){
-                if(arr[i] != 0) sb.append(i + 'a').append(arr[i]);
+            // turn arr to string:
+            StringBuilder code = new StringBuilder();
+            for(int i = 0; i < arr.length; i++){
+                code.append((char)(i + 'a')+ arr[i]);
             }
-            // turn sb into str
-            String code = sb.toString();
-
-            // add str into map's list;
-            map.computeIfAbsent(code, k -> new ArrayList<>()).add(str);
+            // add it to map
+            map.computeIfAbsent(code.toString(), k->new ArrayList<>()).add(str);
         }
-        // turn map's values into list
-        return new ArrayList<>(map.values()); 
+
+        // turn map into res
+        List<List<String>> res = new ArrayList<>(map.values());
+        return res;
+        
+        
     }
 }

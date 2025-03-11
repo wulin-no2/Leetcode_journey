99e97b9accf69dc4d19a9c069445b37c0bae0d0a
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-03-11 18:14:47 +0000

    Time: 33 ms (34.63%), Space: 66.9 MB (49.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,26 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         int res = 0;
-        // use hashset
-        // if num - 1 not in set, maybe it's a start of sequence. then we find num,num+1..
-        HashSet<Integer> set = new HashSet<>();
+        // use a set to store elements
+        // then check one by one to see if there's concecutive elements
+        Set<Integer> set = new HashSet<>();
         for(int num : nums){
             set.add(num);
         }
+        // then check one by one to see if there's concecutive elements. How to check? From the smallest one. 
+        // If no one is smaller 1 than it, then it can be a start; get the following one until can't get anymore; record the length; 
+        // Get next one without an element smaller 1 than 1; do the above again;
+        // the set should be sorted? No. we can check one by one from the array
         for(int num: set){
-            if(! set.contains(num-1)){
-                int longest = 1;
-                num++;
-                while(set.contains(num)){
-                    longest++;
-                    num++;
-                    
-                }
-                res = Math.max(res, longest);
+            if(! set.contains(num - 1)) { // can be a start
+                int count = 0;
+                while(set.contains(num)) {count++; num++;}
+                res = Math.max(count, res);
             }
         }
         return res;
+
+
         
     }
 }

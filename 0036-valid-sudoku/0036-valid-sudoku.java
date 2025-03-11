0de2c71c8a123b02b5e97f18579cda708137902b
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-03-11 17:58:36 +0000

    Time: 1 ms (100%), Space: 44.8 MB (33.9%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,29 +1,28 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // traverse the board, if the char is not '.', record its existence.
-        // for each number, check if it already exists in its row, its column, its cube;
-        boolean[][] rows = new boolean[9][9]; // if row i exist num j
-        boolean[][] cols = new boolean[9][9]; // if col i exist num j       
-        boolean[][] cubes = new boolean[9][9]; // if cube i exist num j 
-        for(int i = 0; i < 9 ;i++){
-            for(int j = 0; j < 9 ; j++){
+        // 3 passes. check row, check column, check box
+        boolean[][] rows = new boolean[9][9]; // if row i has num j
+        boolean[][] cols = new boolean[9][9]; // if col i has num j
+        boolean[][] boxes = new boolean[9][9]; // if box i has num j
+        for(int i = 0 ; i < 9 ; i++){
+            for(int j = 0; j < 9; j++){
                 if(board[i][j] != '.'){
-                    int num = board[i][j]-'1';
+                    int num = board[i][j] - '1'; // num is in 0~8
                     // check row
                     if(rows[i][num]) return false;
-                    else rows[i][num] = true;
+                    rows[i][num] = true;
 
                     // check col
                     if(cols[j][num]) return false;
-                    else cols[j][num] = true;
+                    cols[j][num] = true;
 
-                    // check cube
-                    int cube = (i/3) * 3+ (j/3);
-                    if(cubes[cube][num]) return false;
-                    else cubes[cube][num] = true;
+                    // check box
+                    int box = (i / 3) * 3 + ( j / 3);
+                    if(boxes[box][num]) return false;
+                    boxes[box][num] = true;
                 }
             }
-        }
-        return true;       
+        }  
+        return true;              
     }
 }

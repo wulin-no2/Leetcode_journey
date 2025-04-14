ec3cba3e2279b1e97fdd18311e60a98b17e80a3c
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-04-14 17:03:50 +0100

    Time: 2 ms (99.08%), Space: 43 MB (83.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        // 2 pointers inward
+        // 2 pointers inward, if it's not digit or letter, remove to next, then convert it into lower case
         int left = 0;
-        int right = s.length()-1;
-        while(left < right){
-            while(left<right && ! Character.isLetterOrDigit(s.charAt(left))) left++;
-            while(left<right && ! Character.isLetterOrDigit(s.charAt(right))) right--;
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
-            left++;
-            right--;
-
-
-
+        int right = s.length() - 1;
+        while(left <= right){
+            while(left<=right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while(left<=right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if(left<=right && Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))) return false;
+            else {left++; right--;}
         }
         return true;
-
         
     }
+    
 }

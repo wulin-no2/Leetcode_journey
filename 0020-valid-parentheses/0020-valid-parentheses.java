a19972996533de0639606b6d3146004f87363da9
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-04-16 00:00:59 +0100

    Time: 2 ms (97.45%), Space: 42.1 MB (20.4%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        // stack 
-        // push open brackets into stack
-        // pop it when there is a close bracket
-        Deque<Character> stack = new LinkedList<>();
-        for(int i = 0; i < s.length() ; i++){
+        // use a stack cause we have to match parentheses from the latest to the earlist
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i = 0 ; i < s.length(); i++){
+            // push left parenthesis into stack
+            // pop element to match when it's a right parenthesis
             char c = s.charAt(i);
-            if(c=='(') stack.push(c);
-            if(c=='[') stack.push(c);
-            if(c=='{') stack.push(c);
-            
-            if(c==')'){
+            if(c =='(' || c =='{' || c =='[') stack.push(c);
+            else if(c ==')' || c =='}' || c ==']'){
                 if(stack.isEmpty()) return false;
                 char temp = stack.pop();
-                if(temp!='(') return false;
-            };
-            if(c==']'){
-                if(stack.isEmpty()) return false;
-                char temp = stack.pop();
-                if(temp!='[') return false;
+                if(c == ')' && temp != '(') return false;
+                if(c == '}' && temp != '{') return false;
+                if(c == ']' && temp != '[') return false;
 
             }
-            if(c=='}'){
-                if(stack.isEmpty()) return false;
-                char temp = stack.pop();
-                if(temp!='{') return false;
-            }
-
         }
         return stack.isEmpty();
     }

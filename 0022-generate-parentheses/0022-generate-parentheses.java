643ba229d3b92116172a327a34a7111b61b0fda9
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-04-21 17:04:14 +0100

    Time: 0 ms (100%), Space: 43.2 MB (68.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,31 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        // backtracking
-        // number of open parentheses >= number of close parentheses
-        // number of open parentheses <= n
+        // record count of used left ul and used right ur, 
+        // if ul > ul, then we could choose both l and r in this round, 
+        // otherwise we could only choose l
+        // if ur == n, end it
         List<String> res = new ArrayList<>();
-        backtracking(n, "", 0, 0, res);
+        backtracking(res, new StringBuilder(), 0, 0, n);
         return res;
+        
     }
-    public void backtracking(int n, String curr, int left, int right, List<String> res){
-        // base case
-        if(left==right && left==n) {res.add(curr); return;}
-        // recursive
-        if(left < n) backtracking(n, curr + "(", left + 1, right, res);
-        if(right < left) backtracking(n, curr + ")",left, right + 1, res);
+    public void backtracking(List<String> res, StringBuilder path, int ul, int ur, int n){
+        // get result
+        if (path.length() == 2 * n) {
+            res.add(path.toString());
+            return;
+        }
+        // Can we add a '(' ?
+        if (ul < n) {
+            path.append('(');
+            backtracking(res, path, ul + 1, ur, n);
+            path.deleteCharAt(path.length() - 1);
+        }
 
-
+        if(ul > ur) {
+            path.append(')');
+            backtracking(res, path, ul, ur + 1, n);
+            path.deleteCharAt(path.length() - 1);
+        }
     }
 }

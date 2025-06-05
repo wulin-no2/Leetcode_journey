460c912b0d5ea928b70f2bb670a3e1a39f8dd4ec
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-06-05 14:41:49 +0100

    Time: 13 ms (79.56%), Space: 58.3 MB (60.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+
+
+        // hashing array, hashset, hashmap to detect duplicated element
+        // traverse from left to right
+        // use hashset cause the element's range is big
         HashSet<Integer> set = new HashSet<>();
         for(int num: nums){
             if(set.contains(num)) return true;
             set.add(num);
+
         }
         return false;
+        
     }
 }

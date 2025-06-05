cc41ea1a63a1945e4f7cd50aa2f4e1890bbdea6b
Lina <117023721+wulin-no2@users.noreply.github.com>
2025-06-05 14:43:48 +0100

    Time: 13 ms (79.56%), Space: 58.6 MB (39.59%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -6,12 +6,14 @@ class Solution {
         // traverse from left to right
         // use hashset cause the element's range is big
         HashSet<Integer> set = new HashSet<>();
-        for(int num: nums){
-            if(set.contains(num)) return true;
-            set.add(num);
+        for(int num: nums){  // 0(N)
+            if(set.contains(num)) return true; // 0(1)
+            set.add(num); // 0(1)
 
         }
         return false;
+        // space complexity: O(N) worst 
+        
         
     }
 }

9d02e1a2f8034a9f822933fd3c382270c06ae1b5
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-02-19 20:07:10 +0000

    Time: 6 ms (99.37%), Space: 48.9 MB (99.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,21 +5,32 @@ class Solution {
         // we use letter + number to encode them
         // then use map to put them together, each code is a key
         Map<String, List<String>> map = new HashMap<>();
-        for(String str: strs){
-            // 1. encode it into string
-            // 1.1. get array code
-            int[] arr = new int[26];
-            for(char i : str.toCharArray()){
-                arr[i - 'a']++;
+        // for(String str: strs){
+        //     // 1. encode it into string
+        //     // 1.1. get array code
+        //     int[] arr = new int[26];
+        //     for(char i : str.toCharArray()){
+        //         arr[i - 'a']++;
+        //     }
+        //     // 1.2. get string code
+        //     StringBuilder sb = new StringBuilder();
+        //     for(int i = 0; i < 26; i++){
+        //         sb.append(i + 'a').append(arr[i]);
+        //     }
+        //     String s = sb.toString();
+        //     // 2. put it into map
+        //     map.computeIfAbsent(s, key -> new ArrayList<String>()).add(str);
+        // }
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
-            // 1.2. get string code
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < 26; i++){
-                sb.append(i + 'a').append(arr[i]);
-            }
-            String s = sb.toString();
-            // 2. put it into map
-            map.computeIfAbsent(s, key -> new ArrayList<String>()).add(str);
+            
+            map.get(sortedWord).add(word);
         }
         // put values into a list
         // List<List<String>> res = new ArrayList<>();

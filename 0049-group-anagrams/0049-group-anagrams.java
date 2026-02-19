9196e9ee24cf26cd5dbbda6d910530b575225a58
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-02-19 20:05:14 +0000

    Time: 24 ms (8.41%), Space: 51.5 MB (5.3%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,33 +1,32 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // encode each string into a key, make the strings with same code into a list as a value
-        // now there is a map for us to track all the strings with same code
+        // we have to encode the type of each string.
+        // for each string, we have to remember it's letters and each count of them
+        // we use letter + number to encode them
+        // then use map to put them together, each code is a key
         Map<String, List<String>> map = new HashMap<>();
-        // finally turn map into list
-
-        // first question is, how to encode string?
-        // encode string into int array, then turn int array into string; 
         for(String str: strs){
-            // encode str
+            // 1. encode it into string
+            // 1.1. get array code
             int[] arr = new int[26];
-            // turn str into int array
-            for(int i = 0; i < str.length(); i++){
-                arr[str.charAt(i) -'a']++;
+            for(char i : str.toCharArray()){
+                arr[i - 'a']++;
             }
-            // turn int array into string
+            // 1.2. get string code
             StringBuilder sb = new StringBuilder();
             for(int i = 0; i < 26; i++){
-                if(arr[i] != 0) sb.append(arr[i]).append((char)(i + 'a'));
+                sb.append(i + 'a').append(arr[i]);
             }
-            String code = sb.toString();
-            // put code and str into map:
-            map.computeIfAbsent(code, k->new ArrayList<>()).add(str);
-
+            String s = sb.toString();
+            // 2. put it into map
+            map.computeIfAbsent(s, key -> new ArrayList<String>()).add(str);
         }
-        // turn map into list;
+        // put values into a list
+        // List<List<String>> res = new ArrayList<>();
+        // for(List<String> value: map.values()){
+        //     res.add(value);
+        // }
+        // return res;
         return new ArrayList<>(map.values());
-
-
-        
     }
 }

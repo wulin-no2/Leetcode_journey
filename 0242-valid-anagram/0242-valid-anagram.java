70993a2408bf9bba7a92763f39855505dcdabcb1
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-02-19 19:18:17 +0000

    Time: 19 ms (11.71%), Space: 46.5 MB (41.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        // same elements with same counts
-        // HashMap
-        Map<Character, Integer> map = new HashMap<>(); // element, count
-        // Travers s and put elements into map;
-        for(int i = 0; i < s.length(); i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        // store the value and the counts, using map
+        // pop in s to map
+        // pop out from map
+        // use map or array of length 26
+        // method 1, map
+        Map<Character, Integer> map = new HashMap<>();
+        for(char i: s.toCharArray()){
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        // Travers t and get elements out of map;
-        for(int i = 0; i < t.length(); i++){
-            if(! map.containsKey(t.charAt(i))) return false;
-            map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
-            if(map.get(t.charAt(i)) < 0) return false;
+        for(char i: t.toCharArray()){
+            if(! map.containsKey(i) || map.get(i) == 0) return false;
+            // if map.get(i) > 0, then map.put(i, map.get(i) - 1);
+            map.put(i, map.get(i) - 1);
         }
         return true;
+        
     }
-    
 }

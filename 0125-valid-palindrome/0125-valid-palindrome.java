7ff1946c257696713f003be34e98c2f37d4be7c0
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-02-19 22:37:22 +0000

    Time: 2 ms (98.99%), Space: 44.2 MB (92.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -8,21 +8,17 @@ class Solution {
         int p1 = 0;
         int p2 = s.length() - 1;
         while (p1 < p2){
-            while(p1 < p2 & !Character.isLetterOrDigit(s.charAt(p1))){
+            while(p1 < p2 && !Character.isLetterOrDigit(s.charAt(p1))){
                 p1++;
-                // System.out.println("p1 " + p1);
             }
-            while(p1 < p2 & !Character.isLetterOrDigit(s.charAt(p2))){
+            while(p1 < p2 && !Character.isLetterOrDigit(s.charAt(p2))){
                 p2--;
-                // System.out.println("p2 " + p2);
             }
             if(p1 < p2) {
-                System.out.println("p1 p2 c1 c2 are " + p1 + " " + p2 + " " + s.charAt(p1) + " " + s.charAt(p2) );
                 if(Character.toLowerCase(s.charAt(p1)) != Character.toLowerCase(s.charAt(p2))) return false;
                 else {
                     p1++;
                     p2--;
-                    // System.out.println("p1 p2 are " + p1 + " " + p2);
                 }
             }
         }

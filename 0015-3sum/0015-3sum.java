13419c1af40181ff34ebec20775e1bca4f4d2e1b
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-02-21 15:22:10 +0000

    Time: 35 ms (44.29%), Space: 59.1 MB (75.18%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // 2 pointers. fix the first number, then 2 pointers inward
-        // but sort it first in order to user 2 pointers and avoid duplicated triplets;
+        // fix one pointer, then move two other pointers inward
+        // sort it first
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        int first = 0;
-        while(first < nums.length - 2){
-            // trim when the first element is already bigger than 0;
-            if(nums[first] > 0) break;
-            while(first > 0 && first < nums.length - 2 && nums[first]==nums[first-1]) first++;
-            // 2 pointers;
-            int left = first + 1;
-            int right = nums.length - 1;
-            while(left < right){
-                if(left < right && nums[left] + nums[right] + nums[first] > 0) right--;
-                else if(left < right && nums[left] + nums[right] + nums[first] < 0) left++;
-                else if(left < right && nums[left] + nums[right] + nums[first] == 0){
-                    res.add(Arrays.asList(nums[left], nums[right], nums[first]));
-                    left++;
-                    right--;
-                    // avoid duplicated left & right
-                    while(left < right && nums[left] == nums[left - 1]) left++;
-                    while(left < right && nums[right] == nums[right + 1]) right--;
-                    }
-                else break;
+        int i = 0;
+        while(i < nums.length - 2){
+            int target = - nums[i];
+            int p1 = i + 1;
+            int p2 = nums.length - 1;
+            while(p1 < p2){
+                // compare 
+                if(nums[p1] + nums[p2] == target) {
+                    res.add(Arrays.asList(nums[i], nums[p1], nums[p2]));
+                    // remove duplicate
+                    while(p1 < p2 && nums[p1] == nums[p1 + 1]) {p1++;}
+                    while(p1 < p2 && nums[p2] == nums[p2 - 1]) {p2--;}
+                    p1++;
+                    p2--;
+                } else if (nums[p1] + nums[p2] > target){
+                    p2--;
+                }
+                else {
+                    p1++;
+                }
             }
-            // move first
-            first++;
+            // remove duplicate for p0
+            while(i < nums.length - 2 && nums[i] == nums[i + 1]) i++;
+            i++;
+            // System.out.println("3 -- nums p0 p2 p2 are: " + nums[i] + " " + nums[p1] + " " + nums[p2]);
         }
         return res;
-        
     }
 }

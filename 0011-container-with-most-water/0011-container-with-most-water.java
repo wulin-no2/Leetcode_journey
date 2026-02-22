52d80351f44a98f2f84c663b281232d51677ee13
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-02-22 01:07:10 +0000

    Time: 5 ms (81.14%), Space: 77.3 MB (69.42%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,18 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
-        // 2 pointers inward
-        // move the shorter one when we find a higher line
-        int res = 0;
-        int left = 0;
-        int right = height.length - 1;
-        res = Math.min(height[left], height[right]) * (right - left);
-        while(left < right){
-            int leftHeight = height[left];
-            int rightHight = height[right];
-            if(left < right && leftHeight <= rightHight) left++;
-            else if(left < right && leftHeight > rightHight) right--;
-            res = Math.max(res, Math.min(height[left], height[right]) * (right - left));
+        // two pointers inward
+        // move the shorter one, if we can find a taller one and make result bigger, then move it
+        int p1 = 0;
+        int p2 = height.length - 1;
+        int res = Math.min(height[p2], height[p1]) * (p2 - p1);
+        while(p1 < p2){
+            while(p1 < p2 && height[p2] > height[p1]){
+                // move p1
+                p1++;
+                res = Math.max(res, Math.min(height[p2], height[p1]) * (p2 - p1));
+            }
+            while(p1 < p2 && height[p2] <= height[p1]){
+                p2--;
+                res = Math.max(res,  Math.min(height[p2], height[p1]) * (p2 - p1));
+            }
         }
         return res;
+        
     }
 }

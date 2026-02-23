3a09eb73cc5ed1ddf6112d7efc29569f03f133b7
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-02-23 22:44:40 +0000

    Time: 29 ms (92.01%), Space: 59.4 MB (45.65%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // fix one pointer, then move two other pointers inward
-        // sort it first
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        int i = 0;
-        while(i < nums.length - 2){
-            int target = - nums[i];
-            int p1 = i + 1;
-            int p2 = nums.length - 1;
-            while(p1 < p2){
-                // compare 
-                if(nums[p1] + nums[p2] == target) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
+            if (nums[i] > 0) break; // early stop
+
+            int p1 = i + 1, p2 = n - 1;
+            int target = -nums[i];
+
+            while (p1 < p2) {
+                int sum = nums[p1] + nums[p2];
+                if (sum == target) {
                     res.add(Arrays.asList(nums[i], nums[p1], nums[p2]));
-                    // remove duplicate
-                    while(p1 < p2 && nums[p1] == nums[p1 + 1]) {p1++;}
-                    while(p1 < p2 && nums[p2] == nums[p2 - 1]) {p2--;}
-                    p1++;
+                    while (p1 < p2 && nums[p1] == nums[p1 + 1]) p1++;
+                    while (p1 < p2 && nums[p2] == nums[p2 - 1]) p2--;
+                    p1++; p2--;
+                } else if (sum > target) {
                     p2--;
-                } else if (nums[p1] + nums[p2] > target){
-                    p2--;
-                }
-                else {
+                } else {
                     p1++;
                 }
             }
-            // remove duplicate for p0
-            while(i < nums.length - 2 && nums[i] == nums[i + 1]) i++;
-            i++;
-            // System.out.println("3 -- nums p0 p2 p2 are: " + nums[i] + " " + nums[p1] + " " + nums[p2]);
         }
         return res;
     }

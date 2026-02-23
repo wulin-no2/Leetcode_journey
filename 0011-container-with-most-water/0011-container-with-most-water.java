109131b7f780d6d3b86a0a98ab398119ccf659ed
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-02-23 21:56:13 +0000

    Time: 5 ms (81.31%), Space: 77.4 MB (48.09%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,19 +4,15 @@ class Solution {
         // move the shorter one, if we can find a taller one and make result bigger, then move it
         int p1 = 0;
         int p2 = height.length - 1;
-        int res = Math.min(height[p2], height[p1]) * (p2 - p1);
+        int res = 0; 
         while(p1 < p2){
-            while(p1 < p2 && height[p2] > height[p1]){
+            res = Math.max(Math.min(height[p2], height[p1]) * (p2 - p1), res);
+            if(height[p2] > height[p1]){
                 // move p1
                 p1++;
-                res = Math.max(res, Math.min(height[p2], height[p1]) * (p2 - p1));
             }
-            while(p1 < p2 && height[p2] <= height[p1]){
-                p2--;
-                res = Math.max(res,  Math.min(height[p2], height[p1]) * (p2 - p1));
-            }
+            else p2--;
         }
         return res;
-        
     }
 }

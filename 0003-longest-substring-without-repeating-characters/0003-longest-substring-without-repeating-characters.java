a8a1d3b638d3627efd04aa2863a4bad04e1131cf
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-03-18 21:15:15 +0000

    Time: 6 ms (69.52%), Space: 46.5 MB (45.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,29 +1,26 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // sliding window 
-        // use left and right to ensure the window
-        // use set to make sure the substring doesn't have repeating characters
-        // one pass
-        // if there is repeating characters,shrink from the left until no repeating characters
-        if(s.length() < 2) return s.length();
-        Set<Character> set = new HashSet<>();
+        // sliding window
+        // if not duplicate, right++; if duplicate, left++ until not duplidate
+        // keep updating length;
+        int length = s.length();
+        if(length == 0 || length == 1) return length;
         int left = 0;
-        int right = 0;
-        int res = 0;
-        while(right < s.length()){
-            // substring(0,1), len = right - left;
-            char c = s.charAt(right);
-
-                while(left < right && set.contains(c)){
-                    set.remove(s.charAt(left));
-                    left++;
-                }
-                set.add(c);
-                right++;
-                res = Math.max(res, right - left);
-            
+        int right = 1;
+        int res = 1;
+        // use set to detect duplicates
+        Set<Character> set = new HashSet<>();
+        set.add(s.charAt(left));
+        while(right < length){
+            char temp = s.charAt(right);
+            while(set.contains(temp)){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(temp);
+            res = Math.max(set.size(), res);
+            right++;
         }
         return res;
-        
     }
 }

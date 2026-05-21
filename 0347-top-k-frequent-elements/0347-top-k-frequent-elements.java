b5810b4e907c0122b4842b9533428c8335400524
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-05-21 16:58:01 +0100

    Time: 15 ms (54.71%), Space: 47.7 MB (47.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,25 +1,19 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // we sort the elements based on their frequencies
-        // map to store key value pairs map <num, frequency>
-
         Map<Integer, Integer> map = new HashMap<>();
         for(int num: nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        // then sort the EntrySet based on values
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b) -> Integer.compare(b.getValue(),a.getValue()));
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b)->Integer.compare(b.getValue(), a.getValue()));
         for(Map.Entry<Integer, Integer> entry: map.entrySet()){
             pq.offer(entry);
         }
-        // then return top k elements of this keySet
         int[] res = new int[k];
         while(k > 0){
-            res[k-1] = pq.poll().getKey();
+            res[k - 1] = pq.poll().getKey();
+
             k--;
         }
         return res;
-
-        
     }
 }

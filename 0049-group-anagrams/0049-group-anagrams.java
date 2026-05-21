58a1ef5fd98084bd35bc0f2e552ac72c69117cc8
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-05-21 16:53:56 +0100

    Time: 7 ms (73.86%), Space: 49.5 MB (83.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,25 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // for each str, we encode it;
-        // if the code exist, then we put str in the same area;
-        // map<code, list>
         Map<String, List<String>> map = new HashMap<>();
         for(String str: strs){
-            // encode str
+            // encode it
             char[] chars = str.toCharArray();
             Arrays.sort(chars);
-            String code = new String(chars); // convert char array to string
-            // check if map contains the code
-            // if not contains, put code and str in the map and create a new list
+            String code = new String(chars);
+            // put it in map
             if(!map.containsKey(code)){
                 map.put(code, new ArrayList<>());
             }
-            // if contains, put str in the list
             map.get(code).add(str);
         }
-        // now we got the map
-        // we convert it into our list result;
-        // go through the values and put them into list one by one
         return new ArrayList<>(map.values());
+        
     }
 }

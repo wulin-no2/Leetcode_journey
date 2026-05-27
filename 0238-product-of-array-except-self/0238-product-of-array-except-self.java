42f0332ed952fa30c6c21bc1d46d8751d29094bd
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-05-27 12:02:52 +0100

    Time: 3 ms (27.45%), Space: 64.5 MB (91.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // if we traverse once, we can either get the product of the prefix or the suffix
-        // so we have to traverse twice in different order
-        // then multiple them:
-        int length = nums.length;
-        int[] prefix = new int[length];
-        int[] suffix = new int[length];
-        int[] res = new int[length];
-        for(int i = 0 ; i < length; i++){
-            if(i == 0) prefix[i] = 1;
-            else prefix[i] = nums[i - 1] * prefix[i - 1];
+        int len = nums.length;
+        int[] prefix = new int[len];
+        int[] suffix = new int[len];
+        prefix[0] = 1;
+        for(int i = 1 ; i < len ; i++){
+            prefix[i] = prefix[i - 1] * nums[i - 1];
         }
-        for(int i = length - 1; i >= 0; i --){
-            if(i == length - 1) suffix[i] = 1;
-            else suffix[i] = nums[i + 1] * suffix[i + 1];
-            res[i] = suffix[i] * prefix[i];
+        suffix[len - 1] = 1;
+        for(int i = len - 2; i >= 0; i--){
+            suffix[i] = suffix[i + 1] * nums[i + 1];
         }
-        return res;
-
-        
+        // get the result
+        for(int i = 0 ; i < len; i++){
+            prefix[i] = prefix[i] * suffix[i];
+        }
+        return prefix;
     }
 }

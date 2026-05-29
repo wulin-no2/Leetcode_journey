85ae773b4f1ed49155766d5215942b4fbaf8965c
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-05-29 11:46:11 +0100

    Time: 3 ms (60.44%), Space: 44.5 MB (51.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        char[] arr = s.toCharArray();
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left < right) {
-
-            while (left < right && !Character.isLetterOrDigit(arr[left])) {
-                left++;
+        // we use 2 pointers going forward
+        int p1 = 0;
+        int p2 = s.length() - 1;
+        // we walk through this string
+        while(p1 < p2){
+            // if char is not digit or letter, move pointer
+            if(!Character.isLetterOrDigit(s.charAt(p1))) p1++;
+            else if(!Character.isLetterOrDigit(s.charAt(p2))) p2--;
+            else if(Character.isLetterOrDigit(s.charAt(p1)) && Character.isLetterOrDigit(s.charAt(p2))){
+                // if it is, compare two chars
+                // if they are different, return false
+                if(Character.toLowerCase(s.charAt(p1)) != Character.toLowerCase(s.charAt(p2))) return false;
+                else{
+                    p1++;
+                    p2--;
+                }
             }
-
-            while (left < right && !Character.isLetterOrDigit(arr[right])) {
-                right--;
-            }
-
-            if (Character.toLowerCase(arr[left]) != 
-                Character.toLowerCase(arr[right])) {
-                return false;
-            }
-
-            left++;
-            right--;
         }
-
         return true;
     }
 }

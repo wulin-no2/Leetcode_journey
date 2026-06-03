beef7e8973d29cb4e7ffe03a87eff07da79e17e8
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-06-03 17:13:53 +0100

    Time: 0 ms (100%), Space: 43.5 MB (94.51%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,21 +1,28 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // it can be seen as a 1D non-decreasing array 
-        int m = matrix.length;
-        int n = matrix[0].length;
+        // make it into 1D array
+        // Binary search
+        int m = matrix[0].length;
+        int n = matrix.length;
+        if(target < matrix[0][0] || target > matrix[n-1][m-1]) return false;
         int left = 0;
-        int right = m * n; // left closed right open
+        int right = n;
         while(left < right){
-            int mid = (right - left) / 2 + left;  // index
-            // find the val
-            int row = mid / n;
-            int col = mid % n;
-            int val = matrix[row][col];
-            if(val == target) return true;
-            else if(val > target) right = mid;
+            int mid = (left + right) / 2;
+            if(target == matrix[mid][0]) return true;
+            if(target < matrix[mid][0]) right = mid;
             else left = mid + 1;
         }
+        // for line left
+        int l = 0;
+        int r = m;
+        while(l < r){
+            int mid = (l + r) / 2;
+            if(target == matrix[left - 1][mid]) return true;
+            if(target < matrix[left - 1][mid]) r = mid;
+            else l = mid + 1;
+        }
         return false;
-     
+        
     }
 }

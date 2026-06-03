205ad8a2e1476df2f2130ee1d134e2906ecc99cb
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-06-03 23:04:05 +0100

    Time: 7 ms (79.84%), Space: 47.7 MB (70.93%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,15 +1,19 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         // the answer is between 1 to biggest element 
-        // use binary search tree
-        Arrays.sort(piles);
-        int n = piles.length;
+        // use binary search 
+        int max = 0;
+
+        for (int pile : piles) {
+            max = Math.max(max, pile);
+        }
+
         int left = 1;
-        int right = piles[n - 1] + 1;
-        int res = piles[n - 1];
+        int right = max + 1; // [left, right)
+
         while(left < right){
             int mid = left + (right - left) / 2;
-            int time = 0;
+            long time = 0;
             // caculate time
             for(int pile: piles){
                 time += (pile + mid - 1) / mid;
@@ -17,11 +21,11 @@ class Solution {
             if(time > h) left = mid + 1;
             else {
                 // we might get answer, but try to decrease speed (mid)
-                res = Math.min(mid, res);
+                // res = Math.min(mid, res); // we don't need it, cause left is the answer
                 right = mid;
             }
         }
-        return res;
+        return left;
         
     }
 }

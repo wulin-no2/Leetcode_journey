acbd4f778a9c94779231d944b4cbcf05d1e09be0
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-06-03 22:35:20 +0100

    Time: 0 ms (100%), Space: 43.6 MB (88.91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // make it into 1D array
-        // Binary search
-        int m = matrix[0].length;
-        int n = matrix.length;
-        if(target < matrix[0][0] || target > matrix[n-1][m-1]) return false;
+        // binary search
+        // make it into a 1D array
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] arr = new int[m * n - 1];
+        if(target < matrix[0][0] || target > matrix[m - 1][n-1]) return false;
         int left = 0;
-        int right = n;
+        int right = m * n;
         while(left < right){
-            int mid = (left + right) / 2;
-            if(target == matrix[mid][0]) return true;
-            if(target < matrix[mid][0]) right = mid;
+            int mid = (right + left) / 2;
+            // compare matrix[][] vs target
+            int row = mid / n;
+            int col = mid % n;
+            if(matrix[row][col] == target) return true;
+            if(matrix[row][col] > target) right = mid;
             else left = mid + 1;
-        }
-        // for line left
-        int l = 0;
-        int r = m;
-        while(l < r){
-            int mid = (l + r) / 2;
-            if(target == matrix[left - 1][mid]) return true;
-            if(target < matrix[left - 1][mid]) r = mid;
-            else l = mid + 1;
         }
         return false;
         

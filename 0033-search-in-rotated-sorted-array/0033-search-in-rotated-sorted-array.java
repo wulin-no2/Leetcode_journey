9841bcc8040956ced364e85b5527eece1865ee1b
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-06-09 14:58:06 +0100

    Time: 0 ms (100%), Space: 44 MB (10.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0, right = nums.length; // [left, right)
-
-        while (left < right) {
+        // binary search, find a target, left close right close
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
             int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) return mid;
-
-            // Left half [left..mid] is sorted
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid;          // target in left sorted half
-                } else {
-                    left = mid + 1;       // target in right half
-                }
-            }
-            // Right half [mid..right-1] is sorted
-            else {
-                if (nums[mid] < target && target <= nums[right - 1]) {
-                    left = mid + 1;       // target in right sorted half
-                } else {
-                    right = mid;          // target in left half
-                }
+            if(nums[mid] == target) return mid;
+            // find something stable. There's always one side is in order, find it
+            // if left side is in order
+            if(nums[mid] >= nums[left]){
+                // see if target is between them
+                if(target >= nums[left] && target < nums[mid]) right = mid - 1;
+                else left = mid + 1;
+            } else { // if right side is in order
+                // see if target is between right and mid
+                if(target > nums[mid] && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
             }
         }
-        return -1;
+     return -1;
     }
 }

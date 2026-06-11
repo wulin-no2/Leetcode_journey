216024820d917bf29b61b5277a3a6a025b765224
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-06-11 23:11:44 +0100

    Time: 1 ms (99.93%), Space: 94.6 MB (31.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // we can traverse the array
-        // calculate the max profit we can make on each day
-        // then return the maximum value of the results
-        // so have to track the smallest value so we can get the max profit on the day
-        int res = 0;
-        int min_until_now = prices[0];
+        // assume we might see it everyday, update the max profit day by day
+        int max = 0;
+        int minPrice = Integer.MAX_VALUE;
         for(int price: prices){
-            int profit = price - min_until_now;
-            res = Math.max(profit, res);
-            min_until_now = Math.min(min_until_now, price);
+            max = Math.max(max, price - minPrice);
+            minPrice = Math.min(minPrice, price);
         }
-        return res;
+        return max;
         
     }
 }

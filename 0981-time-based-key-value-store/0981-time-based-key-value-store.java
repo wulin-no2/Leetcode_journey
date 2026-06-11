135569b7a7f585e701fbc8d2bd5eaf3aede10744
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-06-11 22:49:06 +0100

    Time: 132 ms (79.91%), Space: 109.4 MB (11.83%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,49 +1,41 @@
 class TimeMap {
+    // we need a map<key,<timestamp,value>>
+    // for <timestamp, value>, timestamp in already in order
+    // we just need to find a timestamp which is <= timeValue that's retrieved 
+    // so in this <timestamp, value> collection, we do binary search to find the first F - 1
+    // which feels like TTT[T]FFFFF, we get the last T or the first F - 1
+    Map<String, ArrayList<Pair>> map = new HashMap<>();
     class Pair {
-        int timestamp;
+        Integer timestamp;
         String value;
-
-        Pair(int timestamp, String value) {
-            this.timestamp = timestamp;
+        public Pair(Integer timestamp, String value) {
+            this.timestamp = timestamp; // problem 1
             this.value = value;
         }
     }
-    Map<String, List<Pair>> map;
 
     public TimeMap() {
-        map = new HashMap<>();
-        
     }
     
     public void set(String key, String value, int timestamp) {
-        map.computeIfAbsent(key, k -> new ArrayList<>())
-           .add(new Pair(timestamp, value));
-        
+        Pair pair = new Pair(timestamp, value);
+        map.computeIfAbsent(key, k -> new ArrayList<Pair>()).add(pair); // problem 2
     }
     
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) {
-            return "";
-        }
+        // get collection first
+        if(!map.containsKey(key)) return "";
         List<Pair> list = map.get(key);
+        // binary search
         int left = 0;
-        int right = list.size();
-        while (left < right) {
-
+        int right = list.size(); // problem 3
+        while(left < right){
             int mid = left + (right - left) / 2;
-
-            if (list.get(mid).timestamp <= timestamp) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+            if(list.get(mid).timestamp > timestamp) right = mid;
+            else left = mid + 1;
         }
-        int index = left - 1;
-        if (index < 0) {
-            return "";
-        }
-        return list.get(index).value;
-
+        if (left == 0) return ""; // problem 4
+        return list.get(left - 1).value;
         
     }
 }

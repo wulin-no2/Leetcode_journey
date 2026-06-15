7f5c6a9e85f869199198b3157d5739773d812d13
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-06-15 11:14:07 +0100

    Time: 5 ms (86.3%), Space: 45.6 MB (68.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,26 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // sliding window
-        // if not duplicate, right++; if duplicate, left++ until not duplidate
-        // keep updating length;
-        int length = s.length();
-        if(length == 0 || length == 1) return length;
         int left = 0;
-        int right = 1;
-        int res = 1;
-        // use set to detect duplicates
-        Set<Character> set = new HashSet<>();
-        set.add(s.charAt(left));
-        while(right < length){
-            char temp = s.charAt(right);
-            while(set.contains(temp)){
-                set.remove(s.charAt(left));
-                left++;
+        int maxLength = 0;
+        // sliding window
+        // use map or set to track characters in the window
+        Map<Character, Integer> map = new HashMap<>();
+        for(int right = 0; right < s.length(); right++){
+            char c = s.charAt(right);
+        // for the right side character:
+        // 1. check if it's in map or set, move left, update map or set, put it in, update maxLength
+            if(map.containsKey(c)){
+                left = Math.max(map.get(c) + 1, left);
             }
-            set.add(temp);
-            res = Math.max(set.size(), res);
-            right++;
+        // if it's not, put it in, update maxLength
+            map.put(c, right);
+            maxLength = Math.max(maxLength, right - left + 1);
         }
-        return res;
+        return maxLength;
     }
 }

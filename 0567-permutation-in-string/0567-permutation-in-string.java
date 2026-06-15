fcbc3573b9dbf0b683842791c7790eb06fe37c62
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-06-15 17:21:16 +0100

    Time: 5 ms (97.69%), Space: 44 MB (62.52%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()) return false;
-        // use a sliding window to checkValid
-        int left = 0;
-        for(int right = left + s1.length() - 1; right < s2.length() ; right++){
-            String sub = s2.substring(left, right + 1);
-            if(isValid(s1, sub)) return true;
+        // cause they only consist of letters, we can compare them with Arrays.equals(), O(1)
+        if(s1.length() > s2.length()) return false;
+        // we encode them into char array
+        int[] code = new int[26];
+        int[] windowCode = new int[26];
+        for(char c: s1.toCharArray()){
+            code[c - 'a']++;
+        }
+        for(char c: s2.substring(0, s1.length()).toCharArray()){
+            windowCode[c - 'a']++;
+        }
+        if(Arrays.equals(code, windowCode)) return true;
+        // move window
+        int left = 1;
+        for(int right = s1.length(); right < s2.length() ; right++){
+            // get windowCode
+            windowCode[s2.charAt(right) - 'a']++;
+            windowCode[s2.charAt(left - 1) - 'a']--;
+            // compare with code
+            if(Arrays.equals(code, windowCode)) return true;
             left++;
         }
         return false;
-    }
-    boolean isValid(String s1, String s2){
-        // check if s2 is s1's permutation when they're the same length
-        int[] arr = new int[26];
-        for(char c: s1.toCharArray()){
-            arr[c-'a']++;
-        }
-        for(char c: s2.toCharArray()){
-            arr[c-'a']--;
-            if(arr[c-'a'] < 0) return false;
-        }
-        return true;
+
+        
     }
 }

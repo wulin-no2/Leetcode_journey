eb2866df9bfb31faf5dac0b76c5806b3dbec9a5b
Lina <117023721+wulin-no2@users.noreply.github.com>
2026-08-31 15:57:14 +0100

    Time: 1 ms (100%), Space: 46.3 MB (90.12%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,24 +10,41 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(); // dummyHead
-        ListNode p = res; // to traverse and construct list
-        int carry = 0;
-        while(l1 != null || l2 != null || carry != 0){
-            int sum = carry;
-            if(l1 != null){
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if(l2 != null){
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            // construct node
-            p.next = new ListNode(sum % 10);
-            carry = sum / 10;
-            p = p.next;
+        // we need a seperate flag to remember if we need to +1 to the next digit
+        ListNode dummyHead = new ListNode(0);
+        ListNode p0 = dummyHead;
+        int complement = 0;
+        while(l1 != null && l2 != null){
+            int temp = (l1.val + l2.val + complement) % 10;
+            complement = (l1.val + l2.val + complement) / 10;
+            ListNode curr = new ListNode(temp); // create node
+            p0.next = curr;// link to the last one
+            p0 = curr; // move pointer
+            l1 = l1.next;
+            l2 = l2.next;//move 2 lists
         }
-        return res.next;
-}
+        while(l1 != null){
+            int temp = (l1.val + complement) % 10;
+            complement = (l1.val + complement) / 10;
+            ListNode curr = new ListNode(temp); // create node
+            p0.next = curr;// link to the last one
+            p0 = curr; // move pointer
+            l1 = l1.next; // move list
+        }
+        while(l2 != null){
+            int temp = (l2.val + complement) % 10;
+            complement = (l2.val + complement) / 10;
+            ListNode curr = new ListNode(temp); // create node
+            p0.next = curr;// link to the last one
+            p0 = curr; // move pointer
+            l2 = l2.next; // move list
+        }
+        if(complement == 1){
+            ListNode curr = new ListNode(1);
+            p0.next = curr;
+        }
+        return dummyHead.next;
+        
+        
+    }
 }
